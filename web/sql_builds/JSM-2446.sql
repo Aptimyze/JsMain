9d67f115cc8983b5a683608199ed37a944d483e8
lavesh <lavesh.rawat@jeevansathi.com>
2016-06-07 02:35:11 +0530

    JSM-2246 track deleted email reusage

---
+++ web/sql_builds/JSM-2446.sql
@@ -0,0 +1,11 @@
+use REGISTER;
+
+CREATE TABLE `TRACK_REUSAGE_EMAIL_DELETED` (
+ `S_NO` int(10) unsigned NOT NULL AUTO_INCREMENT,
+ `EMAIL` varchar(100) NOT NULL,
+ `CHANNEL` enum('Desktop','MS','NewMS','Android','Ios','Offline') DEFAULT NULL,
+ `TIME` datetime DEFAULT NULL,
+ PRIMARY KEY (`S_NO`),
+ UNIQUE KEY `EMAIL` (`EMAIL`)
+);
+
