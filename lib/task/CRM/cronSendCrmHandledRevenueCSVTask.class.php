cd921c12f92d4a3944b2023b51ce05811483bc05
richa.arora <ankita.g@99acres.com>
2016-06-06 17:09:45 +0530

    JSC-1446

--- lib/task/CRM/cronSendCrmHandledRevenueCSVTask.class.php
+++ lib/task/CRM/cronSendCrmHandledRevenueCSVTask.class.php
@@ -45,13 +45,21 @@ EOF;
 		//generate csv if valid fortnight passed
 		if($fortnight=="H1" || $fortnight=="H2")
 		{
-			$fortnight = substr($fortnight, 1);
+			$fortnightValue = substr($fortnight, 1);
 			$file_path = JsConstants::$docRoot."/uploads/crmRevenue.csv";
 			$fp = fopen($file_path, "w");
-
+			if($fortnight=="H1")
+			{
+				$month = date('M');
+			}
+			else
+			{
+				$month = date('M',strtotime("- 1 month"));
+			}	
+			
 			//send curl request to mis action to generate csv
 			$tuCurl = curl_init();
-	        curl_setopt($tuCurl, CURLOPT_URL, JsConstants::$siteUrl."/operations.php/crmMis/crmHandledRevenueCsvGenerate?fromMisCron=1&monthValue="."May"."&yearValue=".date('Y')."&fortnightValue=".$fortnight."&report_type=TEAM&report_content=REVENUE&report_format=XLS&dialer_check=1");
+	        curl_setopt($tuCurl, CURLOPT_URL, JsConstants::$siteUrl."/operations.php/crmMis/crmHandledRevenueCsvGenerate?fromMisCron=1&monthValue=".$month."&yearValue=".date('Y')."&fortnightValue=".$fortnightValue."&report_type=TEAM&report_content=REVENUE&report_format=XLS&dialer_check=1");
 	        //curl_setopt($tuCurl, CURLOPT_RETURNTRANSFER, 1);
 	        curl_setopt($tuCurl, CURLOPT_FILE, $fp);
 			curl_setopt($tuCurl, CURLOPT_TIMEOUT, 20);
@@ -67,11 +75,12 @@ EOF;
 	        else
 	        {
 		        //send csv as mail
-		        $to = "ankita.g@jeevansathi.com";
+		        $to = "ankita.g@jeevansathi.com,jitesh.bhugra@naukri.com,rohan.mathur@jeevansathi.com,anamika.singh@jeevansathi.com,rajeev.joshi@jeevansathi.com,Amit.Malhotra@jeevansathi.com";
+		       // $to = "ankita.g@jeevansathi.com";
 		        $message = "PFA";
 		        $subject = "Crm revenue mis csv";
 		        $csvAttachment = file_get_contents($file_path);
-		        print_r($csvAttachment);
+		        //print_r($csvAttachment);
 		        SendMail::send_email($to,$message,$subject,"","","",$csvAttachment,"","crmHandledRevenue.csv");
 		        unset($csvAttachment);
 	    	}
