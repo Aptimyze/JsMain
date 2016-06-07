e230ed32a9f2b2dfecef13be5a901ea319003d41
lavesh <lavesh.rawat@jeevansathi.com>
2016-06-07 13:45:12 +0530

    JSM-2450 changes for screen new 500 error issue

--- web/jsadmin/screen_new.php
+++ web/jsadmin/screen_new.php
@@ -558,7 +558,7 @@ if (authenticated($cid)) {
 			$smarty->assign("myprofilechecksum", $myprofilechecksum);
 			$checksum = $protect_obj->js_encrypt($myprofilechecksum, $to);
 			$smarty->assign("CHECKSUM", $checksum);
-			
+		
 			if ($val == 'edit') {
 				
 				$cScoreObject = ProfileCompletionFactory::getInstance(null,null,$pid);
@@ -566,6 +566,7 @@ if (authenticated($cid)) {
 				$arrMsgDetails= $cScoreObject->GetIncompleteDetails();
 				$arrLinkDetails = array(
 				'ME'=>"ABOUT_ME",
+                                'BASIC'=>"BASIC_DETAILS",
 				'CAREER'=>"EDU_OCC",
 				'RELIGION'=>"PROFILE_RELIGION",
 				'ASTRO'=>"UPLOAD_HOROSCOPE",
