94c86c00a5d69a0b968e80fc83fcb51f6870a35d
richa.arora <ankita.g@99acres.com>
2016-06-14 13:06:37 +0530

    JSC-1548

---
+++ web/js/jspc/chat/chatHelper_js.js
@@ -0,0 +1,19 @@
+function initiateChat()
+{
+      converse.initialize({
+            bosh_service_url: 'http://localhost:7070/http-bind/', // Please use this connection manager only for testing purposes
+            keepalive: true,
+            message_carbons: true,
+            play_sounds: true,
+            roster_groups: true,
+            hide_offline_users: true,
+            debug:true,
+            //prebind:true,
+            jid:"a1@localhost",
+            //sid:sid,
+            //rid:1,
+            authentication:'prebind',
+            show_controlbox_by_default: true,
+            prebind_url: 'http://localhost/api/v1/c/getNotification?jid=a1@localhost',  
+      });
+}
