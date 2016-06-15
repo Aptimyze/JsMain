a88590fd34591e7554ac4a48314a57a0a73967ec
ankita.g <ankita.g@jeevansathi.com>
2016-06-15 15:31:06 +0530

    JSC-1548

--- web/js/jspc/chat/chatHelper_js.js
+++ web/js/jspc/chat/chatHelper_js.js
@@ -5,19 +5,21 @@
 function initiateChat()
 {
       converse.initialize({
-            bosh_service_url: 'http://localhost:7070/http-bind/', // Please use this connection manager only for testing purposes
+            bosh_service_url: 'ws://localhost:7070/ws/', // Please use this connection manager only for testing purposes
             keepalive: true,
             message_carbons: true,
             play_sounds: true,
             roster_groups: true,
-            hide_offline_users: true,
+            hide_offline_users: false,
             debug:true,
             //prebind:true,
+            auto_login:true,
             jid:"a1@localhost",
+            password:"123",
             //sid:sid,
             //rid:1,
-            authentication:'prebind',
+            authentication:'login',
             show_controlbox_by_default: true,
-            prebind_url: 'http://localhost/api/v1/chat/authenticateChatSession?jid=a1@localhost',  
+            //prebind_url: 'http://localhost/api/v1/chat/authenticateChatSession?jid=a1@localhost',  
       });
 }
