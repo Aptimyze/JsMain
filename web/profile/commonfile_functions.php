20c8824d72910328fbfa8b98a56aef3655a773a5
avneet.bindra <color.ui>
2016-06-20 15:37:57 +0530

    Change commonfile function version

--- web/profile/commonfile_functions.php
+++ web/profile/commonfile_functions.php
@@ -487,7 +487,7 @@ $js_arr[]=array("timetracker_js"   => "1");
 $js_arr[]=array("hamburger_js"   => "1");
 $js_arr[]=array("resetPass_js"   => "1");
 $js_arr[]=array("jsms/search/viewSimilarMax"     => "4");
-$js_arr[]=array("helpWidget_js"           => "8");
+$js_arr[]=array("helpWidget_js"           => "9");
 $js_arr[]=array("advance_search"   => "1");
 $js_arr[]=array("jsms/search/overlayLayerJsms"      => "3");
 $js_arr[]=array("jsms/search/savesearch_js"     => "2");
