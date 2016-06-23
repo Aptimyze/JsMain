46fdb6d85c9b783e9da28186da54c442c2c2e9d9
sanyam.chopra <sanyam.chopra@99acres.com>
2016-06-23 19:04:19 +0530

    JSM-2552 code review changes in apieditdppv1Action.class.php

--- apps/jeevansathi/modules/profile/actions/apieditdppv1Action.class.php
+++ apps/jeevansathi/modules/profile/actions/apieditdppv1Action.class.php
@@ -320,15 +320,31 @@ class apieditdppv1Action extends sfAction
 					}
 					
 				}
-				foreach($cityArr as $key=>$value)
-				{	
-					if(!in_array(substr($value,0,2),$stateArr))
-					{
-						$cityString .= $value.",";
+				if(is_array($cityArr))
+				{
+					foreach($cityArr as $key=>$value)
+					{	
+						if(!in_array(substr($value,0,2),$stateArr))
+						{
+							$cityString .= $value.",";
+						}
 					}
+					$arrOut["P_CITY"] = rtrim($cityString,",");
 				}
-				$arrOut["P_CITY"] = rtrim($cityString,",");
-				$arrOut['P_STATE'] = implode(",",$stateArr);
+				else
+				{
+					$arrOut["P_CITY"] = "";
+				}
+				if(is_array($stateArr))
+				{
+					$arrOut['P_STATE'] = implode(",",$stateArr);	
+				}
+				else
+				{
+					$arrOut["P_STATE"] = "";
+				}
+
+				//For AP Users. CITY_INDIA should not be set to null in case of AP users
 				if(!in_array("T", explode(",", $this->m_objLoginProfile->getSUBSCRIPTION())))
 				{
 					$arrOut['CITY_INDIA'] = NULL;	
@@ -339,6 +355,7 @@ class apieditdppv1Action extends sfAction
 				$this->m_bDppUpdate = true;
 				if(strpos($val,'51') !== false)
 				{
+					//For AP Users. CITY_INDIA should not be set to null in case of AP users
 					if(!in_array("T", explode(",", $this->m_objLoginProfile->getSUBSCRIPTION())))
 					{
 						$arrOut['CITY_INDIA'] = NULL;	
