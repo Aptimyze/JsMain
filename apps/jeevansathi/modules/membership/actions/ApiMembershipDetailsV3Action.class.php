2d0b41a20448adadb3fee0078874f8e86bb3f2a8
avneet.bindra <color.ui>
2016-06-29 13:40:50 +0530

    JSC-1671

--- apps/jeevansathi/modules/membership/actions/ApiMembershipDetailsV3Action.class.php
+++ apps/jeevansathi/modules/membership/actions/ApiMembershipDetailsV3Action.class.php
@@ -15,7 +15,6 @@ class ApiMembershipDetailsV3Action extends sfAction
         $apiResponseHandlerObj = ApiResponseHandler::getInstance();
         if($this->apiParams->processPayment){
             $this->apiParams = $memResponseHandlerObj->processPaymentAndRedirect($request,$this->apiParams);
-            //print_r($this->apiParams); die;
         }
         else{
             $this->apiResponse = $memResponseHandlerObj->generateResponseData($request);
@@ -25,38 +24,44 @@ class ApiMembershipDetailsV3Action extends sfAction
                 $this->apiParams->device = "JSAA_mobile_website";
             }
             if ($this->apiParams->processPayment) {
-                try {
-                	switch ($this->apiParams->pageRedirectTo) { 
-	                    case 'ccavenue':
-	                        JeevansathiGatewayManager::setCCAVENUEParams($this,$this->apiParams);
-	                        break;
+                if (empty($this->apiParams->profileid) || !is_numeric($this->apiParams->profileid)) {
+                    $this->setTemplate("paramsError");
+                } else {
+                    try {
+                        switch ($this->apiParams->pageRedirectTo) { 
+                            case 'ccavenue':
+                                JeevansathiGatewayManager::setCCAVENUEParams($this,$this->apiParams);
+                                break;
 
-	                    case 'payu':
-	                        JeevansathiGatewayManager::setPayUParams($this,$this->apiParams);
-	                        break;
+                            case 'payu':
+                                JeevansathiGatewayManager::setPayUParams($this,$this->apiParams);
+                                break;
 
-	                    case 'paytm':
-	                        JeevansathiGatewayManager::setPayTMParams($this,$this->apiParams);
-	                        break;
+                            case 'paytm':
+                                JeevansathiGatewayManager::setPayTMParams($this,$this->apiParams);
+                                break;
 
-	                    case 'paypal':
-	                        JeevansathiGatewayManager::setPaypalParams($this,$this->apiParams);
-	                        break;
+                            case 'paypal':
+                                JeevansathiGatewayManager::setPaypalParams($this,$this->apiParams);
+                                break;
 
-	                    default:
-	                        $this->setTemplate("apiPaymentRedirect");
-	                        break;
-	                
-	                }
-	            } catch (Exception $e) {
-	            	SendMail::send_email('avneet.bindra@jeevansathi.com, vibhor.garg@jeevansathi.com', $e, 'Failure in Setting Gateway Parameters', 'js-sums@jeevansathi.com', 'avneetbindra180691@gmail.com', '', '', '', '', '', '', '', 'Membership Alerts');
-	            }
-	            try {
-                	JeevansathiGatewayManager::reAuthenticateUser($this);
-	            } catch (Exception $e) {
-	            	SendMail::send_email('avneet.bindra@jeevansathi.com, vibhor.garg@jeevansathi.com', $e, 'Failure in User Re-Authentication Function', 'js-sums@jeevansathi.com', 'avneetbindra180691@gmail.com', '', '', '', '', '', '', '', 'Membership Alerts');
-	            }
-	            return;
+                            default:
+                                $this->setTemplate("apiPaymentRedirect");
+                                break;
+                        
+                        }
+                    } catch (Exception $e) {
+                        $serverStr = "\n<br>\n<br>\n<br>".json_encode($_SERVER);
+                        SendMail::send_email('avneet.bindra@jeevansathi.com, vibhor.garg@jeevansathi.com', $e.$serverStr, 'Failure in Setting Gateway Parameters', 'js-sums@jeevansathi.com', 'avneetbindra180691@gmail.com', '', '', '', '', '', '', '', 'Membership Alerts');
+                    }
+                    try {
+                        JeevansathiGatewayManager::reAuthenticateUser($this);
+                    } catch (Exception $e) {
+                        $serverStr = "\n<br>\n<br>\n<br>".json_encode($_SERVER);
+                        SendMail::send_email('avneet.bindra@jeevansathi.com, vibhor.garg@jeevansathi.com', $e.$serverStr, 'Failure in User Re-Authentication Function', 'js-sums@jeevansathi.com', 'avneetbindra180691@gmail.com', '', '', '', '', '', '', '', 'Membership Alerts');
+                    }
+                }
+                return;
             } 
             elseif ($this->apiResponse == "logout_case") {
                 $apiResponseHandlerObj->setHttpArray(ResponseHandlerConfig::$LOGOUT_PROFILE);
@@ -65,7 +70,7 @@ class ApiMembershipDetailsV3Action extends sfAction
                 $apiResponseHandlerObj->setHttpArray(ResponseHandlerConfig::$SUCCESS);
                 $apiResponseHandlerObj->setResponseBody($this->apiResponse);
             }
-        } 
+        }
         else {
             $apiResponseHandlerObj->setHttpArray(ResponseHandlerConfig::$FAILURE);
         }
@@ -74,16 +79,17 @@ class ApiMembershipDetailsV3Action extends sfAction
         }
         if ($request->getParameter('INTERNAL') == 1 && empty($this->apiParams->processCallback)) {
             return sfView::NONE;
-        } 
+        }
         else {
             if ($this->apiResponse) {
                 die;
             } else {
-            	$allParams = $request->getParameterHolder()->getAll();
-            	$stringParams = json_encode($allParams);
-            	SendMail::send_email('avneet.bindra@jeevansathi.com, vibhor.garg@jeevansathi.com', $stringParams, 'Failure in Unknown Case', 'js-sums@jeevansathi.com', 'avneetbindra180691@gmail.com', '', '', '', '', '', '', '', 'Membership Alerts');
-            	return sfView::NONE;
-            	die;
+                $allParams = $request->getParameterHolder()->getAll();
+                $stringParams = json_encode($allParams);
+                $serverStr = "\n<br>\n<br>\n<br>".json_encode($_SERVER);
+                SendMail::send_email('avneet.bindra@jeevansathi.com, vibhor.garg@jeevansathi.com', $stringParams.$serverStr, 'Failure in Unknown Case', 'js-sums@jeevansathi.com', 'avneetbindra180691@gmail.com', '', '', '', '', '', '', '', 'Membership Alerts');
+                return sfView::NONE;
+                die;
             }
         }
     }
