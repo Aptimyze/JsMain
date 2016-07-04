924f6c9b133288e3e9efb1068fec2c51c16755f2
Pankaj Khandelwal <pankaj.khandelwal@jeevansathi.com>
2016-07-04 18:52:01 +0530

    Roster api

--- apps/jeevansathi/modules/chat/actions/actions.class.php
+++ apps/jeevansathi/modules/chat/actions/actions.class.php
@@ -166,10 +166,12 @@ class chatActions extends sfActions
 	    $limit = $request->getParameter("limit");
 	    $getRosterDataObj = new GetRosterData($profileid);
 
-	    $getData = $getRosterDataObj->getRosterDataByType($type,$limit);
-
-	    
-	    
+	    $getData["profiles"] = $getRosterDataObj->getRosterDataByType($type,$limit);
+	    $apiResponseHandlerObj = ApiResponseHandler::getInstance();
+	    $apiResponseHandlerObj->setHttpArray(ResponseHandlerConfig::$SUCCESS);
+	    $apiResponseHandlerObj->setResponseBody($getData);
+	    $apiResponseHandlerObj->generateResponse();
+	    die;
     }
 }
 ?>
