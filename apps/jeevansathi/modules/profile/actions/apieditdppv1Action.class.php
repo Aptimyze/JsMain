8fce76ab7a58730dd44a85d192228b53cf70566d
lavesh <ankit.shukla@jeevansathi.com>
2016-07-05 18:49:12 +0530

    JSM-2586 audit profile flow removed

--- apps/jeevansathi/modules/profile/actions/apieditdppv1Action.class.php
+++ apps/jeevansathi/modules/profile/actions/apieditdppv1Action.class.php
@@ -159,7 +159,7 @@ class apieditdppv1Action extends sfAction
 		$request = sfContext::getInstance()->getRequest();		
 		$this->m_objLoginProfile->getDetail($this->profileId,"","*","RAW");
 		
-		if(in_array("T", explode(",", $this->m_objLoginProfile->getSUBSCRIPTION()))) 
+		/*if(in_array("T", explode(",", $this->m_objLoginProfile->getSUBSCRIPTION()))) 
 			$userType = UserType::AP_USER;
 		
 		if ($userType == UserType::AP_EXECUTIVE) 
@@ -225,7 +225,7 @@ class apieditdppv1Action extends sfAction
 			}
 		} 
 		else
-		{
+		{*/
 			if($scase)
 				$scase.= ",DPP='E',DATE=now()";
 			
@@ -244,7 +244,7 @@ class apieditdppv1Action extends sfAction
 			$jpartnerEditLog = new JpartnerEditLog();
 			$param["fromBackend"] = $fromBackend;
 			$jpartnerEditLog->logDppEdit($jpartnerObj,$this->dppUpdateArray,$param);
-		}
+		//}
 		
 		//If profile's Source is ofl_prof Then do following
 		if (strtolower($this->m_objLoginProfile->getSOURCE()) == "ofl_prof") 
