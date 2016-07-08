6eea973a59126c84c9adaf6cd43ff79b29455c92
bhavana <bhavana.kadwal@jeevansathi.com>
2016-07-08 14:17:36 +0530

    JSM-NATIVE, JSM-2570

--- crontabs/swap_jprofile.php
+++ crontabs/swap_jprofile.php
@@ -228,7 +228,7 @@ while($row=mysql_fetch_row($res))
         $nativePlace    =mysql_fetch_array($dataNativePlace);
         if(!empty($nativePlace)){
                 
-                if($nativePlace["NATIVE_CITY"] == "" || $nativePlace["NATIVE_CITY"] == null || $nativePlace["NATIVE_CITY"] == 0)
+                if($nativePlace["NATIVE_CITY"] == "" || $nativePlace["NATIVE_CITY"] == null || $nativePlace["NATIVE_CITY"] == '0')
                         $nativePlace["NATIVE_CITY"] = $nativePlace["NATIVE_STATE"]."000";
                 
                 $sql_update="UPDATE SWAP SET NATIVE_STATE = '".$nativePlace["NATIVE_STATE"]."', NATIVE_CITY = '".$nativePlace["NATIVE_CITY"]."' WHERE PROFILEID=$profileid";
