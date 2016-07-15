90fe146192a4a751a77ba540ff6c17be6ca69135
ankita.g <ankita.g@jeevansathi.com>
2016-07-15 15:51:30 +0530

    JSPC_Chat

--- web/js/jspc/chat/chatPCHelper_js.js
+++ web/js/jspc/chat/chatPCHelper_js.js
@@ -27,12 +27,12 @@ function initiateChatConnection()
 {
     var username = loggedInJspcUser+'@localhost';
     
-    /* var username = 'a1@localhost';
+    /*var username = 'a1@localhost';
     if(readSiteCookie("CHATUSERNAME")=="bassi")
         username = 'a8@localhost';
     else if(readSiteCookie("CHATUSERNAME")=="ZZTY8164")
-        username = 'a2@localhost';*
-    
+        username = 'a2@localhost';
+    */
     console.log("Nitish"+username);
 
     console.log(chatConfig.Params[device].bosh_service_url);
