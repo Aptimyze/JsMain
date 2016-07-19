285d0cd0b36d824b4cba1934347b22be347bb08d
Lavesh Rawat <lavesh.rawat@jeevansathi.com>
2016-07-19 12:50:55 +0530

    JSM-2674 : PictureApi For Chat

--- apps/jeevansathi/modules/social/actions/GetMultiUserPhotoV1Action.class.php
+++ apps/jeevansathi/modules/social/actions/GetMultiUserPhotoV1Action.class.php
@@ -10,33 +10,47 @@ class GetMultiUserPhotoV1Action extends sfActions
 { 
 	public function execute($request)
 	{
+		/*
 		$pidArr["PROFILEID"] ='5547372,8914646,8953994,1,2,3,4';
 		$photoType = 'MainPicUrl';
-		$profileObj=LoggedInProfile::getInstance('newjs_master');
-	        $multipleProfileObj = new ProfileArray();
+		*/
+		$pidArr["PROFILEID"] =  $request->getParameter("pid");
+		$photoType =  $request->getParameter("photoType");
 
-		$pidArrTemp = explode(",",$pidArr["PROFILEID"]);
-
- 	        $profileDetails = $multipleProfileObj->getResultsBasedOnJprofileFields($pidArr);
-
-                $multiplePictureObj = new PictureArray($profileDetails);
-		$photosArr = $multiplePictureObj->getProfilePhoto();
-
-		foreach($pidArrTemp as $profileId)
+		$inputValidateObj = ValidateInputFactory::getModuleObject($request->getParameter("moduleName"));
+		$inputValidateObj->validateGetMultiUserPhotoV1Action($request);
+		$finalArr = $inputValidateObj->getResponse();
+		if($finalArr["statusCode"]==ResponseHandlerConfig::$SUCCESS["statusCode"])
 		{
-			$photoObj = $photosArr[$profileId];
-			if($photoObj)
+			$profileObj=LoggedInProfile::getInstance('newjs_master');
+			$multipleProfileObj = new ProfileArray();
+
+			$pidArrTemp = explode(",",$pidArr["PROFILEID"]);
+
+			$profileDetails = $multipleProfileObj->getResultsBasedOnJprofileFields($pidArr);
+
+			$multiplePictureObj = new PictureArray($profileDetails);
+			$photosArr = $multiplePictureObj->getProfilePhoto();
+
+			foreach($pidArrTemp as $profileId)
 			{
-				eval('$temp =$photoObj->get'.$photoType.'();');
-				$finalArr[$profileId]['PHOTO'] = $temp;
-				unset($temp);
-			}
-			else
-			{
-				$finalArr[$profileId]['PHOTO'] = '';
+				$photoObj = $photosArr[$profileId];
+				if($photoObj)
+				{
+					$photoTypeArr = explode(",",$photoType);
+					foreach($photoTypeArr as $k=>$v)
+					{
+						eval('$temp =$photoObj->get'.$v.'();');
+						$finalArr[$profileId]['PHOTO'][$v] = $temp;
+						unset($temp);
+					}
+				}
+				else
+				{
+					$finalArr[$profileId]['PHOTO'] = '';
+				}
 			}
 		}
-
 		$respObj = ApiResponseHandler::getInstance();
 		$respObj->setHttpArray(ResponseHandlerConfig::$SUCCESS);
 		$respObj->setResponseBody($finalArr);
