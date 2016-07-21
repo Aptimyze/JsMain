bc5f55bc841e77d14e08b730dd42e81dc6aa5876
nitesh <nitesh.s@jeevansathi.com>
2016-07-21 11:15:00 +0530

    JSI-1937 bug for monthlyhousekeeping cron

--- lib/model/store/newjs_MESSAGE_LOG.class.php
+++ lib/model/store/newjs_MESSAGE_LOG.class.php
@@ -1525,5 +1525,37 @@ return $result;
 				throw new jsException($e);
 			}
 		}
+		
+		public function getMessageLogHousekeeping($profileId1,$profileId2)
+	{
+		
+		
+		try 
+			{
+				if(!$profileId1 || !$profileId2)
+				{
+					throw new jsException("","profile id is not specified in function getMessageLogHousekeeping of newjs_MESSAGE_LOG.class.php");
+				}
+				else
+				{
+					$sql="SELECT ID FROM newjs.MESSAGE_LOG WHERE SENDER IN (:PROFILEID1,:PROFILEID2) AND RECEIVER IN (:PROFILEID1,:PROFILEID2)";
+					$prep=$this->db->prepare($sql);
+					$prep->bindValue(":PROFILEID1",$profileId1,PDO::PARAM_INT);
+					$prep->bindValue(":PROFILEID2",$profileId2,PDO::PARAM_INT);
+					$prep->execute();
+					while($row = $prep->fetch(PDO::FETCH_ASSOC))
+					{
+						$output[] = $row;
+					}
+					return $output;
+				}	
+			}
+			catch(PDOException $e)
+			{
+				jsCacheWrapperException::logThis($e);
+				/*** echo the sql statement and error message ***/
+				throw new jsException($e);
+			}
+	}
 }
 	?>
