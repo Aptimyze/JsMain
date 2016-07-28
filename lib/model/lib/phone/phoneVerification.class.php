4c262593b586a2c2f12c227406c56c1016c97097
nitesh <nitesh.s@jeevansathi.com>
2016-07-28 15:44:20 +0530

    JSI-2046

--- lib/model/lib/phone/phoneVerification.class.php
+++ lib/model/lib/phone/phoneVerification.class.php
@@ -69,6 +69,7 @@ public function __construct($profileObject='',$phoneType)
         public function getPhone(){return $this->phone;}
         public function getPhoneWithoutIsd(){return $this->phoneWithoutIsd;}
         public function isVerified(){return $this->isVerified;}
+        public function getIsd(){return $this->isd;}
 
 protected function phoneUpdateProcess($message)
 	{
@@ -123,8 +124,15 @@ protected function phoneUpdateProcess($message)
            	$jsadminObj=new jsadmin_OFFLINE_MATCHES();
            	$jsadminObj->updateCategory($profileid,'6');
 
-			$phoneLogObj= new PHONE_VERIFIED_LOG();
-			$phoneLogObj->insertEntry($profileid,$this->getPhoneType(), $this->getPhone(),$message);
+		//	$phoneLogObj= new PHONE_VERIFIED_LOG();
+			$id=$verifiedLogObj->insertEntry($profileid,$this->getPhoneType(), $this->getPhone(),$message);
+             
+             //OTP LOGGING
+             $otpLogObj =new MIS_OTP_LOG();
+             $channel=MobileCommon::getChannel();
+             if(!$channel)
+				$channel='M';
+             $otpLogObj->insertEntry($id,$this->getPhoneType(),$this->getPhone(),$this->getIsd(),$channel);
             
 
 			JsMemcache::getInstance()->set($profileid."_PHONE_VERIFIED",'Y');
---
+++ lib/model/store/MIS/MIS_OTP_LOG.class.php
@@ -0,0 +1,40 @@
+<?php
+class MIS_OTP_LOG extends TABLE{
+        public function __construct($dbname="")
+        {
+                        parent::__construct($dbname);
+        }
+
+	
+
+public function insertEntry($id,$phoneType,$phoneNum,$isd,$channel){
+               
+                if(!$id||!$phoneType||!$phoneNum||!$isd || !$channel)
+                        throw new jsException("","one or more of the arguements IS BLANK IN insert() OF MIS_OTP_LOG.class.php");
+
+                try
+                {
+
+                    $sql = "INSERT INTO  `OTP_LOG` (  `ID` ,  `DATE` ,  `PHONE_NO` ,  `CHANNEL` ,  `ISD` ) 
+							VALUES ( :ID,now(),:PHONE,:CHANNEL,:ISD)";
+                    $res = $this->db->prepare($sql);    
+                    $res->bindValue(":ID", $id, PDO::PARAM_INT);            
+                    $res->bindValue(":PHONE", $phoneNum, PDO::PARAM_STR);            
+                    $res->bindValue(":USERNAME", '', PDO::PARAM_STR);            
+                    $res->bindValue(":CHANNEL", $channel, PDO::PARAM_STR);            
+                    $res->bindValue(":ISD", $isd, PDO::PARAM_STR);   
+                    $res->execute();
+                    //return $this->db->lastInsertId();
+                }
+                catch(PDOException $e)
+                {
+					jsCacheWrapperException::logThis($e);
+                        /*** echo the sql statement and error message ***/
+                        //throw new jsException($e);
+                }
+    }
+
+
+
+}
+?>
