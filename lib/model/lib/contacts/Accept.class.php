e7e4766c422cd185c87a51814fc3e8cb6971c88f
Pankaj Khandelwal <pankaj.khandelwal@jeevansathi.com>
2016-07-28 15:03:10 +0530

    change viewer to viewed

--- lib/model/lib/contacts/Accept.class.php
+++ lib/model/lib/contacts/Accept.class.php
@@ -112,7 +112,7 @@ class Accept extends ContactEvent
         $gcmData=array('process'=>'GCM','data'=>array('type'=>'ACCEPTANCE','body'=>array('receiverid'=>$receiver->getPROFILEID(),'senderid'=>$sender->getPROFILEID() ) ), 'redeliveryCount'=>0 );
         $producerObj->sendMessage($gcmData);
 	//Add to acceptance roster
-      $chatData = array('process' => 'CHATROSTERS', 'data' => array('type' => 'ACCEPTANCE', 'body' => array('sender' => array('profileid'=>$this->contactHandler->getViewer()->getPROFILEID(),'checksum'=>JsAuthentication::jsEncryptProfilechecksum($this->contactHandler->getViewer()->getPROFILEID()),'username'=>$this->contactHandler->getViewer()->getUSERNAME()), 'receiver' => array('profileid'=>$this->contactHandler->getViewed()->getPROFILEID(),'checksum'=>JsAuthentication::jsEncryptProfilechecksum($this->contactHandler->getViewer()->getPROFILEID),"username"=>$this->contactHandler->getViewer()->getUSERNAME()))), 'redeliveryCount' => 0);
+      $chatData = array('process' => 'CHATROSTERS', 'data' => array('type' => 'ACCEPTANCE', 'body' => array('sender' => array('profileid'=>$this->contactHandler->getViewer()->getPROFILEID(),'checksum'=>JsAuthentication::jsEncryptProfilechecksum($this->contactHandler->getViewer()->getPROFILEID()),'username'=>$this->contactHandler->getViewer()->getUSERNAME()), 'receiver' => array('profileid'=>$this->contactHandler->getViewed()->getPROFILEID(),'checksum'=>JsAuthentication::jsEncryptProfilechecksum($this->contactHandler->getViewed()->getPROFILEID),"username"=>$this->contactHandler->getViewed()->getUSERNAME()))), 'redeliveryCount' => 0);
       $producerObj->sendMessage($chatData);
         
     }
--- lib/model/lib/contacts/CancelContact.class.php
+++ lib/model/lib/contacts/CancelContact.class.php
@@ -92,7 +92,7 @@ class CancelContact extends ContactEvent{
         $sendMailData = array('process' =>'MAIL','data'=>array('type' => 'CANCELCONTACT','body'=>array('senderid'=>$sender->getPROFILEID(),'receiverid'=>$receiver->getPROFILEID() ) ), 'redeliveryCount'=>0 );
         $producerObj->sendMessage($sendMailData);
 	//Remove from contact roster
-      $chatData = array('process' => 'CHATROSTERS', 'data' => array('type' => 'CANCELCONTACT', 'body' => array('sender' => array('profileid'=>$this->contactHandler->getViewer()->getPROFILEID(),'checksum'=>JsAuthentication::jsEncryptProfilechecksum($this->contactHandler->getViewer()->getPROFILEID()),'username'=>$this->contactHandler->getViewer()->getUSERNAME()), 'receiver' => array('profileid'=>$this->contactHandler->getViewed()->getPROFILEID(),'checksum'=>JsAuthentication::jsEncryptProfilechecksum($this->contactHandler->getViewer()->getPROFILEID),"username"=>$this->contactHandler->getViewer()->getUSERNAME()))), 'redeliveryCount' => 0);
+      $chatData = array('process' => 'CHATROSTERS', 'data' => array('type' => 'CANCELCONTACT', 'body' => array('sender' => array('profileid'=>$this->contactHandler->getViewer()->getPROFILEID(),'checksum'=>JsAuthentication::jsEncryptProfilechecksum($this->contactHandler->getViewer()->getPROFILEID()),'username'=>$this->contactHandler->getViewer()->getUSERNAME()), 'receiver' => array('profileid'=>$this->contactHandler->getViewed()->getPROFILEID(),'checksum'=>JsAuthentication::jsEncryptProfilechecksum($this->contactHandler->getViewed()->getPROFILEID),"username"=>$this->contactHandler->getViewed()->getUSERNAME()))), 'redeliveryCount' => 0);
       $producerObj->sendMessage($chatData);
     }
     else
