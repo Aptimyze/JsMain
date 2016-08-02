2cf5459048819b91c0ac7ad6af84d3421ca4608d
nitish.sharma <nitish.sharma@99acres.com>
2016-08-02 17:41:13 +0530

    JSPC_Chat logout on site logout

--- web/js/jspc/common/commonJspc_js.js
+++ web/js/jspc/common/commonJspc_js.js
@@ -598,6 +598,7 @@ function sendAjaxHtmlDisplay(ajaxConfig, fun) {
 }
 
 function logOutCheck(param,upgradeFromTopNavBar){
+    $(".jschatLogOut").click();
     if(top.logOut) 
         top.logOut(); 
     if(top.profileId) 
