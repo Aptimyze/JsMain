84f3efa77d687adc206f7d328a4e9bc1663c61a2
Palash Chordia <palashc2011@gmail.com>
2016-08-05 16:58:34 +0530

    JSI-2106

---
+++ web/sql_builds/JSI-2106.sql
@@ -0,0 +1,9 @@
+use newjs;
+UPDATE  `SMS_TYPE` SET  `MESSAGE` =  'A profile has been registered on Jeevansathi.com with your phone nos. In case you have not registered it, then please delete it here {DELETE_URL}' WHERE  `SMS_KEY` =  'REGISTER_CONFIRM' LIMIT 1 ;
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
