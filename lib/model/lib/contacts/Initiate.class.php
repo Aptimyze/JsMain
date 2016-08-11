08b3b40d1ee241470e4b0bdc03ecfa8327d5999f
Palash Chordia <palashc2011@gmail.com>
2016-08-11 12:02:55 +0530

    JSI-2021

--- lib/model/lib/contacts/Initiate.class.php
+++ lib/model/lib/contacts/Initiate.class.php
@@ -247,8 +247,11 @@ class Initiate extends ContactEvent{
         $this->contactHandler->getContactObj()->insertContact();
         $action = FTOStateUpdateReason::EOI_SENT;
         $this->contactHandler->getViewer()->getPROFILE_STATE()->updateFTOState($this->viewer, $action);
-        if(sfContext::getInstance()->getRequest()->getParameter('fromJSMS_MYJS')==1)$this->viewerMemcacheObject->setMatchAlertData();
-
+        if(sfContext::getInstance()->getRequest()->getParameter('fromJSMS_MYJS')==1)
+        {
+            $this->viewerMemcacheObject->update("MATCHALERT_TOTAL",-1,$this->optionalFlag);
+            $this->viewerMemcacheObject->setMATCHALERT(0);
+        }
       }
       
 		//curl for analytics team by Nitesh for Lavesh team
