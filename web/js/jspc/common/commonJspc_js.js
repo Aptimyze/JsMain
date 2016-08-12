b2b8f86cca99a78c90ba7431283004ec232f3cc3
Palash Chordia <palashc2011@gmail.com>
2016-08-12 14:28:25 +0530

    JSI-2135

--- web/js/jspc/common/commonJspc_js.js
+++ web/js/jspc/common/commonJspc_js.js
@@ -379,14 +379,14 @@ function setBellCountHTML(data) {
                 $("#totalBellCount").text("9+");
             }
         }
-        /*if (parseInt(data.NEW_MATCHES)) {
+        if (parseInt(data.NEW_MATCHES)) {
             $("#justJoinedCountParent").css('display', 'block');
             if (data.NEW_MATCHES < 10) {
                 $("#justJoinedCount").text(data.NEW_MATCHES);
             } else {
                 $("#justJoinedCount").text("9+");
             }
-        }*/
+        }
         if (parseInt(data.MESSAGE_NEW)) {
             $("#messagesCountParent").css('display', 'block');
             if (data.MESSAGE_NEW < 10) {
@@ -419,14 +419,14 @@ function setBellCountHTML(data) {
                 $("#membersAcceptedMeCount").text("9+");
             }
         }
-        /*if (parseInt(data.DAILY_MATCHES_NEW)) {
+        if (parseInt(data.DAILY_MATCHES_NEW)) {
             $("#membersDailyMatchesCountParent").css('display', 'block');
             if (data.DAILY_MATCHES_NEW < 10) {
                 $("#membersDailyMatchesCount").text(data.DAILY_MATCHES_NEW);
             } else {
                 $("#membersDailyMatchesCount").text("9+");
             }
-        }*/
+        }
 	if (parseInt(data.FILTERED_NEW)) {
             $("#membersFilteredInterestCountParent").css('display', 'block');
             if (data.FILTERED_NEW < 10) {
