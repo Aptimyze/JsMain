4c30ef55c3f2c9a36a61c678320df77acfa01f24
ankita.g <ankita.g@jeevansathi.com>
2016-08-12 16:29:30 +0530

    JSPC_Chat

--- web/js/jspc/chat/chatPlugin_js.js
+++ web/js/jspc/chat/chatPlugin_js.js
@@ -1812,22 +1812,24 @@ JsChat.prototype = {
     _disableChatPanelsBox:function(userId){
         var curElem = this;
         if($('chat-box[user-id="' + userId + '"]').length != 0){
-            setTimeout(function(){
-                var found = false;
-                $.each(curElem._rosterGroups,function(key,groupId){
-                    if($(".chatlist li[id='" + userId + "_" + groupId + "']").length != 0){
-                        found = true;
-                    }
-                    if(found == false){
-                        if($('chat-box[user-id="' + userId + '"]').length != 0){
-                            $('chat-box[user-id="' + userId + '"] .chatMessage').html("");
-                            if($('chat-box[user-id="' + userId + '"] #rosterDeleteMsg_'+ userId + '').length == 0)
-                                $('chat-box[user-id="' + userId + '"] .chatMessage').append('<div id="rosterDeleteMsg_'+userId+'" class="pt20 txtc color5">'+curElem._rosterDeleteChatBoxMsg+'</div>');
-                            $('chat-box[user-id="' + userId + '"] textarea').prop("disabled", true);
+            if($('chat-box[user-id="' + userId + '"] .chatMessage #undoBlock').length == 0 && $('chat-box[user-id="' + userId + '"] .chatMessage #acceptInterest').length == 0){
+                setTimeout(function(){
+                    var found = false;
+                    $.each(curElem._rosterGroups,function(key,groupId){
+                        if($(".chatlist li[id='" + userId + "_" + groupId + "']").length != 0){
+                            found = true;
                         }
-                    }
-                });  
-            },7000);
+                        if(found == false){
+                            if($('chat-box[user-id="' + userId + '"]').length != 0){
+                                $('chat-box[user-id="' + userId + '"] .chatMessage').html("");
+                                if($('chat-box[user-id="' + userId + '"] #rosterDeleteMsg_'+ userId + '').length == 0)
+                                    $('chat-box[user-id="' + userId + '"] .chatMessage').append('<div id="rosterDeleteMsg_'+userId+'" class="pt20 txtc color5">'+curElem._rosterDeleteChatBoxMsg+'</div>');
+                                $('chat-box[user-id="' + userId + '"] textarea').prop("disabled", true);
+                            }
+                        }
+                    });  
+                },2000);
+            }
         }
     },
 
