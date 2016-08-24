6cd6a578503c940e557d1d36b0b3ce7a5adaa583
Pankaj Khandelwal <pankaj.khandelwal@jeevansathi.com>
2016-08-24 18:17:56 +0530

    send eoi

--- lib/model/store/newjs_MESSAGE_LOG.class.php
+++ lib/model/store/newjs_MESSAGE_LOG.class.php
@@ -635,6 +635,50 @@ public function updateMessageLogDetails($msgCommObj)
 			}
 			return $output;
 		}
+
+	public function getEOIMessagesForChat($receiverProfiles,$senderProfiles)
+	{
+		try{
+			if(!is_array($receiverProfiles) && !is_array($senderProfiles))
+			{
+				throw new jsException("","profile id is not specified in function getEOIMessages of newjs_MESSAGE_LOG.class.php");
+			}
+			$count = 1;
+			$str = " IN (";
+			foreach($senderProfiles as $key1=>$value1)
+			{
+				$str = $str.":VALUE".$count.",";
+				$bindArr["VALUE".$count] = $value1;
+				$count++;
+			}
+			foreach($receiverProfiles as $key1=>$value1)
+			{
+				$str = $str.":VALUE".$count.",";
+				$bindArr["VALUE".$count] = $value1;
+				$count++;
+			}
+			$str = substr($str, 0, -1);
+			$str = $str.")";
+			$strS = " RECEIVER ".$str." AND SENDER ".$str;
+			$sql = "SELECT SQL_CACHE SENDER,RECEIVER,DATE, MESSAGE,MESSAGE_LOG.ID FROM  newjs.`MESSAGE_LOG` LEFT JOIN MESSAGES ON ( MESSAGES.ID = MESSAGE_LOG.ID ) WHERE ".$strS."  AND TYPE = 'I' ORDER BY SENDER,DATE ASC LIMIT 1";
+			$prep=$this->db->prepare($sql);
+			foreach($bindArr as $k=>$v)
+				$prep->bindValue($k,$v);
+			$prep->execute();
+
+			while($row = $prep->fetch(PDO::FETCH_ASSOC))
+			{
+				$output[] = $row;
+			}
+			//print_r($output); die;
+
+		}
+		catch (PDOException $e)
+		{
+			throw new jsException($e);
+		}
+		return $output;
+	}
 		
 		public function getMessageListing($condition,$skipArray)
 		{
