b4ac2e737352657b17e4f9f9893cb151e9d0efb3
Lavesh Rawat <lavesh.rawat@jeevansathi.com>
2016-08-26 16:47:32 +0530

    chat messge interminent issue test code

--- web/js/jspc/chat/strophieWrapper_js.js
+++ web/js/jspc/chat/strophieWrapper_js.js
@@ -132,14 +132,9 @@ var strophieWrapper = {
         //binding event for presence update in roster
         strophieWrapper.connectionObj.addHandler(strophieWrapper.onPresenceReceived, null, 'presence', null);
         //binding event for message receive event
-
-        /** 
-	* we need to delete old Binding and add new Binding bcoz on reload and when user is typing message binding is not reflecting correctly
-	**/
-        if(messageBinding)
+ 	if(messageBinding)
             strophieWrapper.connectionObj.deleteHandler(messageBinding);
         messageBinding = strophieWrapper.connectionObj.addHandler(strophieWrapper.onMessage, null, 'message', null, null, null);
-
         //binding event for new node push in roster
         strophieWrapper.connectionObj.addHandler(strophieWrapper.onRosterUpdate, Strophe.NS.ROSTER, 'iq', 'set');
         //binding event for message receipts
@@ -588,10 +583,7 @@ var strophieWrapper = {
     },
     //sending Message
     sendMessage: function (message, to,is_eoi,msg_id) {
-        /** 
-	* we need to delete old Binding and add new Binding bcoz on reload and when user is typing message binding is not reflecting correctly
-	**/
-        if(messageBinding)
+  	if(messageBinding)
             strophieWrapper.connectionObj.deleteHandler(messageBinding);
         messageBinding = strophieWrapper.connectionObj.addHandler(strophieWrapper.onMessage, null, 'message', null, null, null);
 
