db1bc53d561578970624e907575db217b3c15628
lavesh <ankit.shukla@jeevansathi.com>
2016-08-29 13:18:14 +0530

    file logging changes

--- lib/task/profile/updateNTimesTask.class.php
+++ lib/task/profile/updateNTimesTask.class.php
@@ -6,6 +6,7 @@
 ini_set('memory_limit','256M');
 class updateNTimesTask extends sfBaseTask
 {
+  private $measurePerformance = true;
   protected function configure() {
     $this->namespace = 'profile';
     $this->name = 'updateNTimes';
@@ -22,36 +23,49 @@ EOF;
     if(!sfContext::hasInstance()) {
       sfContext::createInstance($this->configuration);
     }
-    $date = date("Y-m-d h:i:s");
-    $file = fopen(sfConfig::get("sf_upload_dir")."/SearchLogs/nTimesView_".$date.".txt","a");
+    $date = date("Y-m-d");
+    if($this->measurePerformance)
+        $file = fopen(sfConfig::get("sf_upload_dir")."/SearchLogs/nTimesView_".$date.".txt","a");
     $queueArr  = array(0=>'nTimesMaleQueue',1=>'nTimesFemaleQueue');
     foreach($queueArr as $key=>$val){
         
         $memcacheObj = JsMemcache::getInstance();
         $lengthOfQueue = $memcacheObj->getLengthOfQueue($val);
+        
+        //use pipeline  for multiple pops
         $pipeline = $memcacheObj->pipeline();
-        /*for($i=0;$i<100000;$i++)
-            $pipeline->LPUSH($val,rand(10000,30000));
-        $usersList = $pipeline->execute();
-        die;*/
+        
         for($i=0;$i<$lengthOfQueue;$i++)
             $pipeline->RPOP($val);
+        //execute pipeline
         $usersList = $pipeline->execute();
+        
+        //get individual count for every profile
         $countArr = $this->countViewsForProfiles($usersList);
         $jpNtimesObj = new NEWJS_JP_NTIMES();
         $greaterThanOne = 0;
         $count = 0;
-        foreach($countArr as $key1=>$val1){
-          $stringToWrite = "$key1  $val1";  
-          fwrite($file,$stringToWrite."\n");
-          $jpNtimesObj->updateProfileViews($key1,$val1);
+        if(sizeof($countArr)>0){
+            //update table rows for every profile
+            foreach($countArr as $key1=>$val1){
+              if($this->measurePerformance){
+                $stringToWrite = "$key1,$val1";  
+                fwrite($file,$stringToWrite."\n");
+              }
+              $jpNtimesObj->updateProfileViews($key1,$val1);
+            }
         }
         $prevQueries = $lengthOfQueue;
         $currQueries = sizeof($countArr);
-        $stringToWrite = "$val -: \n previousQueries  $prevQueries \n currentQueries  $currQueries";  
-        fwrite($file,$stringToWrite."\n");
+        
+        //log perfirmance in file
+        if($this->measurePerformance){
+            $stringToWrite = "$val -: \n previousQueries  $prevQueries \n currentQueries  $currQueries";  
+            fwrite($file,$stringToWrite."\n");
+        }
     }
-    fclose($file);
+    if($this->measurePerformance)
+        fclose($file);
   }           
   
   protected function countViewsForProfiles($usersList){
