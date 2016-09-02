0e651026d9a6e41190bb03775a90ca60009f0a10
ankita.g <ankita.g@jeevansathi.com>
2016-09-02 16:23:10 +0530

    check consumer logs script

--- crontabs/chat/checkConsumerLogs.php
+++ crontabs/chat/checkConsumerLogs.php
@@ -1,7 +1,7 @@
 <?php
 /*cron to check last updated time of consumer logs and send alert sms*/
-include(JsConstants::$docRoot."/commonFiles/sms_inc.php");
-include_once(JsConstants::$docRoot."/profile/SymfonySearchFunctions.class.php");
+//include(JsConstants::$docRoot."/commonFiles/sms_inc.php");
+//include_once(JsConstants::$docRoot."/profile/SymfonySearchFunctions.class.php");
 
 $logFilesPath = array("/log/consumer1.log"=>array("S1"=>20,"S2"=>10),
 					 "/log/consumer2.log"=>array("S1"=>10,"S2"=>20),
@@ -43,16 +43,17 @@ if(is_array($notUpdatedFiles)){
 	debugLog("---not updated files---");
 	debugLog($notUpdatedFiles);
 	//send alert sms
-	foreach($mobileNumberArr as $k=>$v){
+	/*foreach($mobileNumberArr as $k=>$v){
 		debugLog("sending sms to ".$v);
         $date = date("Y-m-d h");
         $message        = "Mysql Error Count have reached -- logs not updated-- ".implode(",", $notUpdatedFiles);
         
         $from           = "JSSRVR";
         $profileid      = "144111";
-        $smsState = send_sms($message,$from,$v,$profileid,'','Y');
+        $smsState = sendMonitoringSMS($message,$from,$v,$profileid,'','Y');
         debugLog($smsState);
-    }
+    }*/
+    sendAlertMail(implode(",", $notUpdatedFiles));
 }
 
 
@@ -66,5 +67,37 @@ function debugLog($message){
 			echo "\n".$message;
 	}
 }
+
+function sendAlertMail($message){
+	debugLog("sending mail-".$message."\n");
+	mail("lavesh.rawat@gmail.com,pankaj139@gmail.com,nsitankita@gmail.com,nitishpost@gmail.com","Error in chat consumers","Please check the logs- ".$message);
+}
+
+/*function sendMonitoringSMS($message,$from,$mobile,$profileid,$gsm,$table=''){
+	
+
+	$xml_content="";
+	$i = 0;
+	$xml_head="%3C?xml%20version=%221.0%22%20encoding=%22ISO-8859-1%22?%3E%3C!DOCTYPE%20MESSAGE%20SYSTEM%20%22http://127.0.0.1/psms/dtd/message.dtd%22%3E%3CMESSAGE%3E%3CUSER%20USERNAME=%22naukari%22%20PASSWORD=%22na21s8api%22/%3E";
+	$xml_content.="%3CSMS%20UDH=%220%22%20CODING=%221%22%20TEXT=%22$message%22%20PROPERTY=%220%22%20ID=%22$profileid%22%3E%3CADDRESS%20FROM=%22$from%22%20TO=%22$mobile%22%20SEQ=%22$profileid%22%20TAG=%22%22/%3E%3C/SMS%3E";
+													     
+	$xml_end="%3C/MESSAGE%3E";
+	$xml_code=$xml_head.$xml_content.$xml_end;
+	$fd=@fopen("http://api.myvaluefirst.com/psms/servlet/psms.Eservice2?data=$xml_code&action=send","rb");  
+	echo "http://api.myvaluefirst.com/psms/servlet/psms.Eservice2?data=$xml_code&action=send"."\n";                     
+	$response = '';
+	if($fd)
+	{
+		while (!feof($fd))
+		{
+			$response= fread($fd, 4096);
+		}
+		fclose($fd);
+		$ts=time();
+		$today=date('Y-m-d',$ts);
+	}
+	//debugLog($response);
+	return 1;
+}*/
 
 ?>
