c4e02875fd66c544478e67a171e02b5f5ac7a95f
ankita.g <ankita.g@jeevansathi.com>
2016-09-02 16:35:40 +0530

    check consumer logs script

--- crontabs/chat/checkConsumerLogs.php
+++ crontabs/chat/checkConsumerLogs.php
@@ -3,9 +3,15 @@
 //include(JsConstants::$docRoot."/commonFiles/sms_inc.php");
 //include_once(JsConstants::$docRoot."/profile/SymfonySearchFunctions.class.php");
 
-$logFilesPath = array("/log/consumer1.log"=>array("S1"=>20,"S2"=>10),
-					 "/log/consumer2.log"=>array("S1"=>10,"S2"=>20),
-					 "/log/consumer3.log"=>array("S1"=>10,"S2"=>20)
+$logFilesPath = array("/data/projects/logs/consumer_user_acceptance.log"=>array("S1"=>20,"S2"=>10),
+					 "/data/projects/logs/consumer_user_acceptance_sent.log"=>array("S1"=>10,"S2"=>20),
+					 "/data/projects/logs/consumer_user_create.log"=>array("S1"=>10,"S2"=>20),
+					 "/data/projects/logs/consumer_user_intrec.log"=>array("S1"=>10,"S2"=>20),
+					 "/data/projects/logs/consumer_user_intsent.log"=>array("S1"=>10,"S2"=>20),
+					 "/data/projects/logs/consumer_user_shortlist.log"=>array("S1"=>10,"S2"=>20),
+					 "/data/projects/logs/consumer_user_update.log"=>array("S1"=>10,"S2"=>20),
+					 "/data/projects/logs/jscommunication.log"=>array("S1"=>10,"S2"=>20),
+					 "/data/projects/logs/JsMessagingConsumer.log"=>array("S1"=>10,"S2"=>20)
 					 );
 $mobileNumberArr = array("9910244159","9650879575","9818424749","8989931104","9810300513");
 $branchPath = JsConstants::$cronDocRoot;
@@ -23,13 +29,15 @@ debugLog("current time ".date("d m Y H:i:s",$currTime));
 debugLog("shift---".$shift);
 foreach ($logFilesPath as $pathName => $offset) {
 	//check if file exists
-	$checkExists = file_exists(JsConstants::$cronDocRoot.$pathName);
+	//$checkExists = file_exists(JsConstants::$cronDocRoot.$pathName);
+	$checkExists = file_exists($pathName);
 	if($checkExists != false){
-		$stat = stat($branchPath.$pathName);
+		$stat = stat($pathName);
 		$lastUpdatedDiff = $currTime - $stat['mtime'];
 		//if file not updated within specified time
 		if($lastUpdatedDiff > $offset[$shift]){
-			$notUpdatedFiles[$i++] = JsConstants::$cronDocRoot.$pathName;
+			//$notUpdatedFiles[$i++] = JsConstants::$cronDocRoot.$pathName;
+			$notUpdatedFiles[$i++] = $pathName;
 		}
 		clearstatcache();
 		debugLog("File- ".$pathName." last modified at: ".date("d m Y H:i:s",$stat['mtime']));
@@ -58,7 +66,7 @@ if(is_array($notUpdatedFiles)){
 
 
 function debugLog($message){
-	$debug = 0;
+	$debug = 1;
 	if($debug == 1){
 		if(is_array($message)){
 			print_r($message);
