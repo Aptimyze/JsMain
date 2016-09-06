0b4f30314492fe6a17b946fd9ac7aa6ceb05f8f0
vibhor.garg <vibhor.garg@localhost.localdomain>
2016-09-06 13:21:25 +0530

    JSC-1848

--- commonConfig/MysqlDbConstants.class.php
+++ commonConfig/MysqlDbConstants.class.php
@@ -72,6 +72,8 @@ class MysqlDbConstants
         public static $masterDDL=array('HOST'=>'172.16.3.185', 'USER'=>'localuser', 'PASS'=>'Km7Iv80l', 'PORT'=>'3306' , 'DEFAULT_DB'=>'newjs');
         public static $shard1DDL=array('HOST'=>'172.16.3.185', 'USER'=>'localuser', 'PASS'=>'Km7Iv80l', 'PORT'=>'3309' , 'DEFAULT_DB'=>'newjs');
         public static $shard2DDL=array('HOST'=>'172.16.3.185', 'USER'=>'localuser', 'PASS'=>'Km7Iv80l', 'PORT'=>'3306' , 'DEFAULT_DB'=>'newjs');
-        public static $shard3DDL=array('HOST'=>'172.16.3.185', 'USER'=>'localuser', 'PASS'=>'Km7Iv80l', 'PORT'=>'3307' , 'DEFAULT_DB'=>'newjs');              /*end*/
-
+        public static $shard3DDL=array('HOST'=>'172.16.3.185', 'USER'=>'localuser', 'PASS'=>'Km7Iv80l', 'PORT'=>'3307' , 'DEFAULT_DB'=>'newjs');              public static $viewSimilarDDL=array('HOST'=>'172.16.3.185', 'USER'=>'localuser', 'PASS'=>'Km7Iv80l', 'PORT'=>'3307' , 'DEFAULT_DB'=>'newjs');
+        public static $viewLogDDL=array('HOST'=>'172.16.3.185', 'USER'=>'localuser', 'PASS'=>'Km7Iv80l', 'PORT'=>'3307' , 'DEFAULT_DB'=>'newjs');
+        public static $alertsDDL=array('HOST'=>'172.16.3.185', 'USER'=>'localuser', 'PASS'=>'Km7Iv80l', 'PORT'=>'3307' , 'DEFAULT_DB'=>'newjs');
+	/*end*/
 }
