3e9b6eb9666eb5a1fef30a21222ff161eebf9ee4
ankita.g <ankita.g@jeevansathi.com>
2016-09-12 14:33:49 +0530

    JSC-1895 chat

--- web/js/jspc/chat/chatPlugin_js.js
+++ web/js/jspc/chat/chatPlugin_js.js
@@ -867,6 +867,7 @@ JsChat.prototype = {
     _bindMaximize: function (elem, userId) {
         var curElem = this;
         $(elem).off("click").on("click", function () {
+            //console.log("clicked",userId);
             curElem._scrollDown($(".extraPopup"), "retain_extra");
             setTimeout(function () {
                 $(".extraChats").css("padding-top", "0px");
@@ -1952,7 +1953,7 @@ JsChat.prototype = {
             curElem._textAreaAdjust(this);
         });
         $('chat-box[user-id="' + userId + '"] #pic_' + userId).addClass("js-chatBoxPic downBarPic cursp");
-        $('chat-box[user-id="' + userId + '"] .chatBoxBar').append('<div class="downBarText fullhgt"><div class="downBarUserName disp_ib pos-rel f14 colrw wid44p fontlig">' + $(".chatlist li[id='" + userId + "_" + groupId + "'] div").html() + '<div class="onlineStatus f11 opa50 mt4"></div></div><div class="iconBar cursp fr padallf_2 disp_ib"><i class="nchatspr nchatic_3"><div class="pos-abs fullBlockTitle disp-none tneg20 bg-white f13 brderinp pad510">Block</div></i><i class="nchatspr nchatic_2 ml10 mr10"><div class="pos-abs fullMinTitle disp-none tneg20_2 bg-white f13 brderinp pad510">Minimize</div></i><i class="nchatspr nchatic_1 mr10"><div class="pos-abs fullCloseTitle disp-none tneg20_3 bg-white f13 brderinp pad510">Close</div></i></div><div class="pinkBubble2 fr vertM scir disp_ib padall-10 m11"><span class="noOfMessg f13 pos-abs">0</span></div></div>');
+        $('chat-box[user-id="' + userId + '"] .chatBoxBar').append('<div class="downBarText fullhgt js-chatBoxTop"><div class="downBarUserName disp_ib pos-rel f14 colrw wid44p fontlig">' + $(".chatlist li[id='" + userId + "_" + groupId + "'] div").html() + '<div class="onlineStatus f11 opa50 mt4"></div></div><div class="iconBar cursp fr padallf_2 disp_ib"><i class="nchatspr nchatic_3"><div class="pos-abs fullBlockTitle disp-none tneg20 bg-white f13 brderinp pad510">Block</div></i><i class="nchatspr nchatic_2 ml10 mr10"><div class="pos-abs fullMinTitle disp-none tneg20_2 bg-white f13 brderinp pad510">Minimize</div></i><i class="nchatspr nchatic_1 mr10"><div class="pos-abs fullCloseTitle disp-none tneg20_3 bg-white f13 brderinp pad510">Close</div></i></div><div class="pinkBubble2 fr vertM scir disp_ib padall-10 m11"><span class="noOfMessg f13 pos-abs">0</span></div></div>');
         curElem._bindInnerHtml(userId, status);
     },
     //binding innerDiv after creating chatbox
@@ -1962,7 +1963,7 @@ JsChat.prototype = {
         $('chat-box[user-id="' + userId + '"] textarea').prop("disabled", true);
         $('chat-box[user-id="' + userId + '"] .pinkBubble2 span').html("0");
         $("chat-box[user-id='" + userId + "'] .chatBoxBar .onlineStatus").html(status);
-        this._bindMaximize($('chat-box[user-id="' + userId + '"] .chatBoxBar'), userId);
+        this._bindMaximize($('chat-box[user-id="' + userId + '"] .js-chatBoxTop'), userId);
         this._bindMinimize($('chat-box[user-id="' + userId + '"] .nchatic_2'));
         this._bindClose($('chat-box[user-id="' + userId + '"] .nchatic_1'));
         this._bindBlock($('chat-box[user-id="' + userId + '"] .nchatic_3'), userId);
@@ -1973,7 +1974,7 @@ JsChat.prototype = {
     //bind click action on chat box pic click
     _bindChatPoxPicClick: function(userId){
         var curElem = this,chatBoxElem= $('chat-box[user-id="' + userId + '"]');
-        chatBoxElem.find('.js-chatBoxPic').bind("click",function(event){
+        chatBoxElem.on("click",".js-chatBoxPic",function(event){
             event.preventDefault();
             var profilechecksum = chatBoxElem.attr("data-checks"),
             groupID = chatBoxElem.attr("group-id"),
@@ -1987,6 +1988,7 @@ JsChat.prototype = {
                 }
             }
             if(typeof profilechecksum!= "undefined" && profilechecksum){
+                //console.log("view profile");
                 window.location.href = "/profile/viewprofile.php?profilechecksum="+profilechecksum+trackingParamsStr;
             }
         });
