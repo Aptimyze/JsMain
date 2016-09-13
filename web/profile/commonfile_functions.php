d07858d8dbad87018721471b056d7689c6ab35be
bhavana <bhavana.kadwal@jeevansathi.com>
2016-09-13 14:08:29 +0530

    ForceAllow Css/Js

--- web/profile/commonfile_functions.php
+++ web/profile/commonfile_functions.php
@@ -401,7 +401,7 @@ $js_arr[]=array("view_similar_profile_js"    => "2");
 $js_arr[]=array("assisted_product"   => "1");
 $js_arr[]=array("pixel_js"    => "2");
 $js_arr[]=array("mouseClickTracking_js"  => "2");
-$js_arr[]=array("jsms/profile/ep/mobedit_js"         => "10");
+$js_arr[]=array("jsms/profile/ep/mobedit_js"          => "11");
 $js_arr[]=array("jsms/profile/ep/mobShow_name_js"           => "2");
 $js_arr[]=array("jsms/common/disableScroll_js"  => "1");
 $js_arr[]=array("jsms/profile/ep/mobeditview_js"        => "9");
