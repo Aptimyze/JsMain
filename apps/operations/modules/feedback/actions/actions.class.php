6730c9de60adcb39febd1933bf56189190aaef78
Ayush Sethi <ayushsethi22031992@gmail.com>
2016-09-14 11:53:30 +0530

    Interface for Mark Invalid

--- apps/operations/modules/feedback/actions/actions.class.php
+++ apps/operations/modules/feedback/actions/actions.class.php
@@ -32,11 +32,19 @@ class feedbackActions extends sfActions
   	$this->setTemplate('reportAbuse');
 
 }
+
+ public function executeReportInvalid(sfWebRequest $request)
+  {
+    $this->setTemplate('reportInvalid');
+
+}
+
 	
 
 
 	public function executeReportAbuseLog(sfWebRequest $request)
 	{
+
 	   	$startDate=$request->getParameter('RAStartDate');
 	   	$endDate=$request->getParameter('RAEndDate');
 	   	$reportAbuseOb = new REPORT_ABUSE_LOG('newjs_slave');
@@ -66,12 +74,57 @@ class feedbackActions extends sfActions
 			unset($tempArray);
 			# code...
 		  }
-    }
-        echo json_encode($resultArr);
+    
+      echo json_encode($resultArr);
                         return sfView::NONE;
                         die;
                 
 	}
+}
+  public function executeReportInvalidLog(sfWebRequest $request)
+  { 
+      $startDate=$request->getParameter('RAStartDate');
+      $endDate=$request->getParameter('RAEndDate');
+      $reportInvalidOb = new JSADMIN_REPORT_INVALID_PHONE();
+      $reportArray = $reportInvalidOb->getReportInvalidLog($startDate,$endDate);
+  
+      foreach ($reportArray as $key => $value) 
+      {
+         $profileArray[]=$value['SUBMITTEE'];
+         $profileArray[]=$value['SUBMITTER'];
+      # code...
+      }
+  
+
+    if(is_array($profileArray))
+    { 
+      $daysForCount = 90;
+      $countArray= (new JSADMIN_REPORT_INVALID_PHONE('newjs_slave'))->getReportInvalidCount($profileArray,$daysForCount);
+//die("no");
+      $profileDetails=(new JPROFILE())->getProfileSelectedDetails($profileArray,"PROFILEID,EMAIL,USERNAME");
+
+      foreach ($reportArray as $key => $value) 
+      { 
+      $tempArray['submitee_id']=$profileDetails[$value['SUBMITTEE']]['USERNAME'];
+      $tempArray['count']=$countArray[$value['SUBMITTEE']];
+      $tempArray['submiter_id']=$profileDetails[$value['SUBMITTER']]['USERNAME'];
+      $tempArray['comments']=$value['COMMENTS'];
+      $tempArray['timestamp']=$value['SUBMIT_DATE'];
+      $tempArray['comments']=$value['OTHER_REASON'];
+      $tempArray['submiter_email']=$profileDetails[$value['SUBMITTER']]['EMAIL'];
+      $tempArray['submitee_email']=$profileDetails[$value['SUBMITTEE']]['EMAIL'];
+      $resultArr[]=$tempArray;      
+      unset($tempArray);
+      # code...
+      }
+    }
+     // print_r($resultArr);
+      //die("correctly done");
+        echo json_encode($resultArr);
+                        return sfView::NONE;
+                        die;
+                
+  }
 
 
 
--- lib/model/store/jsadmin/JSADMIN_REPORT_INVALID_PHONE.class.php
+++ lib/model/store/jsadmin/JSADMIN_REPORT_INVALID_PHONE.class.php
@@ -63,5 +63,62 @@ public function updateAsVerified($submittee){
                 }
     }
 
+        public function getReportInvalidLog($startDate,$endDate)
+    {
+        try     
+        {   
+            $sql = "SELECT * from jsadmin.REPORT_INVALID_PHONE WHERE DATE(`SUBMIT_DATE`) BETWEEN :STARTDATE AND :ENDDATE ORDER BY `SUBMIT_DATE` DESC";
+            $prep = $this->db->prepare($sql);
+            $prep->bindValue(":STARTDATE",$startDate,PDO::PARAM_STR);
+            $prep->bindValue(":ENDDATE",$endDate,PDO::PARAM_STR);
+            $prep->execute();
+            while($row=$prep->fetch(PDO::FETCH_ASSOC))
+            $result[]=$row;
+        //print_r($result);
+        //die(x);
+        return $result;
+        }
+        catch(Exception $e)
+        {
+            throw new jsException($e);
+        }
+    
+    }
+
+
+
+    public function getReportInvalidCount($profileArray , $days)
+    {
+        try     
+        {   
+                        if(!is_array($profileArray))
+                            throw new jsException("","profileArray IS not array or blank");
+                        $pdoStr="";
+                        foreach($profileArray as $k=>$v)
+                        {
+
+                                                    $pdoStr.=":v".$k.",";
+                                                    
+                        }
+                        $pdoStr = substr($pdoStr, 0, -1);
+
+                        $sql = "SELECT SUBMITTEE,count(*) AS CNT from jsadmin.REPORT_INVALID_PHONE WHERE SUBMIT_DATE > ( CURDATE() - INTERVAL ".$days." DAY )  AND SUBMITTEE IN ($pdoStr) GROUP BY SUBMITTEE"; 
+                        $prep = $this->db->prepare($sql);
+                        foreach($profileArray as $k=>$v)
+                            $prep->bindValue(":v".$k,$v,PDO::PARAM_INT);
+                        $prep->execute();
+                        while($row=$prep->fetch(PDO::FETCH_ASSOC))
+                            $result[$row['SUBMITTEE']]=$row['CNT'];
+                        return $result;
+        }
+        catch(Exception $e)
+        {
+            throw new jsException($e);
+        }
+    
+    }
+
+
+
 }
-?>
+?>    
