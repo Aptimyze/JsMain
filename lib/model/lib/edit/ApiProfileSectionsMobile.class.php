9d0f17a20cccd496a99cd7b9fdffa72a4e4475a6
lavesh <ankit.shukla@jeevansathi.com>
2016-09-20 18:25:23 +0530

    blank mstatus check

--- lib/model/lib/edit/ApiProfileSectionsMobile.class.php
+++ lib/model/lib/edit/ApiProfileSectionsMobile.class.php
@@ -712,7 +712,7 @@ class ApiProfileSectionsMobile extends ApiProfileSections{
 		$DppBasicArr["BasicDetails"][OnClick][]= $this->getApiFormatArray("P_MSTATUS","Marital Status",trim($jpartnerObj->getDecoratedPARTNER_MSTATUS()),$szMStatus,$this->getApiScreeningField("PARTNER_MSTATUS"),$this->dropdown,'',1,'dppMstatus');
                 //Have Children
                 $showHaveChild=1;
-                if($jpartnerObj->getPARTNER_MSTATUS()=="'N'")
+                if($jpartnerObj->getPARTNER_MSTATUS()=="'N'" || $jpartnerObj->getPARTNER_MSTATUS()=="")
                     $showHaveChild=0;
 		$szChildren = $this->getDecorateDPP_Response($jpartnerObj->getCHILDREN());
 		$DppBasicArr["BasicDetails"][OnClick][] = $this->getApiFormatArray("P_HAVECHILD","Have Children",trim($jpartnerObj->getDecoratedCHILDREN()),$szChildren,$this->getApiScreeningField("CHILDREN"),$this->dropdown,'',1,'',!$showHaveChild);
