cabc9945feba4b9719f472433178c2d6955260ff
ankita.g <ankita.g@jeevansathi.com>
2016-09-22 15:49:39 +0530

    JSPC_CHAT_DPP merged into CIRelease

--- web/profile/commonfile_functions.php
+++ web/profile/commonfile_functions.php
@@ -602,7 +602,7 @@ $js_arr[]=array("common_comscore_js"   => "2");
 $js_arr[]=array("jspc/chat/chatCommonConfig_js"            => "15");
 $js_arr[]=array("jspc/chat/chatPCHelper_js"                 => "19");
 $js_arr[]=array("jspc/chat/chatPlugin_js"               => "17");
-$js_arr[]=array("jspc/chat/strophieWrapper_js"             => "15");
+$js_arr[]=array("jspc/chat/strophieWrapper_js"             => "16");
 $js_arr[]=array("jspc/chat/chatStrophieClient_js"      => "5");
 $js_arr[]=array("jspc/help/JSPCHelp_js"     => "3");
 $js_arr[]=array("jsms/help/JSMSHelp_js"     => "3");
