fcbc73dcfea0308075657074a69d370752a0f9a7
Nikhil <nikhil.mittal@jeevansathi.com>
2016-09-28 18:30:40 +0530

    JSI-2240: Sql build

---
+++ web/sql_builds/JSI-2240.sql
@@ -0,0 +1,2 @@
+use newjs;
+UPDATE `SMS_TYPE` SET `MESSAGE` = 'Dear {USERNAME} ({EMAIL}), you may reset the password for your jeevansathi account through {FORGOT_PASSWORD_URL}' WHERE `ID` = '7' LIMIT 1;
