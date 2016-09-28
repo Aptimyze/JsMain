ce8b4bb1ab1a589080f1e053e7ea420ee61427e3
lavesh <ankit.shukla@jeevansathi.com>
2016-09-28 19:36:37 +0530

    syntax error

--- lib/model/lib/profile/FieldForm.class.php
+++ lib/model/lib/profile/FieldForm.class.php
@@ -318,10 +318,6 @@ class FieldForm extends sfForm
                                                 $screen_flag = Flag::removeFlag($FLAGID="name", $screen_flag);
                                       }
                                 }
-        
-				if($screen_flag!=$this->loggedInObj->getSCREENING())
-					$jprofileFieldArr['SCREENING']=$screen_flag;
-			  }
 			  if(array_key_exists("ANCESTRAL_ORIGIN",$jprofileFieldArr))
 			  {
 				if($jprofileFieldArr["ANCESTRAL_ORIGIN"]=="")
