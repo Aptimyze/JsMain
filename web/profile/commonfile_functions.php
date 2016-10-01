23529043d51bc899719c2227dd28cb935f15d9ef
root <root@devjs.infoedge.com>
2016-10-01 16:28:18 +0530

    ForceAllow Css/Js

--- web/profile/commonfile_functions.php
+++ web/profile/commonfile_functions.php
@@ -590,7 +590,7 @@ $js_arr[]=array("customReg_js"  => "2");
 $js_arr[]=array("jspc/common/jquery_mousewheel" => "1");
 $js_arr[]=array("jspc/bounceMail/bounceMailDetection_js" => "1");
 $js_arr[]=array("jspc/contactus/sw"   => "3");
-$js_arr[]=array("jsms/settings/mobDeleteProfile_js"       => "9");
+$js_arr[]=array("jsms/settings/mobDeleteProfile_js"        => "10");
 $js_arr[]=array("commonExpiration_js"  => "2");
 $js_arr[]=array("sw"      => "4");
 $js_arr[]=array("main"      => "4");
