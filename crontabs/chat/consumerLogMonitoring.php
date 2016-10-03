82bf55373adace4110640c4ee847bdb8bfbe21a8
ankita.g <ankita.g@jeevansathi.com>
2016-10-03 16:35:49 +0530

    consumerLogMonitoring path changes

--- crontabs/chat/consumerLogMonitoring.php
+++ crontabs/chat/consumerLogMonitoring.php
@@ -1,34 +1,56 @@
 <?php
 	$domain = "http://10.10.18.67:9090/";
-	$logFilePath = array("remainingLog"=>"/data/projects/logs/JsRosterConsumer.log",
-						 "updateLog"=>"/data/projects/logs/JsRosterConsumer_update.log");
+	$logFilePath = array("USER_CREATE_Log"=>"/data/projects/logs/JsRosterConsumer_USER_CREATE.log",
+						 "USER_DELETE_Log"=>"/data/projects/logs/JsRosterConsumer_USER_DELETE.log",
+						 "SHORTLIST_ROSTER_Log"=>"/data/projects/logs/JsRosterConsumer_SHORTLIST_ROSTER.log",
+						 "DPP_ROSTER_Log"=>"/data/projects/logs/JsRosterConsumer_DPP_ROSTER.log",
+						 "INT_REC_ROSTER_Log"=>"/data/projects/logs/JsRosterConsumer_INT_REC_ROSTER.log",
+						 "INT_SENT_ROSTER_Log"=>"/data/projects/logs/JsRosterConsumer_INT_SENT_ROSTER.log",
+						 "ACCEPTANCE_ROSTER_Log"=>"/data/projects/logs/JsRosterConsumer_ACCEPTANCE_ROSTER.log",
+						 "ROSTER_UPDATOR_Log"=>"/data/projects/logs/JsRosterConsumer_ROSTER_UPDATOR.log",
+						 "ACCEPTANCE_SENT_Log"=>"/data/projects/logs/JsRosterConsumer_ACCEPTANCE_SENT.log");
 
-	$consumerMapping = array($logFilePath["remainingLog"]=>array(array(
-								"consumerGrep"=>"/home/developer/projects/JsConsumer/jsconsumer-create.jar",
+	$consumerMapping = array(
+							$logFilePath["USER_CREATE_Log"]=>array(array(
+								"consumerGrep"=>"/home/developer/projects/JsRosterConsumer/jsrosterconsumer-0.1.0.jar",
 								"consumerPath"=>"java -Dspring.profiles.active=1 -jar jsrosterconsumer-0.1.0.jar >>/dev/null &"),
-								array(
-								"consumerGrep"=>"/home/developer/projects/JsConsumer/jsconsumer-shortlist.jar",
-								"consumerPath"=>"java -Dspring.profiles.active=3 -jar jsrosterconsumer-0.1.0.jar >>/dev/null &"),
-								array(
-								"consumerGrep"=>"/home/developer/projects/JsConsumer/jsconsumer-delete.jar",
+							),
+							$logFilePath["USER_DELETE_Log"]=>array(array(
+								"consumerGrep"=>"/home/developer/projects/JsRosterConsumer/jsrosterconsumer-0.1.0.jar",
 								"consumerPath"=>"java -Dspring.profiles.active=2 -jar jsrosterconsumer-0.1.0.jar >>/dev/null &"),
-								array(
-								"consumerGrep"=>"/home/developer/projects/JsConsumer/jsconsumer-intrec.jar",
+							),
+							$logFilePath["SHORTLIST_ROSTER_Log"]=>array(array(
+								"consumerGrep"=>"/home/developer/projects/JsRosterConsumer/jsrosterconsumer-0.1.0.jar",
+								"consumerPath"=>"java -Dspring.profiles.active=3 -jar jsrosterconsumer-0.1.0.jar >>/dev/null &"),
+							),
+							$logFilePath["DPP_ROSTER_Log"]=>array(array(
+								"consumerGrep"=>"/home/developer/projects/JsRosterConsumer/jsrosterconsumer-0.1.0.jar",
+								"consumerPath"=>"java -Dspring.profiles.active=4 -jar jsrosterconsumer-0.1.0.jar >>/dev/null &"),
+							),
+							$logFilePath["INT_REC_ROSTER_Log"]=>array(array(
+								"consumerGrep"=>"/home/developer/projects/JsRosterConsumer/jsrosterconsumer-0.1.0.jar",
 								"consumerPath"=>"java -Dspring.profiles.active=5 -jar jsrosterconsumer-0.1.0.jar >>/dev/null &"),
-								array(
-								"consumerGrep"=>"/home/developer/projects/JsConsumer/jsconsumer-intsent.jar",
+							),
+							$logFilePath["INT_SENT_ROSTER_Log"]=>array(array(
+								"consumerGrep"=>"/home/developer/projects/JsRosterConsumer/jsrosterconsumer-0.1.0.jar",
 								"consumerPath"=>"java -Dspring.profiles.active=6 -jar jsrosterconsumer-0.1.0.jar >>/dev/null &"),
-								array(
-								"consumerGrep"=>"/home/developer/projects/JsConsumer/jsconsumer-acceptance.jar", 
+							),
+							$logFilePath["ACCEPTANCE_ROSTER_Log"]=>array(array(
+								"consumerGrep"=>"/home/developer/projects/JsRosterConsumer/jsrosterconsumer-0.1.0.jar",
 								"consumerPath"=>"java -Dspring.profiles.active=7 -jar jsrosterconsumer-0.1.0.jar >>/dev/null &"),
-								array(
-								"consumerGrep"=>"/home/developer/projects/JsConsumer/jsconsumer-acceptance_sent.jar",
-								"consumerPath"=>"java -Dspring.profiles.active=9 -jar jsrosterconsumer-0.1.0.jar >>/dev/null &"),
-								),
-							$logFilePath["updateLog"]=>array(array(
-								"consumerGrep"=>"/home/developer/projects/JsConsumer/jsconsumer-update.jar",
+							),
+							$logFilePath["ROSTER_UPDATOR_Log"]=>array(array(
+								"consumerGrep"=>"/home/developer/projects/JsRosterConsumer/jsrosterconsumer-0.1.0.jar",
 								"consumerPath"=>"java -Dspring.profiles.active=8 -jar jsrosterconsumer-0.1.0.jar >>/dev/null &"),
-								));
+							),
+							$logFilePath["ACCEPTANCE_SENT_Log"]=>array(array(
+								"consumerGrep"=>"/home/developer/projects/JsRosterConsumer/jsrosterconsumer-0.1.0.jar",
+								"consumerPath"=>"java -Dspring.profiles.active=9 -jar jsrosterconsumer-0.1.0.jar >>/dev/null &"),
+							)
+						);
+	//print_r($consumerMapping);
+	//print_r($logFilePath);
+	//die;
 	$currentTime = time();
 	$timeThreshold = 900; //in sec
 	foreach ($logFilePath as $key => $path) {
@@ -65,7 +87,7 @@
 	}
 
 	function sendMailAlert($subject){
-		mail("lavesh.rawat@gmail.com,pankaj139@gmail.com,nsitankita@gmail.com,nitishpost@gmail.com,vibhor.garg@jeevansathi.com",$subject,"Please check");
+		mail("lavesh.rawat@gmail.com/*,pankaj139@gmail.com*/,nsitankita@gmail.com,nitishpost@gmail.com,vibhor.garg@jeevansathi.com",$subject,"Please check");
 		//mail("nitishpost@gmail.com",$subject,"Please check");
 	}
 ?>
