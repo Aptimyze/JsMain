db602c99076307a1e5c2f2315e4a863facb321cf
Palash Chordia <palash.chordia@jeevansathi.com>
2016-10-04 18:41:46 +0530

    JSI-655

--- lib/model/lib/contacts/Initiate.class.php
+++ lib/model/lib/contacts/Initiate.class.php
@@ -247,11 +247,7 @@ class Initiate extends ContactEvent{
         $this->contactHandler->getContactObj()->insertContact();
         $action = FTOStateUpdateReason::EOI_SENT;
         $this->contactHandler->getViewer()->getPROFILE_STATE()->updateFTOState($this->viewer, $action);
-        if(sfContext::getInstance()->getRequest()->getParameter('fromJSMS_MYJS')==1)
-        {
-            $this->viewerMemcacheObject->update("MATCHALERT_TOTAL",-1,$this->optionalFlag);
-            $this->viewerMemcacheObject->setMATCHALERT(0);
-        }
+        
       }
                 $requestTimeOut = 300;
     //curl for analytics team by Nitesh for Lavesh team
@@ -269,7 +265,11 @@ class Initiate extends ContactEvent{
     }
 
       $this->_searchContactFlowTracking();
-
+      if(in_array($this->stype, array(SearchTypesEnums::MATCHALERT_MYJS_JSMS,SearchTypesEnums::MATCHALERT_MYJS_IOS,  SearchTypesEnums::AppMyJsMatchAlertSection)))
+            {
+            $this->viewerMemcacheObject->update("MATCHALERT_TOTAL",-1,$this->optionalFlag);
+            $this->viewerMemcacheObject->setMATCHALERT(0);
+            } 
       $this->contactHandler->setElement(CONTACT_ELEMENTS::STATUS,"I");
       $this->handleMessage();
       $isFiltered = $this->_makeEntryInContactsOnce();
@@ -398,8 +398,8 @@ if ($this->contactHandler->getContactObj()->getFILTERED() != Contacts::FILTERED 
       else if($this->stype=='VO' || $this->stype=='VN')
         $this->stype='V';
     }
-    
-    $searchContactFlowTrackingObj->insert(
+            
+            $searchContactFlowTrackingObj->insert(
         $this->viewer->getPROFILEID(),
         $this->stype,
         $this->contactHandler->getContactObj()->getCONTACTID(),
