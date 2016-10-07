8b6117f58e94b683d73179693e5566146606b965
Palash Chordia <palash.chordia@jeevansathi.com>
2016-10-07 16:41:43 +0530

    JSI-2241

--- apps/jeevansathi/modules/common/actions/actions.class.php
+++ apps/jeevansathi/modules/common/actions/actions.class.php
@@ -803,9 +803,9 @@ class commonActions extends sfActions
      $this->phoneNum =$verificationObj->getPhoneWithoutIsd();
      $this->isd = $verificationObj->getIsd();
      if($this->isd == 91)
-     $this->contactHelp = "1-800-419-6299";
+     $this->contactHelp = CommonConstants::HELP_NUMBER_INR;
      else
-     $this->contactHelp =  "+91-120-4393500";    
+     $this->contactHelp =  CommonConstants::HELP_NUMBER_NRI;    
     $this->phoneType =$verificationObj->getPhoneType();
     if($response['trialsOver']=='Y')
         $response['trialsOverMessage']=PhoneApiFunctions::$OTPTrialsOverMsg;
