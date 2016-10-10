8604688b141a4e421546357a86053bddf84f77fe
ankita.g <ankita.g@jeevansathi.com>
2016-10-10 14:08:39 +0530

    JSC-2031

--- commonConfig/JsConstants.class.php
+++ commonConfig/JsConstants.class.php
@@ -62,7 +62,8 @@ class JsConstants
 	public static $alertSymfonyRoot   = '/var/www/htmlrevamp/ser6/branches/milestoneConfig';
 	public static $userHome           = '/home/developer';
 	public static $alertServerEnable  = 1;
-	public static $stopOnPeakLoad     = 1;
+	//if set to 1, hides unimportant features at time of peak load on site
+	public static $hideUnimportantFeatureAtPeakLoad     = 0;
         
         /* Vsp URL */
 	public static $vspMaleUrl           = 'http://maleapi.analytics.resdex.com:9000/ecpRecommendations_live';
