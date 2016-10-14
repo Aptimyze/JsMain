42b48d35d4adce321b58dd0c84068ff9f1a18a43
Nikhil <nikhil.mittal@jeevansathi.com>
2016-10-14 15:08:39 +0530

    JSI-2298-v2: Captcha enums

---
+++ lib/model/enums/Captcha/CaptchaEnum.class.php
@@ -0,0 +1,14 @@
+<?php 
+
+	/**
+	* Description of CaptchaEnum
+	* Class which contain all the defined constants and enums
+	* related to Google-reCaptcha
+	*/
+	class CaptchaEnum
+	{
+		// Can create new set of keys from here: https://www.google.com/recaptcha/admin
+		const SITE_KEY = "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI";
+		const SECRET_KEY = "6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe";
+	}
+?>
