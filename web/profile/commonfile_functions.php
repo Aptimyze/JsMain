898b5b5922d260c513de39320c3cf8e45aa33501
Palash Chordia <palash.chordia@jeevansathi.com>
2016-10-26 16:19:29 +0530

    JSI-2287

--- web/profile/commonfile_functions.php
+++ web/profile/commonfile_functions.php
@@ -610,7 +610,7 @@ $js_arr[]=array("jspc/help/JSPCHelp_js"             => "15");
 $js_arr[]=array("jsms/help/JSMSHelp_js"     => "3");
 $js_arr[]=array("jsms/help/jsmsPostQuery_js"     => "3");
 $js_arr[]=array("jsms/help/autoPopulateFreshdesk_js"     => "3");
-$js_arr[]=array("jsms/myjs/CALJSMS_js"         => "5");
+$js_arr[]=array("jsms/myjs/CALJSMS_js"         => "6");
 $js_arr[]=array("jspc/chat/customElem"       => "4");
 $js_arr[]=array("jspc/chat/strophe_receipts"       => "4");
 $js_arr[]=array("-1.0.2/Gruntfile"    => "3");
