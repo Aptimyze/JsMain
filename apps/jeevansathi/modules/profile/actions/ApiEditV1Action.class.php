decd0c9b26fd12ee7c6d1b59a18ede2d5d860c29
esha.jain <esha.jain@jeevansathi.com>
2016-11-04 20:09:44 +0530

    app crash issue logging

--- apps/jeevansathi/modules/profile/actions/ApiEditV1Action.class.php
+++ apps/jeevansathi/modules/profile/actions/ApiEditV1Action.class.php
@@ -30,6 +30,9 @@ class ApiEditV1Action extends sfActions
 		$this->loginProfile = LoggedInProfile::getInstance();
     if($this->loginProfile->getAGE()== "")
       $this->loginProfile->getDetail($request->getAttribute("profileid"),"PROFILEID","*");
+                $c =  $this->loginProfile;
+                $d = $request->getAttribute("profileid");
+
 		$sectionFlag = $request->getParameter("sectionFlag");
 		$apiProfileSectionObj=  ApiProfileSections::getApiProfileSectionObj($this->loginProfile);
 		$religion = $this->loginProfile->getReligion();
@@ -43,6 +46,13 @@ class ApiEditV1Action extends sfActions
 		{
 			//called EditDetails method and response stored in $ResponseOut
 			$ResponseOut = $editDetailsObj->getEditDetailsValues($this,$apiProfileSectionObj,$sectionFlag,$myProfileArr);
+                        if(MobileCommon::isApp()=="A" && $sectionFlag=="all")
+                        {
+                                if(!$myProfileArr['MyBasicInfo'][8]['value']|| $myProfileArr['MyBasicInfo'][8]['value']==NULL || $myProfileArr['MyBasicInfo'][8]['value']==''){
+                                        file_put_contents(sfConfig::get("sf_upload_dir")."/SearchLogs/editReligionProb.txt",var_export($myProfileArr['MyBasicInfo'][8],true)."profileid:".$this->loginProfile->getPROFILEID()."obj".var_export($c,true)."profileidattr:".$d."\n",FILE_APPEND);
+
+}
+                        }
 			$apiResponseHandlerObj->setHttpArray($ResponseOut);
 			$apiResponseHandlerObj->setResponseBody($myProfileArr);
 			$apiResponseHandlerObj->generateResponse();
