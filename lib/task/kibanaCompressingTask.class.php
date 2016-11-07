26ebb985836e0f451be8dd418de020e1cab9aece
Ayush Sethi <ayushsethi22031992@gmail.com>
2016-11-07 11:19:39 +0530

    JSI-2260 : CRON MODIFIED

--- lib/task/kibanaCompressingTask.class.php
+++ lib/task/kibanaCompressingTask.class.php
@@ -21,7 +21,7 @@ class kibanaCompressingTask extends sfBaseTask
     $this->namespace        = 'Kibana';
     $this->name             = 'indicesCompressor';
     //This is the path to ELK server
-    $this->elkServer = '10.10.18.66';
+    $this->elkServer = 'localhost';
     //This is the port number where Elastic Search is running
     $this->elkPort = '9200';
     //This is the index name which will be searched for getting the records.
@@ -29,7 +29,7 @@ class kibanaCompressingTask extends sfBaseTask
     //This is the type of query which will be used to query elastic search.
     $this->query = '_search'; 
 
-    $this->addArguments(array(new sfCommandArgument('hours',sfCommandArgument::OPTIONAL,'this parameter is added to include the number of hours for which the cron has to be made',$hours = "72")));   
+    $this->addArguments(array(new sfCommandArgument('hours',sfCommandArgument::OPTIONAL,'this parameter is added to include the number of hours for which the cron has to be made',$hours = "24")));   
     $this->briefDescription = 'This cron is used to compress data which is extracted from indices of the elastic search.';
     $this->detailedDescription = <<<EOF
 The [kibanaCompressing|INFO] task does things.
@@ -47,7 +47,10 @@ EOF;
             mkdir($dirPath,0777,true);
         }
 
-      $hoursNow = 48;
+        $fieldsToQuery = array("channelName",'Domain','typeOfError','beat.name','moduleName','logType','apiVersion','REQUEST_URI','actionName');
+
+      $hoursNow = $arguments['hours'];
+      for($j=0 ; $j <9 ; $j++){
       for($i=0 ; $i <= $hoursNow ; $i++)
       {
         $hoursNow = $i; 
@@ -67,7 +70,7 @@ EOF;
             "aggs" => [
                 "modules" => [
                     "terms" => [
-                        "field" => "channelName",
+                        "field" =>$fieldsToQuery[$j] ,
                         "size" => 1000
                     ]
                 ]
@@ -76,322 +79,23 @@ EOF;
 
         $response = CommonUtility::sendCurlPostRequest($urlToHit,json_encode($params));
         $arrResponse = json_decode($response, true);
-        $arrChannels = array();
+        $arrResult = array();
         foreach($arrResponse['aggregations']['modules']['buckets'] as $module)
         {  
 
-            $channelName = $module['key'];
+            $fieldName = $module['key'];
             for($i = 0 ; $i < $module['doc_count'] ; $i++)
             { 
-                $arrChannels['time'] = $time;
-                $arrChannels['channelName'] = $channelName;
+                $arrResult['time'] = $time;
+                $arrResult['fieldValue'] = $fieldName;
                 $filePath = $dirPath."/kibanaCompressing-";
                 $fileResource = fopen($filePath,"a");
-                fwrite($fileResource,json_encode($arrChannels)."\n");
+                fwrite($fileResource,json_encode($arrResult)."\n");
                 fclose($fileResource);
             }
         }
-     
-        
-         $params = [
-            "query"=> [
-                "range" => [
-                    "@timestamp" => [
-                        "gte" => "now-{$ltHour}h",
-                        "lte" => "now-{$hoursNow}h"                                        ]
-                ]
-            ],
-            'aggs' => [
-                'modules' => [
-                    'terms' => [
-                        'field' => 'Domain',
-                        'size' => 1000
-                    ]
-                ]
-            ]
-        ];
-        $response = CommonUtility::sendCurlPostRequest($urlToHit,json_encode($params));
-        $arrResponse = json_decode($response, true);
-        $arrDomain = array();
-
-        foreach($arrResponse['aggregations']['modules']['buckets'] as $module)
-        {
-
-             $domainName = $module['key']; 
-            for($i = 0 ; $i < $module['doc_count'] ; $i++)
-            { 
-                $arrDomain['time'] = $time;
-                $arrDomain['Domain'] = $domainName;
-                $filePath = $dirPath."/kibanaCompressing-";
-                $fileResource = fopen($filePath,"a");
-                fwrite($fileResource,json_encode($arrDomain)."\n");
-                fclose($fileResource);
-            }
-        }
-
-         $params = [
-            "query"=> [
-                "range" => [
-                    "@timestamp" => [
-                       "gte" => "now-{$ltHour}h",
-                        "lte" => "now-{$hoursNow}h"                    
-                    ]
-                ]
-            ],
-            'aggs' => [
-                'modules' => [
-                    'terms' => [
-                        'field' => 'typeOfError',
-                        'size' => 1000
-                    ]
-                ]
-            ]
-        ];
-        $response = CommonUtility::sendCurlPostRequest($urlToHit,json_encode($params));
-        $arrResponse = json_decode($response, true);
-        $TypeOfError = array();
-
-        foreach($arrResponse['aggregations']['modules']['buckets'] as $module)
-        {
-
-             $errorType = $module['key'];
- 
-            for($i = 0 ; $i < $module['doc_count'] ; $i++)
-            { 
-                $TypeOfError['time'] = $time;
-                $TypeOfError['typeOfError'] = $errorType;
-                $filePath = $dirPath."/kibanaCompressing-";
-                $fileResource = fopen($filePath,"a");
-                fwrite($fileResource,json_encode($TypeOfError)."\n");
-                fclose($fileResource);
-            }
-        }
-
-         $params = [
-            "query"=> [
-                "range" => [
-                    "@timestamp" => [
-                  "gte" => "now-{$ltHour}h",
-                        "lte" => "now-{$hoursNow}h"                    
-                    ]
-                ]
-            ],
-            'aggs' => [
-                'modules' => [
-                    'terms' => [
-                        'field' => 'beat.name',
-                        'size' => 1000
-                    ]
-                ]
-            ]
-        ];
-        $response = CommonUtility::sendCurlPostRequest($urlToHit,json_encode($params));
-        $arrResponse = json_decode($response, true);
-        $arrHostname = array();
-
-        foreach($arrResponse['aggregations']['modules']['buckets'] as $module)
-        {
-            $hostname = $module['key'];
-            for($i = 0 ; $i < $module['doc_count'] ; $i++)
-            { 
-                $arrHostname['time'] = $time;
-                $arrHostname['beat.name'] = $hostname;
-                $filePath = $dirPath."/kibanaCompressing-";
-                $fileResource = fopen($filePath,"a");
-                fwrite($fileResource,json_encode($arrHostname)."\n");
-                fclose($fileResource);
-            }
-
-
-        }
-
-         $params = [
-            "query"=> [
-                "range" => [
-                    "@timestamp" => [
-                        "gte" => "now-{$ltHour}h",
-                        "lte" => "now-{$hoursNow}h"                    
-                    ]
-                ]
-            ],
-            'aggs' => [
-                'modules' => [
-                    'terms' => [
-                        'field' => 'moduleName',
-                        'size' => 1000
-                    ]
-                ]
-            ]
-        ];
-        $response = CommonUtility::sendCurlPostRequest($urlToHit,json_encode($params));
-        $arrResponse = json_decode($response, true);
-        $arrmoduleName = array();
-
-        foreach($arrResponse['aggregations']['modules']['buckets'] as $module)
-        {
-
-             $modulename = $module['key'];
-             for($i = 0 ; $i < $module['doc_count'] ; $i++)
-            { 
-                $arrmoduleName['time'] = $time;
-                $arrmoduleName['moduleName'] = $channelName;
-                $filePath = $dirPath."/kibanaCompressing-";
-                $fileResource = fopen($filePath,"a");
-                fwrite($fileResource,json_encode($arrmoduleName)."\n");
-                fclose($fileResource);
-            }
-        }
-
-         $params = [
-            "query"=> [
-                "range" => [
-                    "@timestamp" => [
-                        "gte" => "now-{$ltHour}h",
-                        "lte" => "now-{$hoursNow}h"                    
-                    ]
-                ]
-            ],
-            'aggs' => [
-                'modules' => [
-                    'terms' => [
-                        'field' => 'logType',
-                        'size' => 1000
-                    ]
-                ]
-            ]
-        ];
-        $response = CommonUtility::sendCurlPostRequest($urlToHit,json_encode($params));
-        $arrResponse = json_decode($response, true);
-        $arrLogType = array();
-
-        foreach($arrResponse['aggregations']['modules']['buckets'] as $module)
-        {
-
-            $logType = $module['key'];
-
-             for($i = 0 ; $i < $module['doc_count'] ; $i++)
-            { 
-                $arrLogType['time'] = $time;
-                $arrLogType['logType'] = $logType;
-                $filePath = $dirPath."/kibanaCompressing-";
-                $fileResource = fopen($filePath,"a");
-                fwrite($fileResource,json_encode($arrLogType)."\n");
-                fclose($fileResource);
-            }
-        }
-
-         $params = [
-            "query"=> [
-                "range" => [
-                    "@timestamp" => [
-                        "gte" => "now-{$ltHour}h",
-                        "lte" => "now-{$hoursNow}h"                    
-                    ]
-                ]
-            ],
-            'aggs' => [
-                'modules' => [
-                    'terms' => [
-                        'field' => 'apiVersion',
-                        'size' => 1000
-                    ]
-                ]
-            ]
-        ];
-        $response = CommonUtility::sendCurlPostRequest($urlToHit,json_encode($params));
-        $arrResponse = json_decode($response, true);
-        $arrApiVersion = array();
-
-        foreach($arrResponse['aggregations']['modules']['buckets'] as $module)
-        {
-
-             $apiVersion = $module['key'];
-             for($i = 0 ; $i < $module['doc_count'] ; $i++)
-            { 
-                $arrApiVersion['time'] = $time;
-                $arrApiVersion['apiVersion'] = $apiVersion;
-                $filePath = $dirPath."/kibanaCompressing-";
-                $fileResource = fopen($filePath,"a");
-                fwrite($fileResource,json_encode($arrApiVersion)."\n");
-                fclose($fileResource);
-            }
-        }
-
-         $params = [
-            "query"=> [
-                "range" => [
-                    "@timestamp" => [
-                        "gte" => "now-{$ltHour}h",
-                        "lte" => "now-{$hoursNow}h"                    
-                    ]
-                ]
-            ],
-            'aggs' => [
-                'modules' => [
-                    'terms' => [
-                        'field' => 'actionName',
-                        'size' => 1000
-                    ]
-                ]
-            ]
-        ];
-        $response = CommonUtility::sendCurlPostRequest($urlToHit,json_encode($params));
-        $arrResponse = json_decode($response, true);
-        $arrActionName = array();
-
-        foreach($arrResponse['aggregations']['modules']['buckets'] as $module)
-        {
-
-            $actionName = $module['key'];
-             for($i = 0 ; $i < $module['doc_count'] ; $i++)
-            { 
-                $arrActionName['time'] = $time;
-                $arrActionName['actionName'] = $actionName;
-                $filePath = $dirPath."/kibanaCompressing-";
-                $fileResource = fopen($filePath,"a");
-                fwrite($fileResource,json_encode($arrActionName)."\n");
-                fclose($fileResource);
-            }
-
-        }
-        
-         $params = [
-            "query"=> [
-                "range" => [
-                    "@timestamp" => [
-                        "gte" => "now-{$ltHour}h",
-                        "lte" => "now-{$hoursNow}h"                    
-                    ]
-                ]
-            ],
-            'aggs' => [
-                'modules' => [
-                    'terms' => [
-                        'field' => 'REQUEST_URI',
-                        'size' => 1000
-                    ]
-                ]
-            ]
-        ];
-        $response = CommonUtility::sendCurlPostRequest($urlToHit,json_encode($params));
-        $arrResponse = json_decode($response, true);
-        $arrrequestURI = array();
-
-        foreach($arrResponse['aggregations']['modules']['buckets'] as $module)
-        {
-
-              $requestURI = $module['key'];
-             for($i = 0 ; $i < $module['doc_count'] ; $i++)
-            { 
-                $arrrequestURI['time'] = $time;
-                $arrrequestURI['REQUEST_URI'] = $requestURI;
-                $filePath = $dirPath."/kibanaCompressing-";
-                $fileResource = fopen($filePath,"a");
-                fwrite($fileResource,json_encode($arrrequestURI)."\n");
-                fclose($fileResource);
-            }
-        }
-
-      }
+     }
     }
+
+}
 }
