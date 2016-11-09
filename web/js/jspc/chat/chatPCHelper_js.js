e1b6c4eec22071ff9d34723e917d6c4b57a6b33b
ankita.g <ankita.g@jeevansathi.com>
2016-11-09 16:14:03 +0530

    JSC-2073_V1 cirelease merge

--- web/js/jspc/chat/chatPCHelper_js.js
+++ web/js/jspc/chat/chatPCHelper_js.js
@@ -113,12 +113,13 @@ function pollForNonRosterListing(type,updateChatListImmediate){
             dataType: 'json',
             //data: postData,
             type: 'GET',
-            //cache: false,
+           // cache: true,
             async: true,
             timeout: chatConfig.Params.nonRosterListingApiConfig[type]["timeoutTime"],
             headers:{
-                'JB-Profile-Identifier':loggedInJspcUser
-                //'Cache-Control':'max-age=300'
+                'JB-Profile-Identifier':loggedInJspcUser/*,
+                'Cache-Control':'max-age=300',
+                'Last-Modified':((new Date()).toLocaleString())*/
             },
             beforeSend: function (xhr) {},
             success: function (response) {
