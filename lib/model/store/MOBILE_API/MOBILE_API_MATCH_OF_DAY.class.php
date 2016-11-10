0ab29c45bc0482d64db857099e07e63081b3ca37
nitish.sharma <nitish.sharma@99acres.com>
2016-11-10 19:31:04 +0530

    JSC-1979 added store

---
+++ lib/model/store/MOBILE_API/MOBILE_API_MATCH_OF_DAY.class.php
@@ -0,0 +1,41 @@
+<?php
+
+
+class MOBILE_API_MATCH_OF_DAY extends TABLE{
+    
+    public function __construct($dbName = "") {
+        parent::__construct($dbName);
+    }
+    
+    public function getMatchForProfileTillDays($paramsArr){
+        if($paramsArr){
+            try{
+                $sql = "SELECT MATCH_PROFILEID FROM MOBILE_API.MATCH_OF_DAY_LOG where PROFILEID = :PROFILEID AND ENTRY_DT > :ENTRY_DT";
+                $prep = $this->db->prepare($sql);
+                $prep->bindValue(":PROFILEID",$paramsArr["PROFILEID"],PDO::PARAM_INT);
+                $prep->bindValue(":ENTRY_DT",$paramsArr["ENTRY_DT"],PDO::PARAM_STR);
+                $prep->execute();
+                while($row = $prep->fetch(PDO::FETCH_ASSOC)){
+                    $result[] = $row["MATCH_PROFILEID"];
+                }
+                return $result;
+            } catch (Exception $ex) {
+                throw new jsException($ex);
+            }
+        }
+    }
+    
+    public function getCountForMatchProfile(){
+        try{
+            $sql = "SELECT MATCH_PROFILEID, COUNT( * ) AS COUNT FROM MOBILE_API.MATCH_OF_DAY_LOG GROUP BY MATCH_PROFILEID ORDER BY COUNT";
+            $prep = $this->db->prepare($sql);
+            $prep->execute();
+            while($row = $prep->fetch(PDO::FETCH_ASSOC)){
+                $result[$row["MATCH_PROFILEID"]] = $row["COUNT"];
+            }
+            return $result;
+        } catch (Exception $ex) {
+            throw new jsException($ex);
+        }
+    }
+}
