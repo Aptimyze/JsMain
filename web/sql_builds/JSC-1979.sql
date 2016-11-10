4a81b99a57e789f245d35dbcdf63c0ed206e7724
nitish.sharma <nitish.sharma@99acres.com>
2016-11-10 10:18:10 +0530

    JSC-1979 added job and sql

---
+++ web/sql_builds/JSC-1979.sql
@@ -0,0 +1,2 @@
+use MOBILE_API;
+INSERT INTO  MOBILE_API.`APP_NOTIFICATIONS` (  `ID` ,  `NOTIFICATION_KEY` ,  `MESSAGE` , `LANDING_SCREEN` ,  `OS_TYPE` ,  `STATUS` ,  `FREQUENCY` ,  `TIME_CRITERIA` ,  `PRIORITY` ,  `COUNT` ,  `COLLAPSE_STATUS` ,  `TTL` ,  `GENDER` , `SUBSCRIPTION` ,  `TITLE` ,  `PHOTO_URL` ) VALUES ('',  'MATCH_OF_DAY',  '{USERNAME_OTHER_1} is your match of the day, Connect now!.',  '',  'ALL',  'Y',  'D', NULL ,  '',  'SINGLE',  'Y',  '3600',  'A',  'A',  'Match of the day!',  'P');
