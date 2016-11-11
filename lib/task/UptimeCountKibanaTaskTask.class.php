ea3f6159ab4b4a16eef23510a3ed4fa7de4820bf
Nikhil <nikhil.mittal@jeevansathi.com>
2016-11-12 02:39:41 +0530

    JSI-2434: get 200,500 counts nd calculate ratio

--- lib/task/UptimeCountKibanaTaskTask.class.php
+++ lib/task/UptimeCountKibanaTaskTask.class.php
@@ -34,86 +34,62 @@ EOF;
 		$timeout = 5000;
 		$dirPath = '/home/nickedes/Desktop/logs';
 		$filePath = $dirPath."/Counts.log";
+		$interval = 24;
 		$date = date('Y-m-d', strtotime('-1 day'));
 		$urlToHit = $elkServer.':'.$elkPort.'/'.$indexName.'/'.$query;
 
 		if (false === is_dir($dirPath)) {
 			mkdir($dirPath,0777,true);
 		}
-
+		$rcode200 = "200";
+		$rcode500 = "500";
 		// parameters required, get all request code counts in the specified interval
-		// $params = [
-		// 	"aggs" => [
-		// 			"rcodes" => [
-		// 				"terms" => [ "field" => "RCODE" ,  "size" => 100 ],
-		// 				"aggs" => [
-		// 					"histo" => [
-		// 						"date_histogram" => [
-		// 							"field" => "ACTIVITY_DATE",
-		// 							"interval" => "day",
-		// 							"format" => "yyyy-MM-dd"
-		// 						]
-		// 					]
-		// 				]
-		// 			]
-		// 		]
-		// ];
-
 		$params = [
 
 			"query"=> [
-			    "filtered"=> [
-			      "query"=> [
-			        "match"=> [ "RCODE" => "200" ]
-			      ],
-			      "filter"=> [
-			        "range"=> [ "ACTIVITY_DATE"=> 
-				        	[
-				        		"gte" => "now-20h",
-				        		"lt" => "now",
-				        	]
-			      ]
-			    ]
+				"filtered"=> [
+				  "query"=> [
+					"match"=> [ "RCODE" => $rcode200 ]
+				  ],
+				  "filter"=> [
+					"range"=> [ "ACTIVITY_DATE"=>
+							[
+								"gte" => "now-".$interval."h",
+								"lt" => "now",
+							]
+				  ]
+				]
 			  ]
 		]];
 
+		// send curl request
+		$response200 =  CommonUtility::sendCurlPostRequest($urlToHit, json_encode($params), $timeout);
 		$params = [
 
 			"query"=> [
-			    "filtered"=> [
-			      "query"=> [
-			        "match"=> [ "RCODE" => "500" ]
-			      ],
-			      "filter"=> [
-			        "range"=> [ "ACTIVITY_DATE"=> 
-				        	[
-				        		"gte" => "now-1h",
-				        		"lt" => "now",
-				        	]
-			      ]
-			    ]
+				"filtered"=> [
+				  "query"=> [
+					"match"=> [ "RCODE" => $rcode500 ]
+				  ],
+				  "filter"=> [
+					"range"=> [ "ACTIVITY_DATE"=> 
+							[
+								"gte" => "now-".$interval."h",
+								"lt" => "now",
+							]
+				  ]
+				]
 			  ]
 		]];
 		// send curl request
-		$response =  CommonUtility::sendCurlPostRequest($urlToHit, json_encode($params), $timeout);
-		if($response)
+		$response500 =  CommonUtility::sendCurlPostRequest($urlToHit, json_encode($params), $timeout);
+		if($response200 && $response500)
 		{
-			$arrResponse = json_decode($response, true);
-			print_r($arrResponse);
-			die;
 			$arrModules = array();
-			foreach($arrResponse['aggregations']['rcodes']['buckets'] as $rcode)
-			{
-					foreach ($rcode['histo']['buckets'] as $dateCounts) {
-						print_r($dateCounts);
-						if($dateCounts['key_as_string'] == $date)
-						{
-							$arrModules[$rcode['key']] = $dateCounts['doc_count'];
-						}
-					}
-			}
-			$ratio = $arrModules['200']/$arrModules['500'];
-			$count = array('200' => $arrModules['200'], '500' => $arrModules['500'], 'ratio' => $ratio);
+			$arrModules[$rcode200] = json_decode($response200, true)['count'];
+			$arrModules[$rcode500] = json_decode($response500, true)['count'];
+			$ratio = $arrModules[$rcode200]/$arrModules[$rcode500];
+			$count = array($rcode200 => $arrModules[$rcode200], $rcode500 => $arrModules[$rcode500], 'ratio' => $ratio);
 			$fileResource = fopen($filePath,"a");
 			fwrite($fileResource,json_encode($count)."\n");
 			fclose($fileResource);
