3917b930a890ad2486a4419fba506714e57019d6
bhavana <bhavana.kadwal@jeevansathi.com>
2016-11-16 09:39:54 +0530

    JSM-3083

--- crontabs/onetime_updatehoroscopedata.php
+++ crontabs/onetime_updatehoroscopedata.php
@@ -10,9 +10,9 @@ include("connect.inc");
 include_once(JsConstants::$docRoot."/classes/Mysql.class.php");
 include_once(JsConstants::$docRoot."/commonFiles/SymfonyPictureFunctions.class.php");
 include_once(JsConstants::$docRoot."/classes/globalVariables.Class.php");
-
+include_once(JsConstants::$docRoot."/classes/JProfileUpdateLib.php");
 // Column mapping and values to update
-$columnToUpdate = array("column_name"=>"SHOW_HOROSCOPE","column_name_rev"=>"PARTNER_CASTE","column_trends"=>"CASTE_VALUE_PERCENTILE","new_value"=>"'N'","old_value"=>"'Y'");
+$columnToUpdate = array("column_name"=>"SHOW_HOROSCOPE","column_txt"=>"HOROSCOPE","new_value"=>"'N'","old_value"=>"'Y'","new_value_Jprofile"=>"N");
 
 /**
  * Table array to update
@@ -24,8 +24,8 @@ $columnToUpdate = array("column_name"=>"SHOW_HOROSCOPE","column_name_rev"=>"PART
  */
 $tableArray = array(
   "0"=>array("table_name"=>"newjs.JPROFILE","update_type"=>"single","conn_type"=>"master","column"=>"column_name","unique_key"=>"PROFILEID"),
-  "1"=>array("table_name"=>"newjs.SEARCH_FEMALE_TEXT","update_type"=>"single","conn_type"=>"master","column"=>"column_name","unique_key"=>"PROFILEID"),
-  "2"=>array("table_name"=>"newjs.SEARCH_MALE_TEXT","update_type"=>"single","conn_type"=>"master","column"=>"column_name","unique_key"=>"PROFILEID")
+  "1"=>array("table_name"=>"newjs.SEARCH_FEMALE_TEXT","update_type"=>"single","conn_type"=>"master","column"=>"column_txt","unique_key"=>"PROFILEID","UPDATE_TABLE"=>'SEARCH_FEMALE'),
+  "2"=>array("table_name"=>"newjs.SEARCH_MALE_TEXT","update_type"=>"single","conn_type"=>"master","column"=>"column_txt","unique_key"=>"PROFILEID","UPDATE_TABLE"=>'SEARCH_MALE')
 );
 // Master and slave connection object
 global $mysqlObjS , $mysqlObjM;
@@ -47,10 +47,21 @@ while($row = $mysqlObjS->fetchAssoc($horoscopeResult)){
         $profiles[] = $row['PROFILEID'];
 }
 if($profiles){
+        $jprofileUpdateObj = JProfileUpdateLib::getInstance();
         foreach($profiles as $profileId){
                 foreach($tableArray as $tableArr){
-                        $updateSql = "UPDATE ".$tableArr['table_name']." SET ".$columnToUpdate[$tableArr['column']]." = ".$columnToUpdate['new_value']." WHERE PROFILEID = ".$profileId." AND ".$columnToUpdate[$tableArr['column']]." = ".$columnToUpdate['old_value'] ;
-                        $mysqlObjM->executeQuery($updateSql,$connMaster) or $mysqlObjM->logError($updateSql);
+                        if($tableArr['table_name'] == 'newjs.JPROFILE'){
+                                $arrFields = array($columnToUpdate[$tableArr['column']]=>$columnToUpdate['new_value_Jprofile']);
+                                $exrtaWhereCond = $columnToUpdate[$tableArr['column']]."=".$columnToUpdate['old_value']."";
+                                //print_r($arrFields);die;
+                                $res = $jprofileUpdateObj->editJPROFILE($arrFields,$profileId,"PROFILEID",$exrtaWhereCond);
+                        }else{
+                                $updateSql = "UPDATE ".$tableArr['table_name']." SET ".$columnToUpdate[$tableArr['column']]." = ".$columnToUpdate['new_value']." WHERE PROFILEID = ".$profileId." AND ".$columnToUpdate[$tableArr['column']]." = ".$columnToUpdate['old_value'] ;
+                                $mysqlObjM->executeQuery($updateSql,$connMaster) or $mysqlObjM->logError($updateSql);
+                                
+                                $updateSql2 = "UPDATE ".$tableArr['UPDATE_TABLE']." SET LAST_MODIFIED = NOW( ) WHERE PROFILEID = '".$profileId."'";
+                                $mysqlObjM->executeQuery($updateSql2,$connMaster) or $mysqlObjM->logError($updateSql2);
+                        }
                 }
                 $cScoreObj = ProfileCompletionFactory::getInstance(null,null,$profileId);
                 $cScoreObj->updateProfileCompletionScore();
