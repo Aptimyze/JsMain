0952afe63df77a547cb353ed83f5b7fd2bf87c52
mohammad.shahjahan <mohammad.shahjahan@jeevansathi.com>
2016-11-17 19:00:40 +0530

    JSM-2989: added changes for css file.

--- web/profile/commonfile_functions.php
+++ web/profile/commonfile_functions.php
@@ -166,7 +166,7 @@ $css_arr[]=array("jspc/common/cssRangeSlider"     => "2");
 $css_arr[]=array("jspc/search/rangeslider"    => "1");
 $css_arr[]=array("jspc/common/jquerymCustomScrollbar"    => "1");
 $css_arr[]=array("jspc/search/searchBand"    => "1");
-$css_arr[]=array("jspc/profile/edit/dpp/dppEdit_css"                       => "20");
+$css_arr[]=array("jspc/profile/edit/dpp/dppEdit_css"                       => "21");
 $css_arr[]=array("jspc/utility/chosen/chosen_css"    => "1");
 $css_arr[]=array("jspc/logout/homecss"           => "8");
 $css_arr[]=array("jspc/phone/phoneVerification_css"     => "2");
