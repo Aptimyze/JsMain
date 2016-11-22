939fa9c8559b303ddbe4c31ffad8232feb958c5a
ankita.g <ankita.g@jeevansathi.com>
2016-11-22 14:04:24 +0530

    JSC-2065

---
+++ web/sql_builds/JSC-2065.sql
@@ -0,0 +1,3 @@
+use MOBILE_API;
+
+INSERT INTO `APP_NOTIFICATIONS` VALUES (43, 'LOGIN_REGISTER', 'Join Jeevansathi now and upload a photo to start receiving interests instantly. If you are already a member, sign in!', 1, 'AND', 'Y', 'D', 0, 0, 'SINGLE', 'Y', 86400, 'A', 'A', 'Login or Register on Jeevansathi', 'D');
