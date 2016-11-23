563529b7aae21f7124e50160f5f081c15531ccd8
Palash Chordia <palash.chordia@jeevansathi.com>
2016-11-23 15:16:56 +0530

    JSI-2510

--- web/js/jsms/myjs/myjs_js.js
+++ web/js/jsms/myjs/myjs_js.js
@@ -119,9 +119,9 @@ function setBlock(blockName) {
 
 	$(document).ready(function() {
             
-                $("#hamburgerIcon").on("click", function() {
+                $("#hamburgerIcon").bind("click", function() {
 			if($("#hamburger").length == 0){
-				$(".loaderSmallIcon").attr("src","IMG_URL/images/jsms/commonImg/loader.gif").removeClass("dn");
+                                $(".loaderSmallIcon").addClass("loaderimg").removeClass("dn");
 				$("#hamIc").hide();
 				if(localStorage.getItem("hamHtml")){
 					$("#perspective").append(localStorage.getItem("hamHtml"));	
@@ -132,9 +132,17 @@ function setBlock(blockName) {
 				}
 					//Jquery call 
                                 getHamburgerCounts();
-                                $("#hamburgerIcon").off("click");
 				var imported = document.createElement('script');
 				imported.src = 'IMG_URL/min/?f=/'+hamJs;
+                                imported.onerror = function() {
+                                ShowTopDownError(['Something went wrong']); 
+                                $("#hamburger").remove(); 
+                                setTimeout(function(){
+                                    $(".loaderSmallIcon").addClass("dn");
+                                    $("#hamIc").show();
+                                }, 100);   
+                                };
+
 				imported.onload = function() {
 					BindNextPage();
 				    $("#hamburgerIcon").click();
