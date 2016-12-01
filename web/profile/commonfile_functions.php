d2497e4b1b5c83e8e1198a1af77aabcd04a669bd
Nikhil <nikhil.mittal@jeevansathi.com>
2016-12-01 18:05:28 +0530

    ForceAllow Css/Js

--- web/profile/commonfile_functions.php
+++ web/profile/commonfile_functions.php
@@ -571,7 +571,7 @@ $js_arr[]=array("jspc/success_story/foggy"  => "1");
 $js_arr[]=array("jspc/success_story/resizeIe"  => "1");
 $js_arr[]=array("jspc/contactEngine/contactEngine"               => "25");
 $js_arr[]=array("jspc/contactEngine/contactEngineButtons"              => "15");
-$js_arr[]=array("jspc/search/advancedSearch"     => "3");
+$js_arr[]=array("jspc/search/advancedSearch"      => "4");
 $js_arr[]=array("jspc/common/jquery_mCustomScrollbar"              => "13");
 $js_arr[]=array("jspc/success_story/jqueryFilestyle" => "1");
 $js_arr[]=array("jspc/profile/edit/profile/jspcCoverPhoto_js"  => "1");
