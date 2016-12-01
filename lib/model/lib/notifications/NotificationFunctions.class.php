aa0f9a17587dc2f29f4b65e49096718a3ceec82c
manoj.rana <manoj.rana@jeevansathi.com>
2016-12-01 18:40:01 +0530

    JSC-2159

--- lib/model/lib/notifications/NotificationFunctions.class.php
+++ lib/model/lib/notifications/NotificationFunctions.class.php
@@ -99,13 +99,13 @@ class NotificationFunctions
         public function notificationCheck($request)
         {
                 $notificationStop =JsConstants::$notificationStop;
-                if((date("H")>='11' && date("H")<='15') || (date("H")>='01' && date("H")<='03'))
+                if((date("H")>='10' && date("H")<='20') || (date("H")>='23' && date("H")<='02'))
                         $notificationStop=1;
                 if($notificationStop)
                 {
                         $notificationData['notifications'] = '';
-			//$newTime =date('Y-m-d H:i:s',time()+86400);
-                        $notificationData['alarmTime']= '';
+			$newTime =date('Y-m-d H:i:s',time()+86400);
+                        $notificationData['alarmTime']= $newTime;
                         $data =json_encode($notificationData);
                         return $data;
                 }
