772123ede23553b6c7722c40c68112457e1a211a
nitish.sharma <nitish.sharma@99acres.com>
2016-12-02 15:34:59 +0530

    ForceAllow Css/Js

--- web/profile/commonfile_functions.php
+++ web/profile/commonfile_functions.php
@@ -592,7 +592,7 @@ $js_arr[]=array("jspc/bounceMail/bounceMailDetection_js" => "1");
 $js_arr[]=array("jspc/contactus/sw"   => "3");
 $js_arr[]=array("jsms/settings/mobDeleteProfile_js"             => "15");
 $js_arr[]=array("commonExpiration_js"  => "2");
-$js_arr[]=array("sw"      => "4");
+$js_arr[]=array("sw"       => "5");
 $js_arr[]=array("main"      => "4");
 $js_arr[]=array("jsms/common/verificationIcon_js"     => "3");
 $js_arr[]=array("jsms/settings/settings_js"     => "2");
