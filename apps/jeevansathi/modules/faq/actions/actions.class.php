117748eb6cadb92a0feee4ecbce267df80cbd75c
Ayush Sethi <ayushsethi22031992@gmail.com>
2017-01-03 19:29:55 +0530

    [JSI-2555]

--- apps/jeevansathi/modules/faq/actions/actions.class.php
+++ apps/jeevansathi/modules/faq/actions/actions.class.php
@@ -15,90 +15,90 @@ class faqActions extends sfActions
   *
   * @param sfRequest $request A request object
   */
- public function preExecute()
- {
-        //  $this->setLayout(false);
- }
- public function executeIndex(sfWebRequest $request)
- {
-   $this->redirect('/contactus/index');
-        //Contains login credentials
-   $loginData=$request->getAttribute("loginData");
-   $this->profileID=$loginData[PROFILEID]; 
-   $objFAQ = new FAQ;
-   $objFAQ->ProcessData($request);
-
-   $this->data = $loginData["PROFILEID"];
-
-   $this->nonstyle =1 ;
-   $this->flagged = null;
-   $this->NO_NAVIGATION = 1 ;
-
-   $this->trace1   = $objFAQ->getTracePath();
-   $this->linkarr  = $objFAQ->getLinkArray();
-   $this->trace    = $objFAQ->getTrace();
-   $this->current  = $objFAQ->getCurrentSelection();
-   $this->arrstart = $objFAQ->getFAQLabel();
-
-        //Banners 
-   $this->bms_topright  = 18;
-   $this->bms_bottom = 19;
-   $this->bms_left = 24;
-   $this->bms_new_win= 32;
-
-        //Success Story
-   $this->bms_1 = 28;
-   $this->bms_2 = 28;
-
-   $this->rightPanelStory = IndividualStories::showSuccessPoolStory();
- }
-
- public function executeFeedback(sfWebRequest $request)
- {
-  $feedBackObj = new FAQFeedBack;
-
-
-  $loginData=$request->getAttribute("loginData");
-  if($loginData[PROFILEID])
-  {
-    $loginProfile=LoggedInProfile::getInstance();
-    $loginProfile->getDetail($loginData['PROFILEID'],"PROFILEID");
-    $this->USERNAME=$loginData[USERNAME];
-  }
-
-  $success=false;
-  if($feedBackObj->ProcessData($request)){
-    $success=true;
-  }
-
-  $objNameStore = new incentive_NAME_OF_USER;
-  $loginProfile=LoggedInProfile::getInstance();
-  $this->NAME = $objNameStore->getName($loginProfile->getPROFILEID());
-  $feedBackForm = new FeedBackForm(0);
-  $this->form = $feedBackObj->getForm();
-  $this->tracepath = $feedBackObj->getTracePath();
-
-
-  if(MobileCommon::isMobile())
-  {
-    if($request->getParameter('feed') && $success===false)
-      $this->ERROR=1;
-
-    if($request->getParameter('feed')  && $success===true)
+    public function preExecute()
     {
+        //  $this->setLayout(false);
+    }
+    public function executeIndex(sfWebRequest $request)
+    {
+      $this->redirect('/contactus/index');
+        //Contains login credentials
+        $loginData=$request->getAttribute("loginData");
+        $this->profileID=$loginData[PROFILEID]; 
+        $objFAQ = new FAQ;
+        $objFAQ->ProcessData($request);
+        
+        $this->data = $loginData["PROFILEID"];
 
-      $this->MESSAGE1="Thank you for your valuable feedback.";
-      $this->MESSAGE2="If required, we will get in touch with you.";
-      sfContext::getInstance()->getResponse()->setTitle("Feedback sent - Jeevansathi") ;
-      $this->setTemplate("mobilefaqconfirm");
-    }   
-    else    
-      $this->setTemplate("mobilefaq");
-  }
+        $this->nonstyle =1 ;
+        $this->flagged = null;
+        $this->NO_NAVIGATION = 1 ;
+        
+        $this->trace1   = $objFAQ->getTracePath();
+        $this->linkarr  = $objFAQ->getLinkArray();
+        $this->trace    = $objFAQ->getTrace();
+        $this->current  = $objFAQ->getCurrentSelection();
+        $this->arrstart = $objFAQ->getFAQLabel();
+        
+        //Banners 
+        $this->bms_topright  = 18;
+        $this->bms_bottom = 19;
+        $this->bms_left = 24;
+        $this->bms_new_win= 32;
+        
+        //Success Story
+        $this->bms_1 = 28;
+        $this->bms_2 = 28;
+                
+        $this->rightPanelStory = IndividualStories::showSuccessPoolStory();
+    }
+    
+    public function executeFeedback(sfWebRequest $request)
+    {
+        $feedBackObj = new FAQFeedBack;
+    
 
-}
+        $loginData=$request->getAttribute("loginData");
+        if($loginData[PROFILEID])
+        {
+            $loginProfile=LoggedInProfile::getInstance();
+            $loginProfile->getDetail($loginData['PROFILEID'],"PROFILEID");
+            $this->USERNAME=$loginData[USERNAME];
+        }
+        
+        $success=false;
+        if($feedBackObj->ProcessData($request)){
+            $success=true;
+        }
 
-public function executeReportAbuseForUserLog(sfWebRequest $request)
+        $objNameStore = new incentive_NAME_OF_USER;
+        $loginProfile=LoggedInProfile::getInstance();
+    $this->NAME = $objNameStore->getName($loginProfile->getPROFILEID());
+        $feedBackForm = new FeedBackForm(0);
+        $this->form = $feedBackObj->getForm();
+        $this->tracepath = $feedBackObj->getTracePath();
+        
+        
+        if(MobileCommon::isMobile())
+        {
+            if($request->getParameter('feed') && $success===false)
+                $this->ERROR=1;
+                
+            if($request->getParameter('feed')  && $success===true)
+            {
+                
+                $this->MESSAGE1="Thank you for your valuable feedback.";
+                $this->MESSAGE2="If required, we will get in touch with you.";
+                sfContext::getInstance()->getResponse()->setTitle("Feedback sent - Jeevansathi") ;
+                $this->setTemplate("mobilefaqconfirm");
+            }   
+            else    
+                $this->setTemplate("mobilefaq");
+        }
+        
+    }
+
+    public function executeReportAbuseForUserLog(sfWebRequest $request)
 {
 
   $resultArr = $request->getParameter('feed');
