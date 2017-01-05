748c11ceb5cbd9fdae54c3e21b24b45df3956723
ankita.g <ankita.g@jeevansathi.com>
2017-01-05 14:23:30 +0530

    [JSC-2266] ForceAllow Css/Js

--- web/profile/commonfile_functions.php
+++ web/profile/commonfile_functions.php
@@ -603,7 +603,7 @@ $js_arr[]=array("jsms/common/disableRightClick_js"    => "2");
 $js_arr[]=array("jsms/profile/horoscope/horoscope_js"  => "1");
 $js_arr[]=array("common_comscore_js"   => "2");
 $js_arr[]=array("jspc/chat/chatCommonConfig_js"                                       => "72");
-$js_arr[]=array("jspc/chat/chatPCHelper_js"                                                                                   => "106");
+$js_arr[]=array("jspc/chat/chatPCHelper_js"                                                                                    => "107");
 $js_arr[]=array("jspc/chat/chatPlugin_js"                                                        => "85");
 $js_arr[]=array("jspc/chat/strophieWrapper_js"                                       => "47");
 $js_arr[]=array("jspc/chat/chatStrophieClient_js"      => "7");
