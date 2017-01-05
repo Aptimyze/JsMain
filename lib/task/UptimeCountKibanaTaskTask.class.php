0978428ccdb329d5e8c5ce5db7d40f6e9db88fd5
Nikhil <nikhil.mittal@jeevansathi.com>
2017-01-05 14:39:09 +0530

    [JSI-2630]

--- lib/task/UptimeCountKibanaTaskTask.class.php
+++ lib/task/UptimeCountKibanaTaskTask.class.php
@@ -20,7 +20,8 @@ EOF;
 
 	protected function execute($arguments = array(), $options = array())
 	{
-		$elkServer = 'es.aura.resdex.com';
+		// $elkServer = 'es.aura.resdex.com';
+		$elkServer = 'aura.infoedge.com';
 		$elkPort = '9203';
 		$indexName = 'jeevansathiactivity';
 		$query = '_search';
@@ -31,8 +32,10 @@ EOF;
 		$indexElkServer = '10.10.18.66';
 		$indexElkPort = '9200';
 		$pushIndexName = 'uptime';
+		$appIndexName = 'filebeat-*';
 		$date = date('Y-m-d', strtotime("-$day day"));
-		$urlToHit = $elkServer.':'.$elkPort.'/'.$indexName.'/'.$query;
+		$auraUrl = $elkServer.':'.$elkPort.'/'.$indexName.'/'.$query;
+		$elkAppUrl = $indexElkServer.':'.$indexElkPort.'/'.$appIndexName.'/'.$query;
 		$rcode200 = "200";
 		$rcode500 = "500";
 		// Calculates the aggregated sum of counts of Rcodes
@@ -54,36 +57,81 @@ EOF;
 			];
 
 		// Calculates 500 counts from APP logs
+		// $params2 = [
+		// 	"query"=> [
+		// 		"filtered"=> [ "query"=> [ "query_string"=> [
+		// 			"analyze_wildcard"=> true,
+		// 			"query"=> "*" ]],
+		// 			"filter"=> ["bool"=> ["must"=> [[
+		// 				"query"=> [
+		// 					"match"=> [ "moduleName"=> [ "query"=> "500", "type"=> "phrase"] ]],
+		// 			  "$state"=> ["store"=> "appState"]],
+		// 	["range"=> [
+		// 		"@timestamp"=> [
+		// 		  "gte"=> "now-".($day*$interval)."h",
+		// 		  "lte"=> "now-".(($day-1)*$interval)."h",
+		// 		]]]],]]]],
+		// 	"aggs"=> ["2"=> ["terms"=> [
+		// 		"field"=> "moduleName","size"=> 100,
+		// 		"order"=> ["_count"=> "desc"]]]]
+		// ];
 		$params2 = [
 			"query"=> [
-				"filtered"=> [ "query"=> [ "query_string"=> [
-					"analyze_wildcard"=> true,
-					"query"=> "*" ]],
-					"filter"=> ["bool"=> ["must"=> [[
-						"query"=> [
-							"match"=> [ "moduleName"=> [ "query"=> "500", "type"=> "phrase"] ]],
-					  "$state"=> ["store"=> "appState"]],
-			["range"=> [
-				"@timestamp"=> [
-				  "gte"=> 1483592781925,
-				  "lte"=> 1483596381925,
-				  "format"=> "epoch_millis"
-				]]]],]]]],
-			"aggs"=> ["2"=> ["terms"=> [
-				"field"=> "moduleName","size"=> 100,
-				"order"=> ["_count"=> "desc"]]]]
+			    "match" => ["logType"=>"Error"]
+			],
+			"aggs"=> [
+			"filtered"=> [
+			  "filter"=> [
+			    "bool"=> [
+			      "must"=> [
+			        [
+			          "range"=> [
+			            "@timestamp"=> [
+			              "gt"=> "now-".$interval."h",
+			              "lt"=> "now"
+			            ]
+			          ]
+			        ]
+			      ]
+			    ]
+			  ],
+			  "aggs"=> [
+			    "modules"=>
+			    [
+			        "terms"=>
+			        [ "field" => "moduleName" ,  "size" => 1000 ]
+			    ]
+			  ]
+			]
+			]
 		];
 		// send curl request
-		$response =  CommonUtility::sendCurlPostRequest($urlToHit, json_encode($params), $timeout);
-		if($response)
+		$AuraResponse =  CommonUtility::sendCurlPostRequest($auraUrl, json_encode($params), $timeout);
+		$ElkResponse =  CommonUtility::sendCurlPostRequest($elkAppUrl, json_encode($params2), $timeout);
+		if($AuraResponse && $ElkResponse)
 		{
-			$arrResponse = json_decode($response, true);
+			$arrResponse = json_decode($AuraResponse, true);
 			$arrModules = array();
 			foreach($arrResponse['aggregations']['2']['buckets'] as $result)
 			{
 				// get the aggregated value of sum of counts
 				$arrModules[$result['key']] = $result['1']['value'];
 			}
+
+			$arrResponse = json_decode($ElkResponse, true);
+			print_r($arrResponse);die;
+			$arrRcode = array();
+			foreach($arrResponse['aggregations']['2']['buckets'] as $result)
+			{
+				print_r($result);
+				// get the aggregated value of counts
+				$arrRcode[$result['key']] = $result['doc_count'];
+			}			
+			print_r($arrRcode);
+			$arrModules[$rcode200] -= $arrRcode[$rcode500];
+			$arrModules[$rcode500] += $arrRcode[$rcode500];
+			print_r($arrModules);
+			die;
 			$ratio = ($arrModules[$rcode500]*100)/($arrModules[$rcode500]+$arrModules[$rcode200]);
 			$count = array(
 					'Date' => $date,
@@ -94,7 +142,7 @@ EOF;
 					);
 			$count = json_encode($count);
 			$ObjectId = time();
-			exec("curl -XPOST '$indexElkServer:$indexElkPort/$pushIndexName/json/$ObjectId' -d'$count'".' 2>&1');
+			// exec("curl -XPOST '$indexElkServer:$indexElkPort/$pushIndexName/json/$ObjectId' -d'$count'".' 2>&1');
 		}
 	}
 }
