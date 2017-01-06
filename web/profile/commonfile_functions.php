33e9002e269bee8831d5780580c267f7fa9904c1
kunal.verma <kunal.verma@jeevansathi.com>
2017-01-06 15:34:43 +0530

    ForceAllow Css/Js

--- web/profile/commonfile_functions.php
+++ web/profile/commonfile_functions.php
@@ -603,7 +603,7 @@ $js_arr[]=array("jsms/common/disableRightClick_js"    => "2");
 $js_arr[]=array("jsms/profile/horoscope/horoscope_js"  => "1");
 $js_arr[]=array("common_comscore_js"   => "2");
 $js_arr[]=array("jspc/chat/chatCommonConfig_js" => "71");
-$js_arr[]=array("jspc/chat/chatPCHelper_js"     => "120");
+$js_arr[]=array("jspc/chat/chatPCHelper_js"      => "121");
 $js_arr[]=array("jspc/chat/chatPlugin_js"  => "85");
 $js_arr[]=array("jspc/chat/strophieWrapper_js"  => "51");
 $js_arr[]=array("jspc/chat/chatStrophieClient_js"      => "7");
