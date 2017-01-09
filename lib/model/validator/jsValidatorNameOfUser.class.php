4fe7d1909563fcd2094d317f7dd8fc893ed7d0e4
Palash Chordia <palash.chordia@jeevansathi.com>
2017-01-09 14:34:14 +0530

    [JSI-2592] JSI-2592

--- lib/model/validator/jsValidatorNameOfUser.class.php
+++ lib/model/validator/jsValidatorNameOfUser.class.php
@@ -66,13 +66,12 @@ class jsValidatorNameOfUser extends sfValidatorBase
   }
   
   public static function validateNameOfUser($name_of_user){
-      
-              $name_of_user = str_replace("."," ",$name_of_user);
+        $name_of_user = str_replace("."," ",$name_of_user);
         $name_of_user = preg_replace("/dr|ms|mr|miss/i", "",$name_of_user);
         $name_of_user = preg_replace("/\,|\'/i", "",$name_of_user);
         $name_of_user = trim(preg_replace("/\s+/i", " ",$name_of_user));
         if($name_of_user == "")return false;
-        $match = preg_match("/^[a-zA-Z]+\s[a-zA-Z]+/i",$name_of_user);
+            $match = preg_match("/^[a-zA-Z\s]+[a-zA-Z]+$/",$name_of_user);
         if($match)
             return true;
         return false;
