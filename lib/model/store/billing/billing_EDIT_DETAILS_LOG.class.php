784ec828f9ef3429bc56abe2cb381edb3444f707
ankita.g <ankita.g@jeevansathi.com>
2017-01-09 15:30:15 +0530

    [JSC-2282] JSC-2282

--- lib/model/store/billing/billing_EDIT_DETAILS_LOG.class.php
+++ lib/model/store/billing/billing_EDIT_DETAILS_LOG.class.php
@@ -20,12 +20,12 @@ class billing_EDIT_DETAILS_LOG extends TABLE {
         {
 			$sqlInsert = "INSERT IGNORE INTO billing.EDIT_DETAILS_LOG (`PROFILEID`,`BILLID`,`RECEIPTID`,`CHANGES`,`ENTRYBY`,`ENTRY_DT`) VALUES (:PROFILEID,:BILLID,:RECEIPTID,:CHANGES,:ENTRYBY,:ENTRY_DT)";
 			$resInsert = $this->db->prepare($sqlInsert);
-			$resInsert->bindValue(":PROFILEID",$profileid,constant('PDO::PARAM_'.$this->{'PROFILEID_BIND_TYPE'}));
-			$resInsert->bindValue(":RECEIPTID",$key,constant('PDO::PARAM_'.$this->{'RECEIPTID_BIND_TYPE'}));
-			$resInsert->bindValue(":CHANGES",$key,constant('PDO::PARAM_'.$this->{'CHANGES_BIND_TYPE'}));
-			$resInsert->bindValue(":BILLID",$messageId,constant('PDO::PARAM_'.$this->{'BILLID_BIND_TYPE'}));
-			$resInsert->bindValue(":ENTRYBY",$nextPollTime,constant('PDO::PARAM_'.$this->{'ENTRYBY_BIND_TYPE'}));
-			$resInsert->bindValue(":ENTRY_DT",$sent,constant('PDO::PARAM_'.$this->{'ENTRY_DT_BIND_TYPE'}));
+			$resInsert->bindValue(":PROFILEID",$paramsArr["PROFILEID"],constant('PDO::PARAM_'.$this->{'PROFILEID_BIND_TYPE'}));
+			$resInsert->bindValue(":RECEIPTID",$paramsArr["RECEIPTID"],constant('PDO::PARAM_'.$this->{'RECEIPTID_BIND_TYPE'}));
+			$resInsert->bindValue(":CHANGES",$paramsArr["CHANGES"],constant('PDO::PARAM_'.$this->{'CHANGES_BIND_TYPE'}));
+			$resInsert->bindValue(":BILLID",$paramsArr["BILLID"],constant('PDO::PARAM_'.$this->{'BILLID_BIND_TYPE'}));
+			$resInsert->bindValue(":ENTRYBY",$paramsArr["ENTRYBY"],constant('PDO::PARAM_'.$this->{'ENTRYBY_BIND_TYPE'}));
+			$resInsert->bindValue(":ENTRY_DT",$paramsArr["ENTRY_DT"],constant('PDO::PARAM_'.$this->{'ENTRY_DT_BIND_TYPE'}));
 			$resInsert->execute();
         } 
         catch (Exception $e){
