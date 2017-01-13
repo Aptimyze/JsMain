2522fe10d1e2d1e1fcff836a10149fbcd60dbbbe
Nikhil <nikhil.mittal@jeevansathi.com>
2017-01-13 11:24:34 +0530

    [JSI-2599]

--- lib/model/lib/profileCommunication/api/MyJsMobileAppV1.class.php
+++ lib/model/lib/profileCommunication/api/MyJsMobileAppV1.class.php
@@ -47,13 +47,9 @@ $className = get_class($this);
 		{
 			if(array_key_exists($key,$displayObj))
 			{
-				if($key == "INTEREST_EXPIRING" && !$showExpiring && !(MobileCommon::isIOSApp()))
-				{
-					continue;
-				}
 				$isApp = MobileCommon::isApp();
 				$appVersion=sfContext::getInstance()->getRequest()->getParameter("API_APP_VERSION")?sfContext::getInstance()->getRequest()->getParameter("API_APP_VERSION"):0;
-				if($isApp == "A" && $appVersion  && $appVersion < 81)
+				if($key == "INTEREST_EXPIRING" && !$showExpiring && !(MobileCommon::isIOSApp()) && ($isApp == "A" && $appVersion  && $appVersion < 81))
 				{
 					continue;
 				}
