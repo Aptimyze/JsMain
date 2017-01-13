3dc49be2b52eb17419b6efdc66411900c305bda3
Ayush Sethi <ayushsethi22031992@gmail.com>
2017-01-13 17:22:30 +0530

    [JSI-2516]

--- web/classes/JProfileUpdateLib.php
+++ web/classes/JProfileUpdateLib.php
@@ -153,7 +153,8 @@ class JProfileUpdateLib
       self::$instance->objProfileChristianStore->setConnection($dbname);
       self::$instance->objProfileAstroDetailsStore = ProfileAstro::getInstance($dbname);
       self::$instance->objProfileHoroscopeForScreenStore->setConnection($dbname);
-      self::$instance->objProfileAlertStore->setConnection($dbname);
+      unset(self::$instance->objProfileAlertStore);
+      self::$instance->objProfileAlertStore = new JprofileAlertsCache($dbname);
     }
     
     return self::$instance;
