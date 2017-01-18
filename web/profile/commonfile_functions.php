81fc0e876df33c1f5e1c016a075836d4da90fe61
Ayush Sethi <ayushsethi22031992@gmail.com>
2017-01-18 10:16:49 +0530

    [JSI-2586] ForceAllow Css/Js

--- web/profile/commonfile_functions.php
+++ web/profile/commonfile_functions.php
@@ -623,7 +623,7 @@ $js_arr[]=array("jspc/chat/aes"    => "3");
 $js_arr[]=array("/web/converse"    => "3");
 $js_arr[]=array(" /web/billing/billing_v2"      => "4");
 $js_arr[]=array(" /web/billing/billing"     => "3");
-$js_arr[]=array("reportAbuseForUser_js"           => "9");
+$js_arr[]=array("reportAbuseForUser_js"            => "10");
 return $js_arr;
 }
 function getJavascript(){
