c6e34d5ac897a7ce927317b4f63bac947af07621
Palash Chordia <palash.chordia@jeevansathi.com>
2017-01-19 13:29:43 +0530

    [JSI-2544] JSI-2544

--- lib/task/cronExecuteLoggingQueueMessageTask.class.php
+++ lib/task/cronExecuteLoggingQueueMessageTask.class.php
@@ -15,11 +15,11 @@ class cronExecuteLoggingQueueMessageTask extends sfBaseTask
   protected function configure()
   {
     $this->namespace           = 'cron';
-    $this->name                = 'cronExecuteUpdateSeenConsumer';
+    $this->name                = 'cronExecuteLoggingQueueMessage';
     $this->briefDescription    = 'reads no. of instances of rabbitmq consumer from MessageQueues.enum.class.php to be run and executes that many instances of cronConsumeQueueMessage.';
     $this->detailedDescription = <<<EOF
      The [cronExecuteSingleConsumer|INFO] reads no. of instances of rabbitmq consumer from MessageQueues.enum.class.php to be run and executes that many instances of cronConsumeQueueMessage:
-     [php symfony cron:cronExecuteLoggingQueueMessageTask] 
+     [php symfony cron:cronExecuteLoggingQueueMessage] 
 EOF;
     $this->addOptions(array(
         new sfCommandOption('application', null, sfCommandOption::PARAMETER_OPTIONAL, 'The application name', 'jeevansathi')
