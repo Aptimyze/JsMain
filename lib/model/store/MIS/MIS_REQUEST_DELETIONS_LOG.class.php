c6fb9b1242b3c48a920d05b38fe9b32b70c5ae85
Ayush Sethi <ayushsethi22031992@gmail.com>
2017-01-19 10:48:53 +0530

    [JSI-2629]

--- lib/model/store/MIS/MIS_REQUEST_DELETIONS_LOG.class.php
+++ lib/model/store/MIS/MIS_REQUEST_DELETIONS_LOG.class.php
@@ -37,5 +37,32 @@ class REQUEST_DELETIONS_LOG extends TABLE{
 			throw new jsException($e);
 		}
 	}
+/*
+	     public function getDataForCSV() 
+	{  
+		
+		try
+		{
 
+			$timeNow=(new DateTime)->format('Y-m-j H:i:s');
+			$sql = "SELECT * FROM MIS.REQUEST_DELETIONS_LOG where `DATE`"
+			$sql="INSERT INTO MIS.REQUEST_DELETIONS_LOG(DATE,CRM_USER,REPORTEE,REQUESTED_BY) VALUES(:DATE,:CRM_USER,:REPORTEE,:REQUESTED_BY)";
+			
+			$pdoStatement = $this->db->prepare($sql);
+			
+			
+			$pdoStatement->bindValue(":REPORTEE",$userPFID,PDO::PARAM_INT);
+			$pdoStatement->bindValue(":REQUESTED_BY",$requestedBy,PDO::PARAM_STR);
+			$pdoStatement->bindValue(":DATE",$timeNow,PDO::PARAM_STR);
+			$pdoStatement->bindValue(":CRM_USER",$crmUser,PDO::PARAM_STR);
+			$pdoStatement->execute();
+			
+			return ;
+		}
+		catch(Exception $e)
+		{
+			throw new jsException($e);
+		}
+	}
+*/
 }
