b56848d78b593ffbae1488e65063d7c05cab58b5
Ayush Sethi <ayushsethi22031992@gmail.com>
2017-01-20 16:12:33 +0530

    [JSI-2629]

--- lib/model/store/MIS/MIS_REQUEST_DELETIONS_LOG.class.php
+++ lib/model/store/MIS/MIS_REQUEST_DELETIONS_LOG.class.php
@@ -47,12 +47,16 @@ class MIS_REQUEST_DELETIONS_LOG extends TABLE{
 		 $date = new DateTime();
 		 $date->sub(new DateInterval('P2D')); //get the date which was 2 days ago
          $lastDateToCheck = $date->format('Y-m-d H:i:s');
-			$sql = "SELECT REPORTEE FROM MIS.REQUEST_DELETIONS_LOG where `DATE` >= ".$lastDateToCheck;
+			$sql = "SELECT DISTINCT REPORTEE FROM MIS.REQUEST_DELETIONS_LOG where `DATE` >= '".$lastDateToCheck."'";
 			$pdoStatement = $this->db->prepare($sql);
-			$pdoStatement->execute();
+			$pdoStatement->execute(); 
 			while($row=$pdoStatement->fetch(PDO::FETCH_ASSOC))
             $result[]=$row;
-        return $result;
+        foreach ($result as $key => $value) {
+        	$resultToReturn[] = $value['REPORTEE'];
+        	# code...
+        }
+        return $resultToReturn;
 		}
 		catch(Exception $e)
 		{
