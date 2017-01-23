597db3c799b564401bcbeac739d692f3420eaf0c
Palash Chordia <palash.chordia@jeevansathi.com>
2017-01-23 06:18:49 -0500

    Update MIS_REQUEST_DELETIONS_LOG.class.php

--- lib/model/store/MIS/MIS_REQUEST_DELETIONS_LOG.class.php
+++ lib/model/store/MIS/MIS_REQUEST_DELETIONS_LOG.class.php
@@ -45,7 +45,7 @@ class MIS_REQUEST_DELETIONS_LOG extends TABLE{
 		{
 
 		 $date = new DateTime();
-		 $date->sub(new DateInterval('P2D')); //get the date which was 2 days ago
+		 $date->sub(new DateInterval('P3D')); //get the date which was 2 days ago
          $lastDateToCheck = $date->format('Y-m-d');
 			$sql = "SELECT DISTINCT REPORTEE FROM MIS.REQUEST_DELETIONS_LOG where DATE(`DATE`) = '".$lastDateToCheck."'";
 			$pdoStatement = $this->db->prepare($sql);
