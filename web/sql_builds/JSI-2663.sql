91d03b772d35370ad6d2ab5862a24999f87ccf62
Nikhil <nikhil.mittal@jeevansathi.com>
2017-01-25 12:00:25 +0530

    [JSI-2663] sql

--- web/sql_builds/JSI-2663.sql
+++ web/sql_builds/JSI-2663.sql
@@ -1,2 +1,2 @@
 use jeevansathi_mailer;
-UPDATE `MAILER_SUBJECT` SET `SUBJECT_CODE` = 'Member <var>{{USERNAME:profileid=~$otherProfileId`}}</var> has sent you personal messages' WHERE `MAIL_ID` = '1759' AND CONVERT(`SUBJECT_TYPE` USING utf8) = 'D' AND CONVERT(`SUBJECT_CODE` USING utf8) = 'Member <var>{{USERNAME:profileid=~$otherProfileId`}}</var> has sent you a personal message' AND CONVERT(`DESCRIPTION` USING utf8) = 'Subject for mail on receiving a message' LIMIT 1;
+UPDATE `MAILER_SUBJECT` SET `SUBJECT_CODE` = 'Member <var>{{USERNAME:profileid=~$otherProfileId`}}</var> has sent you personal messages' WHERE `MAIL_ID` = '1759';
