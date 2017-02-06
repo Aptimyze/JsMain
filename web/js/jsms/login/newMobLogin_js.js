290652028d90154b880f8b7d0605272c78494bf5
vibhor.garg <vibhor.garg@localhost.localdomain>
2017-02-06 13:18:07 +0530

    [JSC-2418] moved static url to https in web folder

--- web/js/jsms/login/newMobLogin_js.js
+++ web/js/jsms/login/newMobLogin_js.js
@@ -291,7 +291,7 @@ $(window).load(function()
 
 
 function createCaptcha(){
-        var captchaDiv = '<div class="captchaDiv pad3"><img class="loaderSmallIcon2" src="http://static.jeevansathi.com/images/jsms/commonImg/loader.gif"><script src="https://www.google.com/recaptcha/api.js"></script><div class="g-recaptcha dn" data-sitekey='+site_key+'></div></div>';
+        var captchaDiv = '<div class="captchaDiv pad3"><img class="loaderSmallIcon2" src="https://static.jeevansathi.com/images/jsms/commonImg/loader.gif"><script src="https://www.google.com/recaptcha/api.js"></script><div class="g-recaptcha dn" data-sitekey='+site_key+'></div></div>';
         if($(".g-recaptcha").length !=0){
             removeCaptcha();
         }
