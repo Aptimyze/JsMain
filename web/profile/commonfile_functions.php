91f0c8284bcdfca09f940ffa27a25da68c86518f
Nikhil <nikhil.mittal@jeevansathi.com>
2017-02-15 19:27:02 +0530

    [JSI-2684] ForceAllow Css/Js

--- web/profile/commonfile_functions.php
+++ web/profile/commonfile_functions.php
@@ -624,7 +624,7 @@ $js_arr[]=array("/web/converse"    => "3");
 $js_arr[]=array("reportAbuseForUser_js"                => "14");
 $js_arr[]=array(" /web/billing/billing_v2"       => "5");
 $js_arr[]=array(" /web/billing/billing"      => "4");
-$js_arr[]=array("jsms/settings/mobHideProfile_js"               => "10");
+$js_arr[]=array("jsms/settings/mobHideProfile_js"                => "11");
 return $js_arr;
 }
 function getJavascript(){
