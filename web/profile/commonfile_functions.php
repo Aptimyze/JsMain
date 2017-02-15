8ea6094ec2ff605aa9c185e8ad0d6b41dc724aee
bhavana <bhavana.kadwal@jeevansathi.com>
2017-02-15 12:33:29 +0530

    [JSM-3610] ForceAllow Css/Js

--- web/profile/commonfile_functions.php
+++ web/profile/commonfile_functions.php
@@ -534,7 +534,7 @@ $js_arr[]=array("jspc/search/common/commonSlider"    => "1");
 $js_arr[]=array("jspc/search/searchSliders"    => "1");
 $js_arr[]=array("jspc/search/common/ionRangeSliderPlugin"    => "1");
 $js_arr[]=array("jspc/common/jquerymCustomScrollbarConcat"   => "1");
-$js_arr[]=array("jspc/profile/edit/dpp/jspcDppEdit_js"                                              => "47");
+$js_arr[]=array("jspc/profile/edit/dpp/jspcDppEdit_js"                                               => "48");
 $js_arr[]=array("jspc/utility/chosen/chosen_jquery"                        => "22");
 $js_arr[]=array("jspc/utility/chosen/docsupport/prism"    => "1");
 $js_arr[]=array("jspc/logout/hpjs"      => "3");
