fec3ce60e2a2b2742ac2cb2cd55dd61087c66c4a
vibhor.garg <vibhor.garg@trunk.jeevansathi.com>
2017-02-15 18:41:38 +0530

    [CIRelease] JSC-2465

--- lib/task/notification/archiveNotificationsLogTask.class.php
+++ lib/task/notification/archiveNotificationsLogTask.class.php
@@ -29,14 +29,15 @@ $this->addOptions(array(
                 sfContext::createInstance($this->configuration);
 
 		ini_set("memory_limit","-1");		
-		$sdate = date('Y-m-d', time()-5*86400);
-		$edate = date('Y-m-d', time()-4*86400);
+		$sdate = date('Y-m-d', time()-4*86400);
+		$edate = date('Y-m-d', time()-3*86400);
 		$notificationLogObj1 =new MOBILE_API_NOTIFICATION_LOG;
 		$notificationLogObj1->deleteRecordDateWise($sdate,$edate);		
 		$notificationLogObj2 =new MOBILE_API_NOTIFICATION_MESSAGE_LOG;
                 $notificationLogObj2->deleteRecordDateWise($sdate,$edate);
 		$notificationLogObj3 =new MOBILE_API_GCM_RESPONSE_LOG;
                 $notificationLogObj3->deleteRecordDateWise($sdate,$edate);
-
+		$notificationLogObj3 =new MOBILE_API_LOCAL_NOTIFICATION_LOG;
+                $notificationLogObj3->deleteRecordDateWise($sdate,$edate);
   }
 }
