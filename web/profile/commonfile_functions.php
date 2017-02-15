c73704d587b1f422689c401ad737c9d8b2460690
Ayush Sethi <ayushsethi22031992@gmail.com>
2017-02-15 13:22:32 +0530

    [JSI-2752] ForceAllow Css/Js

--- web/profile/commonfile_functions.php
+++ web/profile/commonfile_functions.php
@@ -462,7 +462,7 @@ $js_arr[]=array("jsms/angular/registration/hamburger_reg_js"            => "13")
 $js_arr[]=array("jsms/contact_engine/contactEngine_js"                                                            => "65");
 $js_arr[]=array("jsms/profile/viewProfile/jsmsViewProfile_js"                 => "22");
 $js_arr[]=array("jsms/search/inview"   => "2");
-$js_arr[]=array("jsms/login/newMobLogin_js"                                 => "33");
+$js_arr[]=array("jsms/login/newMobLogin_js"                                  => "34");
 $js_arr[]=array("jsms/login/mobForgotPass_js"            => "13");
 $js_arr[]=array("jsms/login/resetPass_js"   => "2");
 $js_arr[]=array("jsms/hamburger/ham_js"                 => "35");
