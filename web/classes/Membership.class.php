dddec5044f4c9e5eb9e4afcb81a2fd859443c9a8
ankita.g <ankita.g@jeevansathi.com>
2017-02-21 11:26:17 +0530

    [UPGRADE_BOOST] JSC-2339 qa sanity merge

--- web/classes/Membership.class.php
+++ web/classes/Membership.class.php
@@ -768,8 +768,7 @@ class Membership
             $modifiedServeFor = $this->servefor;
         }
 
-        error_log("ankita discount in makepaid-".$this->discount);
-        error_log("ankita amount in makepaid-".$this->amount);
+        
         $this->discount_percent = round((($this->discount)/($this->amount+$this->discount)) * 100,2);
         
         
