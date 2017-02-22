3967c9a08e751e6442903a38ed2d2b28701afc0c
mohammad.shahjahan <mohammad.shahjahan@jeevansathi.com>
2017-02-22 16:30:26 +0530

    [JSM-3638_v2] JSM-3638_v2: Added change for sms alerts.

--- lib/task/SMSKibanaAlertsTask.class.php
+++ lib/task/SMSKibanaAlertsTask.class.php
@@ -5,6 +5,10 @@
  */
 class SMSkibanaAlertsTask extends sfBaseTask
 {
+	private $SMS_TO = array("9818424749","9953457479","9873639543","8826380350","9999216910","9868673709","9953178503","9650350387","9755158977","8010619996");
+  	const FROM_ID = "JSSRVR";
+  	const PROFILE_ID = "144111";
+
 	protected function configure()
 	{
 		$this->name             = 'SMSkibanaAlerts';
@@ -15,7 +19,6 @@ Call it with:
 
 	[php symfony SMSkibanaAlerts|INFO]
 EOF;
-		$this->mobileNumberArr = array("9818424749","9953457479","9873639543","8826380350","9999216910","9868673709","9953178503","9650350387","9755158977","8010619996");
 		$this->thresholdSMS = 500;
 	}
 
@@ -68,7 +71,7 @@ EOF;
 		    "modules"=>
 		    [
 		        "terms"=>
-		        [ "field" => "beat.name" ,  "size" => 10000 ],
+		        [ "field" => "beat.name" ],
 		    ]
 		  			]
 				]
@@ -93,7 +96,7 @@ EOF;
 				$serverName = $server['key'];
 				if ( $countError >= $this->thresholdSMS )
 				{
-			   		$this->sms($serverName,$countError,$fromInt);
+			   		$this->sendSMS($serverName,$countError,$fromInt);
 				}
 			}
 		}
@@ -102,16 +105,29 @@ EOF;
 			die;
 		}
 	}
-
-	function sms($serverName,$errorCount,$time)
-	{
-        $message        = "Mysql Error Count have reached server $serverName within 5 minutes $time";
-        $from           = "JSSRVR";
-        $profileid      = "144111";
-
-        foreach ($this->mobileNumberArr as $key => $value) {
-        	$smsState = send_sms($message,$from,$value,$profileid,'','Y');
+	/**
+	 * send sms
+	 * @param  int $serverName server, on which error has occured
+	 * @param  int $errorCount count count on servers
+	 * @param  string $time       
+	 */
+    private function sendSMS($serverName,$errorCount,$time) {
+      $this->smsMessage = "Mysql Error Count have reached Threshold on $serverName($errorCount) within 5 minutes $time";
+      foreach ($this->SMS_TO as $mobPhone) {
+        $xml_head = "%3C?xml%20version=%221.0%22%20encoding=%22ISO-8859-1%22?%3E%3C!DOCTYPE%20MESSAGE%20SYSTEM%20%22http://127.0.0.1/psms/dtd/message.dtd%22%3E%3CMESSAGE%3E%3CUSER%20USERNAME=%22naukari%22%20PASSWORD=%22na21s8api%22/%3E";
+        $xml_content="%3CSMS%20UDH=%220%22%20CODING=%221%22%20TEXT=%22".urlencode($this->smsMessage)."%22%20PROPERTY=%220%22%20ID=%22".self::PROFILE_ID."%22%3E%3CADDRESS%20FROM=%22".self::FROM_ID."%22%20TO=%22".$mobPhone."e%22%20SEQ=%22".self::PROFILE_ID."%22%20TAG=%22%22/%3E%3C/SMS%3E";
+        $xml_end = "%3C/MESSAGE%3E";
+        $xml_code = $xml_head . $xml_content . $xml_end;
+        $fd = @fopen("http://api.myvaluefirst.com/psms/servlet/psms.Eservice2?data=$xml_code&action=send", "rb");
+        if ($fd) {
+          $response = '';
+          while (!feof($fd)) {
+            $response.= fread($fd, 4096);
+          }
+          fclose($fd);
         }
-	}
+      }
+  }
+
 
 }
