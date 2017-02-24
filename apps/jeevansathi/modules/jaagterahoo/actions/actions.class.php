78e5fe2e941017f3a77012b002301d777bd7f02a
esha.jain <esha.jain@jeevansathi.com>
2017-02-24 20:03:42 +0530

    jaagteRahoo

---
+++ apps/jeevansathi/modules/jaagterahoo/actions/actions.class.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * jaagterahoo actions.
+ *
+ * @package    jeevansathi
+ * @subpackage jaagterahoo
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class jaagterahooActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->forward('default', 'module');
+  }
+  public function executeDuniyaWalo(sfWebRequest $request)
+  {
+        echo "33";die;
+  }
+}
