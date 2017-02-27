151ad802fd80b7ddfce132389140fcfeecf1f755
kunal.verma <kunal.verma@jeevansathi.com>
2017-02-27 18:31:12 +0530

    [QASanityReleaseNew] ForceAllow Css/Js

--- web/profile/commonfile_functions.php
+++ web/profile/commonfile_functions.php
@@ -462,7 +462,7 @@ $js_arr[]=array("jsms/angular/registration/hamburger_reg_js"            => "15")
 $js_arr[]=array("jsms/contact_engine/contactEngine_js"                                                             => "69");
 $js_arr[]=array("jsms/profile/viewProfile/jsmsViewProfile_js"                  => "15");
 $js_arr[]=array("jsms/search/inview"   => "2");
-$js_arr[]=array("jsms/login/newMobLogin_js"                                    => "36");
+$js_arr[]=array("jsms/login/newMobLogin_js"                                     => "37");
 $js_arr[]=array("jsms/login/mobForgotPass_js"            => "13");
 $js_arr[]=array("jsms/login/resetPass_js"   => "2");
 $js_arr[]=array("jsms/hamburger/ham_js"                   => "41");
