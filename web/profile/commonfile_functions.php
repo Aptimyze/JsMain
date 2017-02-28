132050f4c4b9b1f54760fb4a8c00f323ef6b0ea4
ankita.g <ankita.g@jeevansathi.com>
2017-02-28 10:43:17 +0530

    [JSC-2503_V1] JSC-2503

--- web/profile/commonfile_functions.php
+++ web/profile/commonfile_functions.php
@@ -465,7 +465,7 @@ $js_arr[]=array("jsms/search/inview"   => "2");
 $js_arr[]=array("jsms/login/newMobLogin_js"                                     => "37");
 $js_arr[]=array("jsms/login/mobForgotPass_js"            => "13");
 $js_arr[]=array("jsms/login/resetPass_js"   => "2");
-$js_arr[]=array("jsms/hamburger/ham_js"                    => "42");
+$js_arr[]=array("jsms/hamburger/ham_js"                    => "45");
 $js_arr[]=array("jsms/settings/mobChangePass_js"   => "2");
 $js_arr[]=array("jsms/404/mob404_js"  => "1");
 $js_arr[]=array("jsms/search/searchByProfileId_js"      => "5");
