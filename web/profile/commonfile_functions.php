53abdf8c87b24044def50df841cafd1087f9eb6b
ankita.g <ankita.g@jeevansathi.com>
2017-03-06 10:46:41 +0530

    [androidChatV1] ForceAllow Css/Js

--- web/profile/commonfile_functions.php
+++ web/profile/commonfile_functions.php
@@ -605,7 +605,7 @@ $js_arr[]=array("common_comscore_js"   => "2");
 $js_arr[]=array("jspc/chat/chatCommonConfig_js"                                               => "95");
 $js_arr[]=array("jspc/chat/chatPCHelper_js"                                                => "130");
 $js_arr[]=array("jspc/chat/chatPlugin_js"                                                             => "99");
-$js_arr[]=array("jspc/chat/strophieWrapper_js"                                            => "58");
+$js_arr[]=array("jspc/chat/strophieWrapper_js"                                             => "59");
 $js_arr[]=array("jspc/chat/chatStrophieClient_js"      => "7");
 $js_arr[]=array("jspc/help/JSPCHelp_js"              => "18");
 $js_arr[]=array("jsms/help/JSMSHelp_js"            => "14");
