b109334dadcfc9732e5ad1afc360df1f27af7165
nitish.sharma <nitish.sharma@99acres.com>
2017-03-09 14:13:04 +0530

    [JSC-2513] ForceAllow Css/Js

--- web/profile/commonfile_functions.php
+++ web/profile/commonfile_functions.php
@@ -605,7 +605,7 @@ $js_arr[]=array("jsms/common/disableRightClick_js"    => "2");
 $js_arr[]=array("jsms/profile/horoscope/horoscope_js"  => "1");
 $js_arr[]=array("common_comscore_js"   => "2");
 $js_arr[]=array("jspc/chat/chatCommonConfig_js"                                                => "96");
-$js_arr[]=array("jspc/chat/chatPCHelper_js"                                                 => "131");
+$js_arr[]=array("jspc/chat/chatPCHelper_js"                                                  => "132");
 $js_arr[]=array("jspc/chat/chatPlugin_js"                                                             => "100");
 $js_arr[]=array("jspc/chat/strophieWrapper_js"                                            => "58");
 $js_arr[]=array("jspc/chat/chatStrophieClient_js"      => "7");
