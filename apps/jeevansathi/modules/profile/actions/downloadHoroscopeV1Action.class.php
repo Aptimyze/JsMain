3a32704b797e249992e3a00fb1d84cb25690b496
sanyam.chopra <sanyam.chopra@99acres.com>
2017-03-10 10:50:53 +0530

    [JSM-3435] JSM-3435

--- apps/jeevansathi/modules/profile/actions/downloadHoroscopeV1Action.class.php
+++ apps/jeevansathi/modules/profile/actions/downloadHoroscopeV1Action.class.php
@@ -23,17 +23,22 @@ class downloadHoroscopeV1Action extends sfActions
 			$apiResponseHandlerObj->generateResponse();
 			die;
 		}
-		//$urlToCall = "http://mq.jeev.com/profile/horoscope_astro.php?SAMEGENDER=&FILTER=&ERROR_MES=&view_username="+ViewedUserName+"&SIM_USERNAME="+ViewedUserName+"&type=Horoscope&ajax_error=2&checksum=&profilechecksum="+ProCheckSum+"&randValue=890&GENDERSAME="+sameGender,'";
+		
 		$url = "http://mq.jeev.com/profile/horoscope_astro.php?SAMEGENDER=&FILTER=&ERROR_MES=&view_username=ZZTA7026&SIM_USERNAME=ZZTA7026&type=Horoscope&ajax_error=2&checksum=&profilechecksum=4df240233ba9f7e19b88a74558885a15i9376853&randValue=890&showDownload=1&GENDERSAME=";
+		
+		//$url = "http://vendors.vedic-astrology.net/cgi-bin/JeevanSathi_DrawChart_matchstro.dll?DrawChart?469:563:1980:05:20:01:55:00:Bhandara%20District,%20Maharashtra:328.446833:35.40627:103.367058:131.573784:43.198575:127.486461:68.529836:146.619363:120.909642:300.909642:0:0:1:0:0:-1:1:2";
+		
 		$file=PdfCreation::PdfFile($url);
-		header("Content-type:application/pdf");
+		PdfCreation::setResponse("myfile.pdf",$file);
+
+		// header("Content-type: application/pdf");
 
 		// It will be called downloaded.pdf
-		header("Content-Disposition:attachment;filename='downloaded.pdf'");
+		// header("Content-Disposition:attachment;filename='downloaded.pdf'");
 
 		// The PDF source is in original.pdf
-		readfile($file);
-		//print_R($file);die("SSS");
+		// readfile($file);
+		// print_R($file);die("SSS");
 		return sfView::NONE;
 	}
 }
