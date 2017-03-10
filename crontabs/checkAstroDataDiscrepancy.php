d3a78164bc8dd0999611592a5305aaf3596d19a1
lavesh <ankit.shukla@jeevansathi.com>
2017-03-10 17:48:41 +0530

    [JSM-3681] cron astro update query

--- crontabs/checkAstroDataDiscrepancy.php
+++ crontabs/checkAstroDataDiscrepancy.php
@@ -11,11 +11,16 @@ include_once(JsConstants::$docRoot."/classes/globalVariables.Class.php");
 
 global $mysqlObjS;
 
+$mysqlObjM = new Mysql;
+$connMaster = $mysqlObjM->connect("master") or logError("Unable to connect to master","ShowErrTemplate");
+mysql_query('set session wait_timeout=10000,interactive_timeout=10000,net_read_timeout=10000',$connMaster);
+
 $mysqlObjS = new Mysql;
 $connSlave = $mysqlObjS->connect("slave") or logError("Unable to connect to slave","ShowErrTemplate");
 mysql_query('set session wait_timeout=10000,interactive_timeout=10000,net_read_timeout=10000',$connSlave);
 
-$searchTables = array(0=>array("type"=>"MALE","number"=>"553000"),1=>array("type"=>"FEMALE","number"=>"264000"));
+
+$searchTables = array(0=>array("type"=>"MALE","number"=>"555000"),1=>array("type"=>"FEMALE","number"=>"266000"));
 
 $file = fopen(sfConfig::get("sf_upload_dir")."/SearchLogs/astroCron".$date.".txt","a");
 
@@ -25,7 +30,6 @@ foreach($searchTables as $key=>$val){
     for($counter = 0;$counter<$val["number"];$counter=$counter+2000 ){
         $sqlSearch = "SELECT PROFILEID FROM SEARCH_".$val['type']." LIMIT 2000 OFFSET ".$counter ;
         $profileidArr = $mysqlObjS->executeQuery($sqlSearch,$connSlave) or $mysqlObjS->logError($sqlSearch);
-        
         $profileStr='';
         while($row = $mysqlObjS->fetchAssoc($profileidArr)){
             $profile = $row['PROFILEID'];
@@ -34,11 +38,14 @@ foreach($searchTables as $key=>$val){
         
         $profileStr = trim($profileStr,",");
         if($profileStr!=''){
-            $sqlJprofile = "SELECT J.PROFILEID FROM JPROFILE J LEFT JOIN ASTRO_DETAILS A ON J.PROFILEID=A.PROFILEID WHERE (J.BTIME!='' OR J.COUNTRY_BIRTH!='' OR J.CITY_BIRTH!='') AND A.PROFILEID IS NULL AND J.PROFILEID IN ($profileStr)";
+            $sqlJprofile = "SELECT J.PROFILEID FROM JPROFILE J LEFT JOIN ASTRO_DETAILS A ON J.PROFILEID=A.PROFILEID WHERE (J.BTIME!='' OR (J.COUNTRY_BIRTH!='' AND J.COUNTRY_BIRTH!=0) OR J.CITY_BIRTH!='') AND A.PROFILEID IS NULL AND J.PROFILEID IN ($profileStr)";
             $result = $mysqlObjS->executeQuery($sqlJprofile,$connSlave) or $mysqlObjS->logError($sqlJprofile);
             while($newRow = $mysqlObjS->fetchAssoc($result)){
                 fwrite($file,$newRow['PROFILEID']."\n");
                 $noOfProfiles++;
+                
+                $sqlJprofile = "UPDATE JPROFILE SET BTIME='',COUNTRY_BIRTH='',CITY_BIRTH='' WHERE PROFILEID =".$newRow['PROFILEID'];
+				$result = $mysqlObjS->executeQuery($sqlJprofile,$connMaster) or $mysqlObjS->logError($sqlJprofile);
             }
         }
     
