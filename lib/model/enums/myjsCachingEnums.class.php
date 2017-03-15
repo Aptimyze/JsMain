9717a178994ea1c1aca830d2cf16747e4aa57700
Ayush Sethi <ayushsethi22031992@gmail.com>
2017-03-15 15:56:40 +0530

    [JSI-2841_v2] Removing Junk Enum

--- lib/model/enums/myjsCachingEnums.class.php
+++ lib/model/enums/myjsCachingEnums.class.php
@@ -6,7 +6,6 @@
 	{
 		const PREFIX = 'OCB_';
 		const TIME = 1800;
-		const TIME_DIFFERENCE_PROFILE_SCREENING = '-30 minutes';
 		const TOP_PART = 'top';
 		const BOTTOM_PART = 'bottom';
 		const PAGEID = 'pageId';
