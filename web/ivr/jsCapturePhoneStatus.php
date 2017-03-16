0e0ea10550b4c4cc1298649f83229b750544977a
Palash Chordia <palash.chordia@jeevansathi.com>
2017-03-16 15:02:48 +0530

    [CIRelease] removed tracking

--- web/ivr/jsCapturePhoneStatus.php
+++ web/ivr/jsCapturePhoneStatus.php
@@ -25,11 +25,6 @@ if($virtualno && $phoneno)
 
 			$verificationObj=new MissedCallVerification($phoneno,$virtualno);
 			$verified = $verificationObj->phoneUpdateProcess("KNW");
-                        if (!$verified){
-                        	$time = date('Y-m-d H:i:s');
-                        SendMail::send_email('palashc2011@gmail.com',"$phoneno $virtualno ".$verificationObj->getTempText(),'missedcallVer');
-                        JsMemcache::getInstance()->setHashObject('missLog_'.$phoneno,array('hitVno'=>$virtualno,'time'=>$time));
-                    }
 	}
 $xmlStr= phoneKnowlarity::genrate_xml();
 echo $xmlStr;
