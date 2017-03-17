dbb2aee7c0d4a6b81d13f26e1ac4088a764bd248
Ayush Sethi <ayushsethi22031992@gmail.com>
2017-03-17 12:09:57 +0530

    [JSI-2826] SMS For Report Invalid on Options

--- web/profile/InstantSMS.php
+++ web/profile/InstantSMS.php
@@ -9,14 +9,14 @@ class InstantSMS {
 	private $otherProfileDetails = array();
 	private $smsSettings = array();
 	private $varArray = array();
-	private $smsTypeIgnoreTimeRange = array("DETAIL_CONFIRM","FORGOT_PASSWORD","PAYMENT_MEMBERSHIP","VIEWED_CONTACT_SMS","FIELD_VISIT_SCHEDULE","OTP","DEL_OTP","MEM_REN_ACT_CRON","MEM_BACK_DISC_SMS","CRM_SMS_BRANCH","CRM_SMS_OFFER","CRM_SMS_NOT_REACH","CRM_SMS_APP_DOWNLOAD","REQ_CRM_DEL_SELF","REQ_CRM_DEL_OTHER");
+	private $smsTypeIgnoreTimeRange = array("DETAIL_CONFIRM","FORGOT_PASSWORD","PAYMENT_MEMBERSHIP","VIEWED_CONTACT_SMS","FIELD_VISIT_SCHEDULE","OTP","DEL_OTP","MEM_REN_ACT_CRON","MEM_BACK_DISC_SMS","CRM_SMS_BRANCH","CRM_SMS_OFFER","CRM_SMS_NOT_REACH","CRM_SMS_APP_DOWNLOAD","REQ_CRM_DEL_SELF","REQ_CRM_DEL_OTHER","REPORT_INVALID");
 	private $errorMessage = "Due to a temporary problem your request could not be processed. Please try after a couple of minutes";
 	private $unverified_key = array("REGISTER_RESPONSE" ,"PHONE_UNVERIFY");
 	private $customCriteria=0;
-	private $settingIndependent = array("FORGOT_PASSWORD","VIEWED_CONTACT_SMS","OTP", "PHONE_UNVERIFY","DEL_OTP","REQ_CRM_DEL_SELF","REQ_CRM_DEL_OTHER");
+	private $settingIndependent = array("FORGOT_PASSWORD","VIEWED_CONTACT_SMS","OTP", "PHONE_UNVERIFY","DEL_OTP","REQ_CRM_DEL_SELF","REQ_CRM_DEL_OTHER","REPORT_INVALID");
 	private $sendToInternational = array("FORGOT_PASSWORD");
 	private $eoiSMSLimit = 2;
-	private $otherProfileRequired = array("INSTANT_EOI","ACCEPTANCE_VIEWED","ACCEPTANCE_VIEWER","VIEWED_CONTACT_SMS","HOROSCOPE_REQUEST");
+	private $otherProfileRequired = array("INSTANT_EOI","ACCEPTANCE_VIEWED","ACCEPTANCE_VIEWER","VIEWED_CONTACT_SMS","HOROSCOPE_REQUEST","REPORT_INVALID");
 	private $kycCity = array("DE00", "UP25", "UP06", "RA07", "UP47", "UP12");
 	private $kycLocality = "";//Comma separated
 	
@@ -81,6 +81,7 @@ include_once(JsConstants::$docRoot."/commonFiles/SymfonyPictureFunctions.class.p
 		if($this->smsKey=='PHONE_UNVERIFY') return true;
 		if($this->smsKey=='REQ_CRM_DEL_SELF') return true;
 		if($this->smsKey=='REQ_CRM_DEL_OTHER') return true;
+		if($this->smsKey == 'REPORT_INVALID') return true;
 		
 		$sendToInt = in_array($this->smsKey, $this->sendToInternational);
 		if(!$sendToInt && !$this->SMSLib->getMobileCorrectFormat($this->profileDetails["PHONE_MOB"],$this->profileDetails["ISD"], $sendToInt))
@@ -148,6 +149,9 @@ include_once(JsConstants::$docRoot."/commonFiles/SymfonyPictureFunctions.class.p
 
 				 case "REQ_CRM_DEL_OTHER":
 				 	return true;
+
+				 case "REPORT_INVALID":
+				 	return true;	
 
 			default:
 				return $this->profileDetails["MOB_STATUS"] == 'Y';
@@ -219,8 +223,27 @@ include_once(JsConstants::$docRoot."/commonFiles/SymfonyPictureFunctions.class.p
 			}
                     }
                 }
+
+                if($this->smsKey == "REPORT_INVALID")
+                {
+                	$rowInvalid['OTHER_EMAIL'] = $row['EMAIL'];
+                	$rowInvalid['USERNAME'] = $row['USERNAME'];
+                	$rowInvalid['CITY_RES'] = $row['CITY_RES'];
+                	$rowInvalid['COUNTRY_RES'] = $row['COUNTRY_RES'];
+                	$rowInvalid['PROFILEID'] = $row['PROFILEID'];
+                	if($row['SHOWPHONE_MOB'] == 'Y')
+                	{	
+                		$rowInvalid['ISD_PHONE_MOB'] = '+'.$row['ISD'].$row['PHONE_MOB'];
+               		}
+
+                }
+                if($this->SMS_KEY == "REPORT_INVALID")
+                {
+                $this->otherProfileDetails["DATA"] = $rowInvalid;			
+                }
+                else{
                 $this->otherProfileDetails["DATA"] = $row;		
-		
+				}
 	}
 	
 	private function getActualMessage ($message) {
