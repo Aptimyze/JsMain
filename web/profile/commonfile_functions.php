40b437b283749b723b1a74c28771de31b90779e2
Nikhil <nikhil.mittal@jeevansathi.com>
2017-03-20 12:30:57 +0530

    [JSI-2760] ForceAllow Css/Js

--- web/profile/commonfile_functions.php
+++ web/profile/commonfile_functions.php
@@ -572,7 +572,7 @@ $js_arr[]=array("jspc/profile/edit/profile/jspcEditProfile_js"   => "90");
 $js_arr[]=array("jspc/profile/view/jspcViewProfile_js" => "55");
 $js_arr[]=array("jspc/success_story/foggy"  => "1");
 $js_arr[]=array("jspc/success_story/resizeIe"  => "1");
-$js_arr[]=array("jspc/contactEngine/contactEngine"                                    => "47");
+$js_arr[]=array("jspc/contactEngine/contactEngine"                                     => "48");
 $js_arr[]=array("jspc/contactEngine/contactEngineButtons"              => "15");
 $js_arr[]=array("jspc/search/advancedSearch"                 => "15");
 $js_arr[]=array("jspc/common/jquery_mCustomScrollbar"              => "13");
