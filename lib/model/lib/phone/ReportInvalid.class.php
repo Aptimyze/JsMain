389c15fbc447360e7c85f54caf02f70c0d3a7358
Ayush Sethi <ayushsethi22031992@gmail.com>
2017-03-21 12:07:49 +0530

    [JSI-2873] Adding Paid Check for QC

--- lib/model/lib/phone/ReportInvalid.class.php
+++ lib/model/lib/phone/ReportInvalid.class.php
@@ -9,13 +9,14 @@ class ReportInvalid
 		
 		$timeNow = date('Y-m-d H:i:s');
 		$timeDaysAgo = date('Y-m-d H:i:s', strtotime('-'.self::$reportInvalidCountDuration.' days'));
+		$loginProfile = LoggedInProfile::getInstance();
 		$reportInvalidObj = new JSADMIN_REPORT_INVALID_PHONE();
 
 		$countInvalids = $reportInvalidObj->getReportInvalidCountSubmitter($profileId , $timeNow , $timeDaysAgo);
 
 		$previousEntryExists = (new JSADMIN_REPORT_INVALID_PHONE)->entryExistsForPair($profileId,$submittee);
-
-		if($countInvalids < 10 && !$previousEntryExists)
+ 
+		if($countInvalids < 10 && !$previousEntryExists && $loginProfile->getPROFILE_STATE()->getPaymentStates()->isPAID())
 		{	
 			$contactsAllotedObj = new jsadmin_CONTACTS_ALLOTED();
    			$contactsAllotedObj->updateAllotedContacts($profileId,1);
