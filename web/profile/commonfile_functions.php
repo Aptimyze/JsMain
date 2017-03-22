c4e6f2548b7f1ac77e286b3affffeb1866091cec
Nikhil <nikhil.mittal@jeevansathi.com>
2017-03-22 10:43:53 +0530

    [JSI-2760] ForceAllow Css/Js

--- web/profile/commonfile_functions.php
+++ web/profile/commonfile_functions.php
@@ -460,7 +460,7 @@ $js_arr[]=array("jsms/angular/registration/scripts/services"    => "4");
 $js_arr[]=array("jsms/angular/registration/scripts/directives"                  => "23");
 $js_arr[]=array("jsms/angular/registration/scripts/factories"                                  => "39");
 $js_arr[]=array("jsms/angular/registration/hamburger_reg_js"              => "17");
-$js_arr[]=array("jsms/contact_engine/contactEngine_js"                                                                 => "75");
+$js_arr[]=array("jsms/contact_engine/contactEngine_js"                                                                  => "76");
 $js_arr[]=array("jsms/profile/viewProfile/jsmsViewProfile_js"                  => "15");
 $js_arr[]=array("jsms/search/inview"   => "2");
 $js_arr[]=array("jsms/login/newMobLogin_js"                                      => "41");
