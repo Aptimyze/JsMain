39b7110726ba92a08e30b1d079f746444fcf7143
Ayush Sethi <ayushsethi22031992@gmail.com>
2017-03-23 20:17:42 +0530

    [JSI-2826] Adding Sql for SMS

---
+++ web/sql_builds/JSI-2826.sql
@@ -0,0 +1,1 @@
+INSERT INTO `SMS_TYPE` VALUES (227, 'REPORT_INVALID', 'I', 2, 'A', 'A', 'SINGLE', 0, 0, 'SERVICE', 'Y', 'JS user {OTHER_USERNAME} from {CITY_RES} (Profile: {URL_PROFILE} ; {REPORT_INVALID_PHONE_ISD_COMMA}{OTHER_EMAIL}) tried to call you. Pls contact when free.');
