839aefebc7c88f14d712bb7e26a038a5cc782cae
bhavana <bhavana.kadwal@jeevansathi.com>
2017-03-29 16:19:29 +0530

    [CIRelease] memcache change

--- crontabs/logDailySearchCount.php
+++ crontabs/logDailySearchCount.php
@@ -10,7 +10,7 @@ $mysqlObj=new Mysql;
 $db=connect_db();
 mysql_query("set session wait_timeout=1000",$db);
 $dt = date("d",strtotime("-1 day"));
-$memcache = JsMemcache::getInstance();
+$memcache = new UserMemcache;
 $counter = $memcache->get("TOTAL_SEARCH_COUNT_".$dt,NULL,0,0);
 if($counter != 0 && $counter != ''){
         $sql = "REPLACE INTO search.DAILY_SEARCH_COUNT VALUES ('".date("Y-m-d")."',$counter,NULL)";
