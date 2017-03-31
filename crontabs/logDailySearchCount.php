0f8f0679483dfc05b2cb55926cad5b8a688f355a
bhavana <bhavana.kadwal@jeevansathi.com>
2017-03-31 16:33:39 +0530

    [JSM-3845] log java count

--- crontabs/logDailySearchCount.php
+++ crontabs/logDailySearchCount.php
@@ -12,8 +12,21 @@ mysql_query("set session wait_timeout=1000",$db);
 $dt = date("d",strtotime("-1 day"));
 $memcache = new UserMemcache;
 $counter = $memcache->get("TOTAL_SEARCH_COUNT_".$dt,NULL,0,0);
+
+$dtZero = str_pad($dt, 2, "0", STR_PAD_LEFT); 
+$javaKeys = array("time_l500","time_l1000","time_l2000","time_l3000","time_g3000");
+$javaPrefixKey = "listingDppPerformance";
+$javacount = 0;
+for($i=0;$i<=23;$i++){
+     $hr = str_pad($i, 2, "0", STR_PAD_LEFT);
+     $key= $javaPrefixKey.$dtZero."_".$hr;
+     foreach($javaKeys as $jKeys){
+             $counter = $memcache->get($key.$jKeys,NULL,0,0);
+             $javacount += $counter;
+     }
+}
 if($counter != 0 && $counter != ''){
-        $sql = "REPLACE INTO search.DAILY_SEARCH_COUNT VALUES ('".date("Y-m-d",strtotime("-1 day"))."',$counter,NULL)";
+        $sql = "REPLACE INTO search.DAILY_SEARCH_COUNT VALUES ('".date("Y-m-d",strtotime("-1 day"))."',$counter,$javacount,NULL)";
         mysql_query($sql,$db) or die("DAILY_SEARCH_COUNT".mysql_error1($db));
 }
 $memcache->delete("TOTAL_SEARCH_COUNT_".date("d",strtotime("-20 day")));
