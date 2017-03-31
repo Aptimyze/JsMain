0d33044fb3ed727a4495f2943b7e2157ee11d0ff
sanyam.chopra <sanyam.chopra@99acres.com>
2017-03-31 11:23:07 +0530

    [QASanityReleaseNew] ForceAllow Css/Js

--- web/profile/commonfile_functions.php
+++ web/profile/commonfile_functions.php
@@ -606,7 +606,7 @@ $js_arr[]=array("jsms/common/disableRightClick_js"    => "2");
 $js_arr[]=array("jsms/profile/horoscope/horoscope_js"  => "1");
 $js_arr[]=array("common_comscore_js"   => "2");
 $js_arr[]=array("jspc/chat/chatCommonConfig_js"                                                        => "104");
-$js_arr[]=array("jspc/chat/chatPCHelper_js"                                                    => "140");
+$js_arr[]=array("jspc/chat/chatPCHelper_js"                                                     => "141");
 $js_arr[]=array("jspc/chat/chatPlugin_js"                                                                  => "107");
 $js_arr[]=array("jspc/chat/strophieWrapper_js"                                               => "62");
 $js_arr[]=array("jspc/chat/chatStrophieClient_js"      => "7");
