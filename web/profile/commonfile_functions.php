d57199f7ab48507c2734195ecd6bdb45d203a9fc
Reshu <reshu.rajput@jeevansathi.com>
2017-04-04 11:27:14 +0530

    [JSM-3847_v1] ForceAllow Css/Js

--- web/profile/commonfile_functions.php
+++ web/profile/commonfile_functions.php
@@ -573,7 +573,7 @@ $js_arr[]=array("jspc/profile/view/jspcViewProfile_js" => "55");
 $js_arr[]=array("jspc/success_story/foggy"  => "1");
 $js_arr[]=array("jspc/success_story/resizeIe"  => "1");
 $js_arr[]=array("jspc/contactEngine/contactEngine"                              => "45");
-$js_arr[]=array("jspc/contactEngine/contactEngineButtons"              => "15");
+$js_arr[]=array("jspc/contactEngine/contactEngineButtons"               => "16");
 $js_arr[]=array("jspc/search/advancedSearch"                 => "15");
 $js_arr[]=array("jspc/common/jquery_mCustomScrollbar"              => "13");
 $js_arr[]=array("jspc/success_story/jqueryFilestyle" => "1");
