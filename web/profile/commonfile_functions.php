c864e8f25e7dcd6971aa1bec14ecffd213831a49
sanyam.chopra <sanyam.chopra@99acres.com>
2017-04-05 17:08:45 +0530

    [JSM-3852] ForceAllow Css/Js

--- web/profile/commonfile_functions.php
+++ web/profile/commonfile_functions.php
@@ -462,7 +462,7 @@ $js_arr[]=array("jsms/angular/registration/scripts/directives"                  
 $js_arr[]=array("jsms/angular/registration/scripts/factories"                                    => "41");
 $js_arr[]=array("jsms/angular/registration/hamburger_reg_js"              => "17");
 $js_arr[]=array("jsms/contact_engine/contactEngine_js"                                                              => "73");
-$js_arr[]=array("jsms/profile/viewProfile/jsmsViewProfile_js"                          => "23");
+$js_arr[]=array("jsms/profile/viewProfile/jsmsViewProfile_js"                           => "24");
 $js_arr[]=array("jsms/search/inview"   => "2");
 $js_arr[]=array("jsms/login/newMobLogin_js"                                      => "41");
 $js_arr[]=array("jsms/login/mobForgotPass_js"            => "13");
