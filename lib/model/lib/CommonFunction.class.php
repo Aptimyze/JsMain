45717afc479bbf78f0c78553abfb19efe05090ea
Nikhil <nikhil.mittal@jeevansathi.com>
2017-04-11 01:08:07 +0530

    [JSI-2956] limits

--- lib/model/lib/CommonFunction.class.php
+++ lib/model/lib/CommonFunction.class.php
@@ -123,6 +123,31 @@ class CommonFunction
 		$limit['NOT_VALIDNUMBER_LIMIT']=$notValidNumber_limit;
 		return $limit;		
 	}
+
+	public static function getContactLimitDates($contactDate)
+	{
+		$verifyDate = "2017-03-16 17:03:42";
+		$contactDate = "2017-04-10 23:21:12";
+
+		$x = date('Y-m-d',strtotime($verifyDate));
+		$y = date('Y-m-d',strtotime($contactDate));
+
+		$t1 = strtotime($x);
+		$t2 = strtotime($y);
+
+		$daysDiff = ($t2 - $t1)/(24*60*60);
+
+		$weeks = floor($daysDiff/7) * 7;
+
+		$weekStartDate = date('Y-m-d', strtotime($x. " + $weeks days"));
+
+		$months = floor($daysDiff/30) * 30;
+
+		$monthStartDate = date('Y-m-d', strtotime($x. " + $months days"));
+
+		return array('weekStartDate' => $weekStartDate, 'monthStartDate' => $monthStartDate);
+	}
+
 	public static function isPaid($subscription)
 	{
 		$paid = 0;
