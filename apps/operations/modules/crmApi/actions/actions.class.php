9a55c2fba6e0877c1c95704e63680d15469d9553
nitish.sharma <nitish.sharma@99acres.com>
2017-04-19 15:59:42 +0530

    [JSC-2450] JSC-2450 api code

--- apps/operations/modules/crmApi/actions/actions.class.php
+++ apps/operations/modules/crmApi/actions/actions.class.php
@@ -336,6 +336,51 @@ class crmApiActions extends sfActions
     $respObj->generateResponse();
     die;
   }
+  
+  /*Api to fetch nearest branch details of profile based on city.
+    * @inputs: $request
+    * @return: api response
+    */
+    public function executeSmsNearestBranchDetailsV1(sfWebRequest $request){
+        $profileid = $request->getParameter('profileid');
+        $respObj = ApiResponseHandler::getInstance();
+        if($profileid){
+            $jprofileObj = new JPROFILE('newjs_slave');
+            $profileDetails = $jprofileObj->get($profileid, "PROFILEID","USERNAME,CITY_RES");
+            $username = $profileDetails['USERNAME'];
+            $newjsConObj = new NEWJS_CONTACT_US('newjs_slave');
+            $arrResult = array();
+            $newjsConObj->fetch_All_Contact($arrResult);
+            foreach($arrResult as $key=>$val) {
+                if (empty($branchArr[$val['STATE_VAL']][$val['CITY_ID']])) {
+                    $branchArr[$val['STATE_VAL']][$val['CITY_ID']] = $val['ADDRESS'];
+                }
+            }
+            $cityRes       = $profileDetails["CITY_RES"];
+            $state = ucwords(preg_replace("/[^A-Z]+/", "", $cityRes));
+            if (in_array($state, array_keys($branchArr))) {
+                if (in_array($cityRes, array_keys($branchArr[$state]))) {
+                    $branchAddress = $branchArr[$state][$cityRes]; 
+                } else {
+                    $branchAddress = array_values($branchArr[$state])[0]; 
+                }
+            } 
+            else {
+                $branchAddress = $branchAddress = array_values($branchArr['DE'])[0]; 
+            }
+            $branchAddress = wordwrap($branchAddress, 120);
+            $tokenArr      = array("BRANCH_ADDRESS" => $branchAddress);
+            CommonUtility::sendPlusTrackInstantSMS('CRM_SMS_BRANCH', $profileid, $tokenArr);
+            $respObj->setHttpArray(CrmResponseHandlerConfig::$CRM_SUCCESS);
+        }
+        else{
+            $respObj->setHttpArray(CrmResponseHandlerConfig::$MISSING_PARAM);
+            $output = array("response"=>"Parameter missing");
+        }
+        $respObj->setResponseBody($output);
+        $respObj->generateResponse();
+        die();
+    }
 
 }
 ?>
