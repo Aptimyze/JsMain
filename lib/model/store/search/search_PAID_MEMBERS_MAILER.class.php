d7bfafa1c94ea47a0eb6f6a89a0145132d364463
sanyam.chopra <sanyam.chopra@99acres.com>
2017-04-19 17:21:40 +0530

    [JSM-3907] JSM-3907 added new classes and sql build

---
+++ lib/model/store/search/search_PAID_MEMBERS_MAILER.class.php
@@ -0,0 +1,70 @@
+<?php
+class search_PAID_MEMBERS_MAILER extends TABLE
+{
+	public function __construct($dbname = "") {
+		parent::__construct($dbname);
+	}
+
+	/* This function is used to get all the profile which need to recieve paidMembersMail 
+    * @param fields : fields to get if different from default
+    * @param totalScript : number of script which can be executed
+    * @param script : current script number
+    * @param limit : limit if required
+    * @return result : details of mailer to be sent 
+    */
+    public function getMailerProfiles($fields="",$totalScript="1",$script="0",$limit="")
+    {
+        try 
+        {
+            $defaultFields ="SNO,RECEIVER,USER1,USER2,USER3,USER4,USER5,USER6,USER7,USER8,USER9,USER10,USER11,USER12,USER13,USER14,USER15,USER16";
+
+            $selectfields = $fields?$fields:$defaultFields;
+            $sql = "SELECT $selectfields FROM search.send_saved_search_mail where SENT IN ('U','F') AND  MOD(SNO,:TOTAL_SCRIPT)=:SCRIPT";
+            if($limit)
+                $sql.= " limit 0,:LIMIT";
+            $prep = $this->db->prepare($sql);
+            $prep->bindValue(":TOTAL_SCRIPT",$totalScript,PDO::PARAM_INT);
+            $prep->bindValue(":SCRIPT",$script,PDO::PARAM_INT);
+            if($limit)
+                  $prep->bindValue(":LIMIT",$limit,PDO::PARAM_INT);
+            $prep->execute();
+            
+            while($row = $prep->fetch(PDO::FETCH_ASSOC))
+            {
+                if(!$fields)
+                {
+                    $fieldsArray = explode(",",$defaultFields);
+                    foreach($fieldsArray as $k=>$v)
+                    {
+                        $result[$row["SNO"]][$v]=$row[$v];
+                    }
+                }
+                else
+                    $result[] = $row;
+                unset($result[$row["SNO"]]["SNO"]);
+            }
+            return $result;         
+        }
+        catch (PDOException $e)
+        {
+            throw new jsException($e);
+        }
+    }
+
+    public function updatePaidMembersReceiverFlag($sno,$flag,$profileId)
+    {
+    	try
+		{
+			$sql = "UPDATE mailer.FEATURED_PROFILE_MAILER SET SENT=:FLAG WHERE PROFILEID=:PROFILEID AND SNO=:SNO";
+			$prep = $this->db->prepare($sql);
+			$prep->bindValue(":FLAG",$flag,PDO::PARAM_STR);
+			$prep->bindValue(":PROFILEID",$profileId,PDO::PARAM_INT);
+			$prep->bindValue(":SNO",$sno,PDO::PARAM_INT);
+			$prep->execute();
+		}
+		catch (PDOException $e)
+		{
+			throw new jsException($e);
+		}
+    }
+}
---
+++ web/sql_builds/JSM-3907.sql
@@ -0,0 +1,32 @@
+use search;
+
+CREATE TABLE  `PIADMEMBERS_TO_BE_SENT` (
+ `PROFILEID` INT( 11 ) UNSIGNED NOT NULL DEFAULT  '0',
+ `IS_CALCULATED` ENUM(  'N',  'Y' ) NOT NULL DEFAULT  'N',
+PRIMARY KEY (  `PROFILEID` )
+) ENGINE = MYISAM;
+
+	CREATE TABLE `PAID_MEMBERS_MAILER` (
+ `RECEIVER` int(11) unsigned DEFAULT '0',
+ `USER1` int(11) unsigned DEFAULT '0',
+ `USER2` int(11) unsigned DEFAULT '0',
+ `USER3` int(11) unsigned DEFAULT '0',
+ `USER4` int(11) unsigned DEFAULT '0',
+ `USER5` int(11) unsigned DEFAULT '0',
+ `USER6` int(11) unsigned DEFAULT '0',
+ `USER7` int(11) unsigned DEFAULT '0',
+ `USER8` int(11) unsigned DEFAULT '0',
+ `USER9` int(11) unsigned DEFAULT '0',
+ `USER10` int(11) unsigned DEFAULT '0',
+ `USER11` int(11) unsigned DEFAULT '0',
+ `USER12` int(11) unsigned DEFAULT '0',
+ `USER13` int(11) unsigned DEFAULT '0',
+ `USER14` int(11) unsigned DEFAULT '0',
+ `USER15` int(11) unsigned DEFAULT '0',
+ `USER16` int(11) unsigned DEFAULT '0',
+ `SNO` mediumint(11) NOT NULL AUTO_INCREMENT,
+ `SENT` varchar(1) NOT NULL DEFAULT 'N',
+ PRIMARY KEY (`SNO`),
+ KEY `SENT` (`SENT`),
+) ENGINE=MYISAM;
+
