9a5033a13fa76a0fa7021260ba2bc84b116f5393
root <root@localhost.localdomain>
2017-04-19 15:48:28 +0530

    JSC-2558

--- crontabs/service_entry_cron.php
+++ crontabs/service_entry_cron.php
@@ -12,12 +12,14 @@ $today=date("Y-m-d",$ts);
 //$today ="2014-04-15";
 $st_date=$today." 00:00:00";
 $end_date=$today." 23:59:59";
-
+$st_date="2017-04-12 00:00:00";
+$end_date="2017-04-17 23:59:59";
 /* Section for newly purchased services */
-$sql ="SELECT COUNT(*) cnt,A.SERVICEID,B.CENTER FROM billing.SERVICE_STATUS AS A JOIN billing.PURCHASES AS B ON A.BILLID=B.BILLID WHERE B.ENTRY_DT BETWEEN '$st_date' AND '$end_date' AND B.STATUS='DONE' GROUP BY A.SERVICEID,CENTER";
+$sql ="SELECT COUNT(*) cnt,A.SERVICEID,B.CENTER, MEM_UPGRADE AS UPGRADE FROM billing.SERVICE_STATUS AS A JOIN billing.PURCHASES AS B ON A.BILLID=B.BILLID WHERE B.ENTRY_DT BETWEEN '$st_date' AND '$end_date' AND B.STATUS='DONE' GROUP BY A.SERVICEID,CENTER";
 $res=mysql_query_decide($sql,$db_slave) or die("$sql".mysql_error_js($db_slave));
 while($row=mysql_fetch_array($res))
 {
+        if($row['UPGRADE']!='MAIN'){
 	$count		=$row["cnt"];
 	$serviceid	=$row["SERVICEID"];
 	$center		=$row["CENTER"];
@@ -79,10 +81,94 @@ while($row=mysql_fetch_array($res))
 		$paidCnt = $paidCount[$center][$serviceid];
 		$freeCnt = $count - $paidCnt;
 		$sqlIns="insert into MIS.SERVICE_DETAILS(`ENTRY_DT`,`COUNT`,`SERVICE`,`BRANCH`,`FREE_COUNT`,`PAID_COUNT`) VALUES('$today','$count','$serviceid','$center','$freeCnt','$paidCnt')";
+                //print_r($sqlIns);
+		mysql_query_decide($sqlIns,$db) or die("$sqlIns".mysql_error_js($db));
+        }
+        }else{
+            /* Start: JSC-2558: Section for entry in case of upgrade services */
+        $count		=$row["cnt"];
+	$serviceid	=$row["SERVICEID"];
+	$center		=$row["CENTER"];
+
+	$sql1 ="SELECT BILLID,ENTRY_DT,SERVICEID FROM billing.PURCHASES WHERE ENTRY_DT BETWEEN '$st_date' AND '$end_date' AND STATUS='DONE' AND CENTER='$center' AND MEM_UPGRADE = 'MAIN'";
+	$res1=mysql_query_decide($sql1,$db_slave) or die("$sql1".mysql_error_js($db_slave));
+	while($row1=mysql_fetch_array($res1))
+	{
+		//select serviceid for billid
+		$billid 	=$row1['BILLID'];
+		$entryDate 	=$row1['ENTRY_DT'];
+		$serviceid1	=$row1['SERVICEID'];
+		$serviceid1Arr	=@explode(",",$serviceid1);
+		$serviceid1	=$serviceid1Arr[0];
+
+		$sql2 = "SELECT AMOUNT FROM billing.PAYMENT_DETAIL WHERE BILLID=$billid";
+		$res2=mysql_query_decide($sql2,$db_slave) or die("$sql2".mysql_error_js($db_slave));
+		$row2=mysql_fetch_array($res2);
+		$amount = $row2['AMOUNT'];
+		//print "billid- ".$billid.", amount- ".$amount.", service_det- ".$serviceid.", purchases-".$serviceid1.PHP_EOL;
+
+		if(strstr($serviceid1,'ES')){
+			$sql3 ="select SERVICEID from billing.SERVICE_STATUS where BILLID='$billid' AND SERVICEID='$serviceid'";
+			$res3=mysql_query_decide($sql3,$db_slave) or die("$sql3".mysql_error_js($db_slave));
+			if($row3=mysql_fetch_array($res3)){
+				$count--;
+				if(strstr($serviceid,'P')){
+                                        $serviceidwithUG = $serviceid1."-UG";
+					$dataSet[$center][$serviceidwithUG]++;
+					if(!empty($amount) && $amount>0){
+						$paidComboCount[$center][$serviceidwithUG]++;
+					}
+				}
+			}
+		} else if(strstr($serviceid1,'NCP')){
+			$sql3 ="select SERVICEID from billing.SERVICE_STATUS where BILLID='$billid' AND SERVICEID='$serviceid'";
+			$res3=mysql_query_decide($sql3,$db_slave) or die("$sql3".mysql_error_js($db_slave));
+			if($row3=mysql_fetch_array($res3)){
+				$count--;
+				if(strstr($serviceid,'C')){
+                                        $serviceidwithUG = $serviceid1."-UG";
+					$dataSet[$center][$serviceidwithUG]++;
+					if(!empty($amount) && $amount>0){
+						$paidComboCount[$center][$serviceidwithUG]++;
+					}
+				}
+			}
+		}
+                 else if(strstr($serviceid1,'X')){
+			$sql3 ="select SERVICEID from billing.SERVICE_STATUS where BILLID='$billid' AND SERVICEID='$serviceid'";
+			$res3=mysql_query_decide($sql3,$db_slave) or die("$sql3".mysql_error_js($db_slave));
+			if($row3=mysql_fetch_array($res3)){
+				$count--;
+				if(strstr($serviceid,'X')){
+                                         $serviceidwithUG = $serviceid1."-UG";
+					$dataSet[$center][$serviceidwithUG]++;
+					if(!empty($amount) && $amount>0){
+						$paidComboCount[$center][$serviceidwithUG]++;
+					}
+				}
+			}
+		}else {
+			$sql3 ="select SERVICEID from billing.SERVICE_STATUS where BILLID='$billid' AND SERVICEID='$serviceid'";
+			$res3=mysql_query_decide($sql3,$db_slave) or die("$sql3".mysql_error_js($db_slave));
+			if($row3=mysql_fetch_array($res3)){
+				if(!empty($amount) && $amount>0){
+					$paidCount[$center][$serviceid1]++;
+				}
+			}
+		}
+
+	}
+
+	if($count>0){
+		$paidCnt = $paidCount[$center][$serviceid];
+		$freeCnt = $count - $paidCnt;
+		$sqlIns="insert into MIS.SERVICE_DETAILS(`ENTRY_DT`,`COUNT`,`SERVICE`,`BRANCH`,`FREE_COUNT`,`PAID_COUNT`) VALUES('$today','$count','$serviceid-UG','$center','$freeCnt','$paidCnt')";
+//                print_r($sqlIns);
 		mysql_query_decide($sqlIns,$db) or die("$sqlIns".mysql_error_js($db));
 	}
+        }
+        /* End: JSC-2558: Section for entry in case of upgrade services */
 }
-
 
 // entry for e-Sathi service purchased
 if(count($dataSet)>0){
@@ -91,7 +177,7 @@ if(count($dataSet)>0){
 			$newCnt =$dataSet[$key][$key1];
 			$paidCnt = $paidComboCount[$key][$key1];
 			$freeCnt = $newCnt - $paidCnt;
-			$sql_insert="insert into MIS.SERVICE_DETAILS(`ENTRY_DT`,`COUNT`,`SERVICE`,`BRANCH`,`FREE_COUNT`,`PAID_COUNT`) VALUES('$entryDate','$newCnt','$key1','$key','$freeCnt','$paidCnt')";
+			$sql_insert="insert into MIS.SERVICE_DETAILS(`ENTRY_DT`,`COUNT`,`SERVICE`,`BRANCH`,`FREE_COUNT`,`PAID_COUNT`) VALUES('$today','$newCnt','$key1','$key','$freeCnt','$paidCnt')";
 			mysql_query_decide($sql_insert,$db) or die("$sql_insert".mysql_error_js($db));
 		}
 	}
@@ -101,7 +187,7 @@ unset($paidCount);
 unset($paidComboCount);
 /* Newly purchased service section ends */
 
-/*  Section for cancelled transaction  */
+//*  Section for cancelled transaction  */
 $sql ="select BILLID from billing.EDIT_DETAILS_LOG where ENTRY_DT>='$st_date' AND ENTRY_DT<='$end_date' AND CHANGES LIKE 'TRANSACTION CANCELLED%'";
 $res=mysql_query_decide($sql,$db_slave) or die("$sql".mysql_error_js($db_slave));
 while($row=mysql_fetch_array($res))
