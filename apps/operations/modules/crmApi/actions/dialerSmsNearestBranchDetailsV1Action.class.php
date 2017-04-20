514f1a080b6321b43fc902b681ab91c762046767
nitish.sharma <nitish.sharma@99acres.com>
2017-04-20 11:57:51 +0530

    [JSC-2450] JSC-2450 comment

--- apps/operations/modules/crmApi/actions/dialerSmsNearestBranchDetailsV1Action.class.php
+++ apps/operations/modules/crmApi/actions/dialerSmsNearestBranchDetailsV1Action.class.php
@@ -1,9 +1,9 @@
 <?php
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Get sms of nearest branch details
+ * @input: profileid
+ * @output: success/failure
  */
 
 /**
