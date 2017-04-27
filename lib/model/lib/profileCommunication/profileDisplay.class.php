3da50a1d299abaa233046e7b41a4a718a32a9a03
Ayush Sethi <ayushsethi22031992@gmail.com>
2017-04-27 17:44:59 +0530

    [JSI-2976] Next Previous MYJS For Android - Patch

--- lib/model/lib/profileCommunication/profileDisplay.class.php
+++ lib/model/lib/profileCommunication/profileDisplay.class.php
@@ -61,23 +61,30 @@ class profileDisplay{
 	*	listing type denotes the list type and offset denotes the current profile number.
 	*/
 
-	public function getNextPreviousProfileForMyjs($iListingType,$iOffset)
+	public static function getNextPreviousProfileForMyjs($iListingType,$iOffset)
 	{	
 
+		$profileObj= LoggedInProfile::getInstance();
+		$pid = $profileObj->getPROFILEID();
+		$cacheCriteria = MyjsSearchTupplesEnums::getListNameForCaching($iListingType);
+		$cachedResultsPoolArray = unserialize(JsMemcache::getInstance()->get("cached".$cacheCriteria."Myjs".$pid));
+		$profileIdToReturn = $cachedResultsPoolArray[$iOffset-1];
+		 return(JsCommon::createChecksumForProfile($profileIdToReturn));
+	}
 
+	public static function getNextProfileIdForMyjs($iListingType,$iOffset)
+	{	
 		$profileObj= LoggedInProfile::getInstance();
 		$pid = $profileObj->getPROFILEID();
 
 		$cacheCriteria = MyjsSearchTupplesEnums::getListNameForCaching($iListingType);
 
 		$cachedResultsPoolArray = unserialize(JsMemcache::getInstance()->get("cached".$cacheCriteria."Myjs".$pid));
-	
 		$profileIdToReturn = $cachedResultsPoolArray[$iOffset-1];
-
-
-		return JsCommon::createChecksumForProfile($profileIdToReturn);
-		
+		 return($profileIdToReturn);		
 	}
+
+
 
 
 
