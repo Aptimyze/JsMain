a1fe3127d072fae08e548e67c2af61cba2693697
Ayush Sethi <ayushsethi22031992@gmail.com>
2017-05-03 16:35:48 +0530

    [JSI-2976] ForceAllow Css/Js

--- web/profile/commonfile_functions.php
+++ web/profile/commonfile_functions.php
@@ -213,7 +213,7 @@ $css_arr[]=array("jsms/myjs/CALJSMS_css"                  => "16");
 $css_arr[]=array("-1"     => "3");
 $css_arr[]=array(" /web/js/ /web/profile/css/my_js_css"   => "1");
 $css_arr[]=array("yatra_style"     => "5");
-$css_arr[]=array("promotions/chatPromo/chatPromoMS_css"       => "3");
+$css_arr[]=array("promotions/chatPromo/chatPromoMS_css"        => "4");
 $css_arr[]=array("promotions/chatPromo/chatPromoPC_css" => "2");
 return $css_arr;
 }
