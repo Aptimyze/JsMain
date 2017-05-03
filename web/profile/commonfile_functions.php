f8766fc8f856b4721a289da566b22346c9427e71
sanyam.chopra <sanyam.chopra@99acres.com>
2017-05-03 11:25:20 +0530

    [JSM-3998] ForceAllow Css/Js

--- web/profile/commonfile_functions.php
+++ web/profile/commonfile_functions.php
@@ -213,7 +213,7 @@ $css_arr[]=array("jsms/myjs/CALJSMS_css"                  => "16");
 $css_arr[]=array("-1"     => "3");
 $css_arr[]=array(" /web/js/ /web/profile/css/my_js_css"   => "1");
 $css_arr[]=array("yatra_style"     => "5");
-$css_arr[]=array("sathiForLife_css"      => "2");
+$css_arr[]=array("sathiForLife_css"       => "3");
 return $css_arr;
 }
 /* JavaScript File */
@@ -634,7 +634,7 @@ $js_arr[]=array("operations_validate2"        => "6");
 $js_arr[]=array("jsms/settings/mobHideProfile_js"                      => "18");
 $js_arr[]=array("jspc/photoUpload/fbPage_js" => "1");
 $js_arr[]=array("/web/billing/billing"   => "1");
-$js_arr[]=array("sathiForLife_js"    => "2");
+$js_arr[]=array("sathiForLife_js"     => "3");
 return $js_arr;
 }
 function getJavascript(){
