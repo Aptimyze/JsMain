1e1707b62a14e8585fe7813e0e5f9f5b382deeed
mohammad.shahjahan <mohammad.shahjahan@jeevansathi.com>
2017-05-08 13:17:59 +0530

    [JSM-3812] ForceAllow Css/Js

--- web/profile/commonfile_functions.php
+++ web/profile/commonfile_functions.php
@@ -410,7 +410,7 @@ $js_arr[]=array("pixel_js"    => "2");
 $js_arr[]=array("mouseClickTracking_js"  => "2");
 $js_arr[]=array("jsms/profile/ep/submitDetails_js"                      => "40");
 $js_arr[]=array("jsms/profile/ep/hamburger_js"                      => "46");
-$js_arr[]=array("jsms/profile/ep/mobedit_js"                                       => "54");
+$js_arr[]=array("jsms/profile/ep/mobedit_js"                                        => "55");
 $js_arr[]=array("jsms/profile/ep/mobeditview_js"                                                           => "83");
 $js_arr[]=array("jsms/profile/ep/mobShow_name_js"              => "8");
 $js_arr[]=array("jsms/common/disableScroll_js"  => "4");
