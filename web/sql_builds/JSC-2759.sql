d22814dd6add82651518f89e02c110d21dd27c71
ankita.g <ankita.g@jeevansathi.com>
2017-05-11 10:23:14 +0530

    [JSC-2759] web/sql_builds/JSC-2759.sql

---
+++ web/sql_builds/JSC-2759.sql
@@ -0,0 +1,6 @@
+use billing;
+
+INSERT IGNORE INTO billing.DISCOUNT_HISTORY_BACKUP SELECT * FROM billing.DISCOUNT_HISTORY WHERE DATE < "2017-04-10";
+
+/* before running this ensure backup done*/
+DELETE FROM billing.DISCOUNT_HISTORY WHERE DATE<"2017-04-10";
