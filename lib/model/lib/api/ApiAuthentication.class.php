da0099667b1d672dc0c6898717da0e4391440077
Nikhil <nikhil.mittal@jeevansathi.com>
2017-05-15 14:05:40 +0530

    [JSI-2904] Use username for logging and module configured for info level logs

--- lib/model/lib/api/ApiAuthentication.class.php
+++ lib/model/lib/api/ApiAuthentication.class.php
@@ -876,7 +876,6 @@ Abstract class ApiAuthentication
     public function setLoginTrackingCookie($loginData)
 	{
 		$username = $loginData["USERNAME"];
-		$profileid = $loginData["PROFILEID"];
 		$cookieName = "track";
 		$expiryTime = 31536000; // Approx 1 year
 
@@ -884,7 +883,7 @@ Abstract class ApiAuthentication
 		{
 			@setcookie($cookieName, json_encode(array($username)), time() + $expiryTime, "/", $this->domain);
 			// send mail
-			LoggingManager::getInstance()->logThis(LoggingEnums::LOG_INFO,"Send mail for New login profile : $profileid ",array(LoggingEnums::MODULE_NAME => LoggingEnums::NEW_LOGIN_TRACK));
+			LoggingManager::getInstance()->logThis(LoggingEnums::LOG_INFO,"Send mail for New login User : $username ",array(LoggingEnums::MODULE_NAME => LoggingEnums::NEW_LOGIN_TRACK));
 		}
 		else
 		{
@@ -894,7 +893,7 @@ Abstract class ApiAuthentication
 				array_push($cookieData, $username);
 				@setcookie($cookieName, json_encode($cookieData), time() + $expiryTime, "/", $this->domain);
 				// send mail
-				LoggingManager::getInstance()->logThis(LoggingEnums::LOG_INFO,"Send mail for New login profile : $profileid ",array(LoggingEnums::MODULE_NAME => LoggingEnums::NEW_LOGIN_TRACK));
+				LoggingManager::getInstance()->logThis(LoggingEnums::LOG_INFO,"Send mail for New login User : $username ",array(LoggingEnums::MODULE_NAME => LoggingEnums::NEW_LOGIN_TRACK));
 			}
 		}
 
