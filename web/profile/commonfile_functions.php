fba623fc478609aec7013edffc9771235c540cf4
Ayush Sethi <ayushsethi22031992@gmail.com>
2017-05-16 13:07:20 +0530

    [JSC-2759] ForceAllow Css/Js

--- web/profile/commonfile_functions.php
+++ web/profile/commonfile_functions.php
@@ -209,7 +209,7 @@ $css_arr[]=array("operations/help_module"   => "1");
 $css_arr[]=array("jspc/chat/chat_css"              => "15");
 $css_arr[]=array("jspc/help/JSPCHelp_css"       => "8");
 $css_arr[]=array("jsms/help/JSMSHelp_css"     => "3");
-$css_arr[]=array("jsms/myjs/CALJSMS_css"                    => "18");
+$css_arr[]=array("jsms/myjs/CALJSMS_css"                     => "19");
 $css_arr[]=array("-1"     => "3");
 $css_arr[]=array(" /web/js/ /web/profile/css/my_js_css"   => "1");
 $css_arr[]=array("yatra_style"     => "5");
