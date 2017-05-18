77d717f8469a9f3d62cd4e7d4f91161f10480c15
Ayush Sethi <ayushsethi22031992@gmail.com>
2017-05-18 12:21:32 +0530

    [JSI-3070] Fixing Bug for Blocked Profiles in Filter Mailer

--- lib/task/mailer/FilterMailerEOITask.class.php
+++ lib/task/mailer/FilterMailerEOITask.class.php
@@ -39,16 +39,30 @@ EOF;
 	            $Contactsobj = new newjs_CONTACTS($dbName);
 	            $chunkstr="RECEIVER%".$this->noOfActiveServers."=".$serverId;
 	            $profilemail=$Contactsobj->getFilterContacts($serverId,$chunkstr);
+
 	            foreach ($profilemail as $key => $value) {			            		
 				    $usercode = explode(',',$value);
+				    $usercode = $this->skipProfiles($usercode,$key);
 				    $count=count($usercode);
 				    if(count($usercode)>10)
 				        $usercode = array_slice($usercode, 0, 10);
 				    $usercode = implode(',',$usercode);
-				    if($key!=0)
+				    if($key!=0 && $count >= 1)
 					$mailerEOIFilterObj->InsertFilterEOI($key,$usercode,$count);
 	        }
 	    }
+	}
+
+		public function skipProfiles($arranged,$key)
+	{
+            $skipProfileObj     = new newjs_IGNORE_PROFILE('newjs_slave');
+        	$skipProfiles       = $skipProfileObj->listIgnoredProfile($key);
+
+			if(is_array($skipProfiles))
+				$temp=array_diff($arranged,$skipProfiles); 
+			else
+				$temp=$arranged;      
+		return $temp;
 	}
 }
 
