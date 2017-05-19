e2dd7b455a97e333b7a13243f65fe107577e3a91
Ayush Sethi <ayushsethi22031992@gmail.com>
2017-05-19 15:50:06 +0530

    [JSI-3084]

--- apps/jeevansathi/modules/faq/lib/FeedbackEnum.class.php
+++ apps/jeevansathi/modules/faq/lib/FeedbackEnum.class.php
@@ -20,7 +20,7 @@ class FeedbackEnum
   const CAT_ABUSE			= "Abuse";
   
   //Success Messages
-  const SUCCESS_ABUSE_MSG = "We will look into this case and delete the profile permanently if we can establish that the user is abusing the system.";
+  const SUCCESS_ABUSE_MSG = "Thank you for helping us. We will look into this case and delete the profile permanently if we can establish that the user is abusing the system.";
   const SUCCESS_ANDROID_MSG = "Thank you for helping us. We will look into this case and delete the profile permanently if we can establish that the user is abusing the system.";
   const SUCCESS_IOS_MSG = "Thank you for helping us. We will look into this case and delete the profile permanently if we can establish that the user is abusing the system.";
 }
