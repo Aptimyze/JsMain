c472968ab97ef327a562fd7383e5ca776941ddc7
Palash Chordia <palash.chordia@jeevansathi.com>
2017-05-24 16:12:09 +0530

    [CoolMetricsQueue] cool

--- lib/model/enums/MessageQueues.enum.class.php
+++ lib/model/enums/MessageQueues.enum.class.php
@@ -18,6 +18,7 @@ class MessageQueues
   CONST UPDATE_SEEN_CONSUMER_COUNT = 1; //variable to store cosumers to be executed for update seen
   CONST UPDATE_SEEN_PROFILE_CONSUMER_COUNT = 3; //variable to store cosumers to be executed for update seen
   CONST LOGGING_QUEUE_CONSUMER_COUNT = 2; //variable to store cosumers to be executed for update seen
+  CONST PRODUCT_METRIC_QUEUE_CONSUMER_COUNT = 1; //variable to store cosumers to be executed for update seen
   CONST FEATURED_PROFILE_CONSUMER_COUNT = 1; //variable to store cosumers to be executed for update seen
   CONST PROFILE_CACHE_CONSUMER_COUNT = 1; //variable to store cosumers to be executed for update seen
   CONST CHAT_CONSUMER_COUNT = 1; //variable to store cosumers to be executed for chat messages
