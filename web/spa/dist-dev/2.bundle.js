63c9113746ad715f4b02501f5599226bc4251e17
Palash Chordia <palash.chordia@jeevansathi.com>
2017-06-08 11:23:02 +0530

    [SPA] JSI-3194

---
+++ web/spa/dist-dev/2.bundle.js
@@ -0,0 +1,15 @@
+webpackJsonp([2],{
+
+/***/ 261:
+/* no static exports found */
+/* all exports used */
+/*!******************************************!*\
+  !*** ./src/login/containers/MyjsPage.js ***!
+  \******************************************/
+/***/ (function(module, exports) {
+
+eval("React.createElement(\n  \"div\",\n  null,\n  \"Hello\"\n);//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiMjYxLmpzIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vL3NyYy9sb2dpbi9jb250YWluZXJzL015anNQYWdlLmpzP2E5NWEiXSwic291cmNlc0NvbnRlbnQiOlsiPGRpdj5IZWxsbzwvZGl2PlxuXG5cbi8vIFdFQlBBQ0sgRk9PVEVSIC8vXG4vLyBzcmMvbG9naW4vY29udGFpbmVycy9NeWpzUGFnZS5qcyJdLCJtYXBwaW5ncyI6IkFBQUE7QUFBQTtBQUFBO0FBQUE7QUFBQSIsInNvdXJjZVJvb3QiOiIifQ==");
+
+/***/ })
+
+});
