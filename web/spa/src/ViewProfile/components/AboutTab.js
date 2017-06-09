dac98a77ae37454cbfad93c60afece37fe0df85e
manvi.mehta <manvi.mehta@jeevansathi.com>
2017-06-09 15:03:07 +0530

    SPA profile tab

---
+++ web/spa/src/ViewProfile/components/AboutTab.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const AboutTab = (props) => (
+    <div id="AboutTab">
+		about data
+	</div>
+);
+
+export default AboutTab;
---
+++ web/spa/src/ViewProfile/components/FamilyTab.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const FamilyTab = (props) => (
+    <div id="FamilyTab" className="dn">
+		family data
+	</div>
+);
+
+export default FamilyTab;
