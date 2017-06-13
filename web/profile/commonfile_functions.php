c89cefef5dae63dda9f6d877bda9f49a0c2188fc
mohammad.shahjahan <mohammad.shahjahan@jeevansathi.com>
2017-06-13 12:07:05 +0530

    [SPA] ForceAllow Css/Js

--- web/profile/commonfile_functions.php
+++ web/profile/commonfile_functions.php
@@ -640,7 +640,7 @@ $js_arr[]=array("jsms/promotions/chatPromo/chatPromo_js" => "3");
 $js_arr[]=array("sathiForLife_js" => "9");
 $js_arr[]=array(" /web/spa/src/login/containers/LoginPage"   => "1");
 $js_arr[]=array("/web/spa/src/login/actions/LoginActions"   => "1");
-$js_arr[]=array(" /web/spa/src/common/components/CookieHelper"    => "2");
+$js_arr[]=array(" /web/spa/src/common/components/CookieHelper"     => "3");
 $js_arr[]=array("/web/spa/src/common/components/App"     => "3");
 $js_arr[]=array(" /web/spa/src/common/containers/EnsureLoggedInContainer"   => "1");
 return $js_arr;
