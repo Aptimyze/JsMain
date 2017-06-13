96b69f8f53535d347916a18fb0ae5aa9ef99cd51
Ayush Sethi <ayushsethi22031992@gmail.com>
2017-06-13 12:46:56 +0530

    [JSI-3130] ForceAllow Css/Js

--- web/profile/commonfile_functions.php
+++ web/profile/commonfile_functions.php
@@ -627,7 +627,7 @@ $js_arr[]=array("jspc/help/JSPCHelp_js"                => "20");
 $js_arr[]=array("jsms/help/JSMSHelp_js"            => "14");
 $js_arr[]=array("jsms/help/jsmsPostQuery_js"        => "6");
 $js_arr[]=array("jsms/help/autoPopulateFreshdesk_js"     => "3");
-$js_arr[]=array("jsms/myjs/CALJSMS_js"                                      => "38");
+$js_arr[]=array("jsms/myjs/CALJSMS_js"                                       => "39");
 $js_arr[]=array("jspc/chat/customElem"       => "4");
 $js_arr[]=array("jspc/chat/strophe_receipts"       => "4");
 $js_arr[]=array("-1.0.2/Gruntfile"    => "3");
