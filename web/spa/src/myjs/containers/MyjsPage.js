47e1806dfc219f9a3ee56acde2ced6ac3cadad55
Atul Shukla <atul.shukla@jeevansathi.com>
2017-06-14 10:22:41 +0530

    SPA: Added changes in myjs.

--- web/spa/src/myjs/containers/MyjsPage.js
+++ web/spa/src/myjs/containers/MyjsPage.js
@@ -35,7 +35,7 @@ export  class MyjsPage extends React.Component {
 		  <div id="mainContent">
 				  <div className="perspective" id="perspective">
 							<div className="" id="pcontainer">
-							<MyjsHeadHTML bellResponse={this.props.reducerData.apiData.BELL_COUNT} fetched={this.props.reducerData.fetched} />;
+							<MyjsHeadHTML bellResponse={this.props.reducerData.apiData.BELL_COUNT} fetched={this.props.reducerData.fetched} />
 							<AcceptCount/>
 							 <ProfileVisitor/>
 
