d064e1ba0dc9b0902d670a6d4ee933bf3b972898
Palash Chordia <palash.chordia@jeevansathi.com>
2017-06-16 12:13:47 +0530

    [SPA] JSI-3194

--- web/spa/src/myjs/containers/MyjsPage.js
+++ web/spa/src/myjs/containers/MyjsPage.js
@@ -38,19 +38,42 @@ export  class MyjsPage extends React.Component {
 			this.props.history.push('/login');
 		}
 	}
+	componentWillMount(){
+			this.CssFix();
+	}
+	CssFix()
+	{
+			// create our test div element
+			var div = document.createElement('div');
+			// css transition properties
+			var props = ['WebkitPerspective', 'MozPerspective', 'OPerspective', 'msPerspective'];
+			// test for each property
+			for (var i in props) {
+					if (div.style[props[i]] !== undefined) {
+						var cssPrefix = props[i].replace('Perspective', '').toLowerCase();
+							this.setState({
+								cssProps:{
+									cssPrefix : cssPrefix,
+									animProp : '-' + cssPrefix + '-transform'
+								}
+					});
+			}
+	};
+}
 
   	render() {
+			if(!this.props.reducerData.fetched)
+			return (<div></div>);
   		return(
 		  <div id="mainContent">
 				  <div className="perspective" id="perspective">
 							<div className="" id="pcontainer">
 							<MyjsHeadHTML bellResponse={this.props.reducerData.apiData.BELL_COUNT} fetched={this.props.reducerData.fetched}/>
-
-							<EditBar fetched={this.props.reducerData.fetched}/>
+							<EditBar cssProps={this.state.cssProps}  profileInfo ={this.props.reducerData.apiData.my_profile} fetched={this.props.reducerData.fetched}/>
 							<AcceptCount fetched={this.props.reducerData.fetched}/>
 							<MyjsProfileVisitor responseMessage={this.props.reducerData.apiData.responseMessage} fetched={this.props.reducerData.fetched}/>
-							<MyjsSlider title={this.state.DR} fetched={this.props.reducerData.fetched}/>
-							<MyjsSlider title={this.state.IR} listing={this.props.reducerData.apiData.interest_received} fetched={this.props.reducerData.fetched}/>
+							<MyjsSlider title={this.state.DR} />
+							<MyjsSlider title={this.state.IR} />
 
 							</div>
 					</div>
