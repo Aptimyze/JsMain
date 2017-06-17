0a3447d40022189cde3062ae7fa07ff75d91b953
Palash Chordia <palash.chordia@jeevansathi.com>
2017-06-17 13:04:07 +0530

    [SPA] SPA

--- web/spa/src/myjs/components/MyjsSliderBar.js
+++ web/spa/src/myjs/components/MyjsSliderBar.js
@@ -25,9 +25,10 @@ export default class MyjsSlider extends React.Component {
   }
   render(){
     console.log(this.props.listing);
-     if(!this.props.fetched) {
+     if(!this.props.listing.tuples.length) {
       return <div></div>;
     }
+
     return(
       <div>
         <div id="matchalertPresent" className="setWidth sliderc1">
