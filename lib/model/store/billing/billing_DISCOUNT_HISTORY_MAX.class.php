bcc8d14dc6dab57f354d8b1ebc480661eceee05d
ankita.g <ankita.g@jeevansathi.com>
2017-06-19 14:23:22 +0530

    [JSC-2987] ci commit

--- lib/model/store/billing/billing_DISCOUNT_HISTORY_MAX.class.php
+++ lib/model/store/billing/billing_DISCOUNT_HISTORY_MAX.class.php
@@ -11,10 +11,11 @@ class billing_DISCOUNT_HISTORY_MAX extends TABLE{
     public function updateDiscountHistoryMax($paramsArr){
         if(is_array($paramsArr)){
             try{
-                $sql = "INSERT INTO billing.DISCOUNT_HISTORY_MAX (PROFILEID, LAST_LOGIN_DATE, P, C, NCP, X) VALUES (:PROFILEID, :LAST_LOGIN_DATE, :P, :C, :NCP, :X) ON DUPLICATE KEY UPDATE P=:P, C=:C, NCP=:NCP, X=:X";
+                $sql = "INSERT INTO billing.DISCOUNT_HISTORY_MAX (PROFILEID, LAST_LOGIN_DATE, P, C, NCP, X,Date1) VALUES (:PROFILEID, :LAST_LOGIN_DATE, :P, :C, :NCP, :X,:DATE1) ON DUPLICATE KEY UPDATE P=CASE WHEN P<:P THEN :P ELSE P END, C=CASE WHEN C<:C THEN :C ELSE C END, NCP=CASE WHEN NCP<:NCP THEN :NCP ELSE NCP END, X=CASE WHEN X<:X THEN :X ELSE X END,LAST_LOGIN_DATE=:LAST_LOGIN_DATE,Date1=:DATE1";
                 $prep = $this->db->prepare($sql);
                 $prep->bindValue(":PROFILEID",$paramsArr["PROFILEID"],PDO::PARAM_STR);
                 $prep->bindValue(":LAST_LOGIN_DATE",date('Y-m-d'),PDO::PARAM_STR);
+                $prep->bindValue(":DATE1",date('Y-m-d H:i:s'),PDO::PARAM_STR);
                 $prep->bindValue(":P",$paramsArr["P"],PDO::PARAM_INT);
                 $prep->bindValue(":C",$paramsArr["C"],PDO::PARAM_INT);
                 $prep->bindValue(":NCP",$paramsArr["NCP"],PDO::PARAM_INT);
