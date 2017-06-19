43a8ab80e0fe1d038c6f3e2ce463909c472eaf78
ankita.g <ankita.g@jeevansathi.com>
2017-06-19 15:00:36 +0530

    [JSC-2987] ci commit

--- lib/model/store/billing/billing_DISCOUNT_HISTORY_MAX.class.php
+++ lib/model/store/billing/billing_DISCOUNT_HISTORY_MAX.class.php
@@ -11,15 +11,13 @@ class billing_DISCOUNT_HISTORY_MAX extends TABLE{
     public function updateDiscountHistoryMax($paramsArr){
         if(is_array($paramsArr)){
             try{
-                $sql = "INSERT INTO billing.DISCOUNT_HISTORY_MAX (PROFILEID, LAST_LOGIN_DATE, P, C, NCP, X,Date1) VALUES (:PROFILEID, :LAST_LOGIN_DATE, :P, :C, :NCP, :X,:DATE1) ON DUPLICATE KEY UPDATE P=CASE WHEN P<:P THEN :P ELSE P END, C=CASE WHEN C<:C THEN :C ELSE C END, NCP=CASE WHEN NCP<:NCP THEN :NCP ELSE NCP END, X=CASE WHEN X<:X THEN :X ELSE X END,LAST_LOGIN_DATE=:LAST_LOGIN_DATE,Date1=:DATE1";
+                $sql = "INSERT INTO billing.DISCOUNT_HISTORY_MAX (PROFILEID, LAST_LOGIN_DATE, MAX_DISCOUNT_DATE, MAX_DISCOUNT) VALUES (:PROFILEID, :LAST_LOGIN_DATE_NEW, :MAX_DISCOUNT_DATE_NEW, :MAX_DISCOUNT_NEW) ON DUPLICATE KEY UPDATE LAST_LOGIN_DATE=:LAST_LOGIN_DATE_NEW, MAX_DISCOUNT=CASE WHEN MAX_DISCOUNT<:MAX_DISCOUNT_NEW THEN :MAX_DISCOUNT_NEW ELSE MAX_DISCOUNT END,MAX_DISCOUNT_DATE=CASE WHEN MAX_DISCOUNT<:MAX_DISCOUNT_NEW THEN :MAX_DISCOUNT_DATE_NEW ELSE MAX_DISCOUNT_DATE END";
                 $prep = $this->db->prepare($sql);
                 $prep->bindValue(":PROFILEID",$paramsArr["PROFILEID"],PDO::PARAM_STR);
-                $prep->bindValue(":LAST_LOGIN_DATE",date('Y-m-d'),PDO::PARAM_STR);
-                $prep->bindValue(":DATE1",date('Y-m-d H:i:s'),PDO::PARAM_STR);
-                $prep->bindValue(":P",$paramsArr["P"],PDO::PARAM_INT);
-                $prep->bindValue(":C",$paramsArr["C"],PDO::PARAM_INT);
-                $prep->bindValue(":NCP",$paramsArr["NCP"],PDO::PARAM_INT);
-                $prep->bindValue(":X",$paramsArr["X"],PDO::PARAM_INT);
+                $prep->bindValue(":LAST_LOGIN_DATE_NEW",date('Y-m-d'),PDO::PARAM_STR);
+                $prep->bindValue(":MAX_DISCOUNT_DATE_NEW",date('Y-m-d H:i:s'),PDO::PARAM_STR);
+                $prep->bindValue(":MAX_DISCOUNT_NEW",$paramsArr["MAX_DISCOUNT"],PDO::PARAM_INT);
+                
                 $prep->execute();
             } catch (Exception $ex) {
                 throw new jsException($ex);
