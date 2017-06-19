70d08145790b9bfff05713297744640bbee0d88f
Nikhil <nikhil.mittal@jeevansathi.com>
2017-06-19 16:16:20 +0530

    [JSI-3065] store for outbound logs

---
+++ lib/model/store/OUTBOUND/OUTBOUND_FAILURE_LOGS.class.php
@@ -0,0 +1,48 @@
+<?php 
+
+/**
+* 
+*/
+class OUTBOUND_FAILURE_LOGS extends AnotherClass
+{
+	/**
+   * @fn __construct
+   * @brief Constructor function
+   * @param $dbName - Database to which the connection would be made
+   */
+  public function __construct($dbname = "") {
+    parent::__construct($dbname);
+  }
+
+  public function insertRecord($arrRecordData) {
+    if (!is_array($arrRecordData))
+      throw new jsException("", "Array is not passed in InsertRecord OF OUTBOUND_FAILURE_LOGS.class.php");
+
+    try {
+      $szINs = implode(',', array_fill(0, count($arrRecordData), '?'));
+
+      $arrFields = array();
+      foreach ($arrRecordData as $key => $val) {
+        $arrFields[] = strtoupper($key);
+      }
+      $szFields = implode(",", $arrFields);
+
+      $sql = "INSERT INTO OUTBOUND.FAILURE_LOGS ($szFields) VALUES ($szINs)";
+      $pdoStatement = $this->db->prepare($sql);
+
+      //Bind Value
+      $count = 0;
+      foreach ($arrRecordData as $k => $value) {
+        ++$count;
+        $pdoStatement->bindValue(($count), $value);
+      }
+      $pdoStatement->execute();
+      
+      return $pdoStatement->rowCount();
+    } catch (Exception $e) {
+      throw new jsException($e);
+    }
+  }
+}
+
+?>
