175ab9895b85ea5496ce57bc6f002e7164df828f
manvi.mehta <manvi.mehta@jeevansathi.com>
2017-06-20 09:55:05 +0530

    SPA

--- web/spa/src/viewProfile/components/DppTab.js
+++ web/spa/src/viewProfile/components/DppTab.js
@@ -3,6 +3,7 @@ import {getCookie} from '../../common/components/CookieHelper';
 
 class DppTab extends React.Component {
 	constructor(props) {
+        console.log("dpp",props)
         super();
         console.log(props)
         let loginStatus = false;
@@ -69,13 +70,16 @@ class DppTab extends React.Component {
             self_gender = "She";
         }
         var matchingCount = "";
-        if(this.props.dpp_Ticks.matching) {
-            matchingCount = this.props.dpp_Ticks.matching.matchingCount;
+        if(this.props.dpp_Ticks) {
+            if(this.props.dpp_Ticks.matching) {
+                matchingCount = this.props.dpp_Ticks.matching.matchingCount;
+            }    
         }
-
         var totalCount = "";
-        if(this.props.dpp_Ticks.matching) {
-            totalCount = this.props.dpp_Ticks.matching.totalCount;
+        if(this.props.dpp_Ticks) {
+            if(this.props.dpp_Ticks.matching) {
+                totalCount = this.props.dpp_Ticks.matching.totalCount;
+            }
         }
         var matching_header;
         if(this.props.dpp_Ticks && this.state.loginStatus) 
