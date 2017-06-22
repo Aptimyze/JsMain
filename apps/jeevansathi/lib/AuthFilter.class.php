ef748765e1b8b6dc7df87456050dce3bfa3519b3
lavesh <ankit.shukla@jeevansathi.com>
2017-06-22 17:27:55 +0530

    [JSM-4107] newMobilesite check

--- apps/jeevansathi/lib/AuthFilter.class.php
+++ apps/jeevansathi/lib/AuthFilter.class.php
@@ -369,7 +369,8 @@ class AuthFilter extends sfFilter {
 					$request->setParameter('showKundliList', 1);
 				}
 			}
-                        $request->setParameter('showAndBeyond', CommonFunction::showAndBeyondPixel($data[PROFILEID]));
+                        if(MobileCommon::isNewMobileSite())
+                            $request->setParameter('showAndBeyond', CommonFunction::showAndBeyondPixel($data[PROFILEID]));
 		}
 		//code to fetch the revision number to clear local storage
 		$revisionObj= new LatestRevision();
