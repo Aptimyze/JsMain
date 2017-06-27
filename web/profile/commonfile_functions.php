41a47ea11fae2d80782f2a1b84aa55067332e83a
mohammad.shahjahan <mohammad.shahjahan@jeevansathi.com>
2017-06-27 10:59:14 +0530

    [SPA] ForceAllow Css/Js

--- web/profile/commonfile_functions.php
+++ web/profile/commonfile_functions.php
@@ -714,6 +714,8 @@ $js_arr[]=array("calendar_js"    => "2");
 $js_arr[]=array("autoSuggest_js"    => "2");
 $js_arr[]=array("/JSNotificationExtension/JSnotification/client"    => "2");
 $js_arr[]=array(" /web/spa/src/common/components/App"   => "1");
+$js_arr[]=array(" /web/spa/src/login/containers/LoginPage"   => "1");
+$js_arr[]=array("/web/spa/src/common/constants/apiConstants"   => "1");
 return $js_arr;
 }
 function getJavascript(){
