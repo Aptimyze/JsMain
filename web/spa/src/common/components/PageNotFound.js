7efabd70621f7e94786728b5ab1bc403633121ee
Atul Shukla <atul.shukla@jeevansathi.com>
2017-06-27 12:38:21 +0530

    pagenotfound

--- web/spa/src/common/components/PageNotFound.js
+++ web/spa/src/common/components/PageNotFound.js
@@ -1,16 +1,38 @@
 import React from "react";
 
-export default class PageNotFound extends React.Component { 
+export default class PageNotFound extends React.Component {
     constructor(props) {
-    super();    
+    super();
   }
-  
+  componentDidMount(){
+    console.log(document.getElementsByClassName("sreen404"));
+    document.getElementById("Pnf").style.height = window.innerHeight+"px";
+    document.getElementById("Pnf").style.width = window.innerWidth+"px";
+  }
+
   render() {
+
       return (
-        <div>
-            <h3>404 page not found</h3>
-            <p>We are sorry but the page you are looking for does not exist.</p>
-        </div>            
+        <div className="mainContent">
+          <div className="perspective" id="perspective">
+            <div id="pcontainer">
+              <div className="bg7 sreen404"  id="Pnf">
+                <div className="pad19">
+                  <div className="hamicon1">
+                    <i id="hamburgerIcon" className="mainsp baricon "></i>
+                  </div>
+                  <div className="disptbl">
+                    <div className="dispcell vertmid">
+                      <div className="posrel">
+                        <img src="../../img/js-error-img-1.png"/>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       );
   }
 }
