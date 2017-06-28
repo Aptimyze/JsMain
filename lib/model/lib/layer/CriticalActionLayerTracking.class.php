1bd17712f248b3680b4833419c4d268727575121
Ayush Sethi <ayushsethi22031992@gmail.com>
2017-06-28 13:20:52 +0530

    [JSI-3247] Switching off name privacy CAL

--- lib/model/lib/layer/CriticalActionLayerTracking.class.php
+++ lib/model/lib/layer/CriticalActionLayerTracking.class.php
@@ -332,10 +332,14 @@ return 0;
                     
                     break; 
                     case '15': 
+                    //This variable is introduced as we had to switch off this CAL for some duration (due to low numbers)
+                      $switchForCAL = 0;
+                      if($switchForCAL){
                       $screening=$profileObj->getSCREENING();
                       $nameArr=(new NameOfUser())->getNameData($profileid);
                       if(!$nameArr[$profileid]['DISPLAY'] && $nameArr[$profileid]['NAME'] && jsValidatorNameOfUser::validateNameOfUser($nameArr[$profileid]['NAME']) && Flag::isFlagSet("name", $screening))
                           $show=1;
+                      }
                     break;  
 
                     case '16':                      
