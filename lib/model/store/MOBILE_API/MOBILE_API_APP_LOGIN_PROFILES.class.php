966078282b3cefb8d760164335c82fbe607b1e54
Sanyam Chopra <sanyam.chopra@jeevansathi.com>
2017-06-28 18:28:47 +0530

    [JSM-4249] JSM-4249

--- lib/model/store/MOBILE_API/MOBILE_API_APP_LOGIN_PROFILES.class.php
+++ lib/model/store/MOBILE_API/MOBILE_API_APP_LOGIN_PROFILES.class.php
@@ -83,22 +83,25 @@ class MOBILE_API_APP_LOGIN_PROFILES extends TABLE
 	{
 		try
 		{
-
-			$sql = "select PROFILEID from MOBILE_API.APP_LOGIN_PROFILES where PROFILEID =:PROFILEID AND APP_TYPE = :APP_TYPE AND DATE>=:DATE";
-			$res=$this->db->prepare($sql);
-			$res->bindValue(":PROFILEID", $pid, PDO::PARAM_INT);
-			$res->bindValue(":APP_TYPE", $appType, PDO::PARAM_STR);
-			$res->bindValue(":DATE", $date, PDO::PARAM_STR);
-			$res->execute();
-			while($row = $res->fetch(PDO::FETCH_ASSOC))
+			if($pid && $date && $appType)
 			{
-				return true;
+				$sql = "select PROFILEID from MOBILE_API.APP_LOGIN_PROFILES where PROFILEID =:PROFILEID AND APP_TYPE = :APP_TYPE AND DATE>=:DATE";
+				$res=$this->db->prepare($sql);
+				$res->bindValue(":PROFILEID", $pid, PDO::PARAM_INT);
+				$res->bindValue(":APP_TYPE", $appType, PDO::PARAM_STR);
+				$res->bindValue(":DATE", $date, PDO::PARAM_STR);
+				$res->execute();
+				while($row = $res->fetch(PDO::FETCH_ASSOC))
+				{
+					return true;
+				}
+				return false;
 			}
-			return false;
+			
 		}
 		catch(PDOException $e)
 		{
-			throw new jsException($e);
+			jsException::nonCriticalError($e." Mobile_api_app_login_profiles");
 		}
 	}
 
