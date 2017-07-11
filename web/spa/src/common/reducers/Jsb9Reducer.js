4210699ee11232563f5733c6d97030d51f4a19bc
manvi.mehta <manvi.mehta@jeevansathi.com>
2017-07-11 17:48:06 +0530

    SPA

--- web/spa/src/common/reducers/Jsb9Reducer.js
+++ web/spa/src/common/reducers/Jsb9Reducer.js
@@ -1,16 +1,13 @@
 const Jsb9Reducer = (state={
 },action) => {
-	console.log('jsb9',action.type);
-
 	switch(action.type)
 	{
-			case 'SET_JSB9_REDIRECTION':
-		state = {
-			...action.payload
-		}
+		case 'SET_JSB9_REDIRECTION':
+			state = {
+				...action.payload
+			}
 		break;
 	}
-	console.log('statestar');console.log(state);console.log('stateend');
 	return state;
 }
 
