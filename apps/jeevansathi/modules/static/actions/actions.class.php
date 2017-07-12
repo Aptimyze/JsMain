5a96d52a708d595ea7595998fbd04d930571f8bd
Kunal Verma <kunal.verma@jeevansathi.com>
2017-07-12 12:53:27 +0530

    [JSI-3288] Fixing tracking work

--- apps/jeevansathi/modules/static/actions/actions.class.php
+++ apps/jeevansathi/modules/static/actions/actions.class.php
@@ -2195,19 +2195,30 @@ if($k=="state_res")
     $RecordingUrl = $request->getPostParameter('RecordingUrl');
     $DateUpdated = $request->getPostParameter('DateUpdated');
 
-    $totalResponse = ' Status : '.$Status.' DateUpdated : '.$DateUpdated.' RecordingUrl : '.$RecordingUrl;
+    $totalResponse = ' Status : '.$Status.' DateUpdated : '.$DateUpdated.' RecordingUrl : '.$RecordingUrl. "\n";
     
     
     if($request->getMethod() == sfWebRequest::GET && is_null($CallSid) && count($request->getGetParameters())) {
       foreach($getParameters as $key) {
+        
+        if ($key == "CallSid") {
+          $CallSid = $request->getParameter($key);
+          continue;
+        }
         $arrOut[] = " $key : " . $request->getParameter($key);
       }
       
       $totalResponse = implode(", ", $arrOut);
+      
+      $totalResponse .= "\n";
     }
     
     $storeObj = new OUTBOUND_THIRD_PARTY_CALL_LOGS();
     $storeObj->updateCallResponse($CallSid, $totalResponse);
-    die(x);
+    
+    $respObj = ApiResponseHandler::getInstance();
+    $respObj->setHttpArray(ResponseHandlerConfig::$SUCCESS);
+    $respObj->generateResponse();
+    die();
   }
 }
