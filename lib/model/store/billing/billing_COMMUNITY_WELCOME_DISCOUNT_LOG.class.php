55d62be3336254fc45004bc6c11f55092172dcff
nitish.sharma <nitish.sharma@99acres.com>
2017-07-21 14:59:25 +0530

    [JSC-2451] JSC-2451 modified sql for offer start date and end date and logging

--- lib/model/store/billing/billing_COMMUNITY_WELCOME_DISCOUNT_LOG.class.php
+++ lib/model/store/billing/billing_COMMUNITY_WELCOME_DISCOUNT_LOG.class.php
@@ -8,12 +8,14 @@ class billing_COMMUNITY_WELCOME_DISCOUNT_LOG extends TABLE
         parent::__construct($dbname);
     }
 
-    public function addEntry($profileid,$discount,$community,$entryDt){
+    public function addEntry($profileid,$discount,$startDate,$endDate,$community,$entryDt){
         try{
-            $sql = "INSERT IGNORE INTO billing.COMMUNITY_WELCOME_DISCOUNT_LOG (PROFILEID,DISCOUNT,COMMUNITY,ENTRY_DT) VALUES(:PROFILEID,:DISCOUNT,:COMMUNITY,:ENTRY_DT)";
+            $sql = "INSERT IGNORE INTO billing.COMMUNITY_WELCOME_DISCOUNT_LOG (PROFILEID,DISCOUNT,SDATE,EDATE,COMMUNITY,ENTRY_DT) VALUES(:PROFILEID,:DISCOUNT,:SDATE,:EDATE,:COMMUNITY,:ENTRY_DT)";
             $res = $this->db->prepare($sql);
             $res->bindValue(":PROFILEID", $profileid, PDO::PARAM_INT);
             $res->bindValue(":DISCOUNT", $discount, PDO::PARAM_INT);
+            $res->bindValue(":SDATE", $startDate, PDO::PARAM_INT);
+            $res->bindValue(":EDATE", $endDate, PDO::PARAM_INT);
             $res->bindValue(":COMMUNITY", $community, PDO::PARAM_INT);
             $res->bindValue(":ENTRY_DT", $entryDt, PDO::PARAM_INT);
             $res->execute();
