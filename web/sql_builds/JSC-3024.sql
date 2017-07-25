eb4cf35090cb3d5de61dcf8c3b3356cf16efef82
Smarth Katyal <smarth.katyal@localhost.localdomain>
2017-07-25 12:28:20 +0530

    [JSC-3079]

--- web/sql_builds/JSC-3024.sql
+++ web/sql_builds/JSC-3024.sql
@@ -30,7 +30,7 @@ CREATE TABLE billing.`EXCLUSIVE_CLIENT_MEMBER_MAPPING` (
 ) ENGINE=Myisam DEFAULT CHARSET=latin1;
 
 
-INSERT INTO jeevansathi_mailer.`EMAIL_TYPE` (`MAIL_ID`, `TPL_LOCATION`, `HEADER_TPL`, `FOOTER_TPL`, `TEMPLATE_EX_LOCATION`, `MAIL_GROUP`, `CUSTOM_CRITERIA`, `SENDER_EMAILID`, `DESCRIPTION`, `MEMBERSHIP_TYPE`, `GENDER`, `PHOTO_PROFILE`, `REPLY_TO_ENABLED`, `FROM_NAME`, `REPLY_TO_ADDRESS`, `MAX_COUNT_TO_BE_SENT`, `REQUIRE_AUTOLOGIN`, `FTO_FLAG`, `PRE_HEADER`, `PARTIALS`) VALUES (1857, 'jsExclusiveServiceDayMailer.tpl', NULL, 'revamp_footer.tpl', NULL, 27, 1, 'info@jeevansathi.com', 'Upgrade membership to JS Exclusive', 'D', NULL, NULL, NULL, 'Jeevansathi Info', NULL, NULL, NULL, NULL, 'Please add info@jeevansathi.com to your address book to ensure delivery of this mail into you inbox', '');
+INSERT INTO `EMAIL_TYPE` (`MAIL_ID`, `TPL_LOCATION`, `HEADER_TPL`, `FOOTER_TPL`, `TEMPLATE_EX_LOCATION`, `MAIL_GROUP`, `CUSTOM_CRITERIA`, `SENDER_EMAILID`, `DESCRIPTION`, `MEMBERSHIP_TYPE`, `GENDER`, `PHOTO_PROFILE`, `REPLY_TO_ENABLED`, `FROM_NAME`, `REPLY_TO_ADDRESS`, `MAX_COUNT_TO_BE_SENT`, `REQUIRE_AUTOLOGIN`, `FTO_FLAG`, `PRE_HEADER`, `PARTIALS`) VALUES (1857, 'jsExclusiveServiceDayMailer.tpl', NULL, 'revamp_footer.tpl', NULL, 27, 1, '~$senderEmail`', 'Upgrade membership to JS Exclusive', 'D', NULL, NULL, NULL, '~$senderName`', NULL, NULL, NULL, NULL, 'Please add ~$senderEmail` to your address book to ensure delivery of this mail into you inbox', '');
 
 
 
