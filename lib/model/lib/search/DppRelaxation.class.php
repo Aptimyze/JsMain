5785c922b7f75e703cd9864906c5c3d09a976d9f
bhavana <bhavana.kadwal@jeevansathi.com>
2017-07-26 09:35:06 +0530

    [JSM-4481] matchalerts cron changes

--- lib/model/lib/search/DppRelaxation.class.php
+++ lib/model/lib/search/DppRelaxation.class.php
@@ -11,6 +11,7 @@ class DppRelaxation {
         }
 
         public function getRelaxedMTONGUE($mtongue) {
+                $removeValuesIfNotPresent = array("7"); // Remove castes if they are not filled in but added as part of group currently bihari
                 $mtongueValues = explode(',', $mtongue);
                 $allHindiMtongues = FieldMap::getFieldLabel("allHindiMtongues", '', 1);
                 $checkHindiMtongues = array_flip($allHindiMtongues);
@@ -20,6 +21,12 @@ class DppRelaxation {
                                 $mtongueFlag = 1;
                         else
                                 $finalMtongue.=',' . $value;
+                }
+                foreach($removeValuesIfNotPresent as $val){
+                        if(!in_array($val, $mtongueValues)){ // remove bihari mtongue if not already present
+                                unset($checkHindiMtongues[$val]);
+                                $allHindiMtongues = array_flip($checkHindiMtongues);
+                        }
                 }
                 if ($mtongueFlag == 1)
                         $finalMtongue.= ',' . implode(',', $allHindiMtongues);
