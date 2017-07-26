b6b1cf8ef6837c41a89e0849b9c9001336e60fb4
administaror <nitesh.s@jeevansathi.com>
2017-07-26 15:58:28 +0530

    adding new task for js excluive ticket

--- lib/model/store/billing/billing_EXCLUSIVE_CLIENT_MEMBER_MAPPING.class.php
+++ lib/model/store/billing/billing_EXCLUSIVE_CLIENT_MEMBER_MAPPING.class.php
@@ -31,5 +31,81 @@ class billing_EXCLUSIVE_CLIENT_MEMBER_MAPPING extends TABLE
 			throw new jsException($e);
 		}
 	}
+
+
+  	/**
+     * Function to get pending entry from EXCLUSIVE_CLIENT_MEMBER_MAPPING table
+     *
+     * @param   $none
+     * @return  none
+     */ 
+	public function getPendingJsExclusiveEoiData()
+	{
+		try{
+				$sql = "select * from  billing.EXCLUSIVE_CLIENT_MEMBER_MAPPING where SCREENED_STATUS='P' /*and DATE(ENTRY_DT)=CURDATE()*/";
+				$res = $this->db->prepare($sql);
+				$res->execute();
+				while($row = $res->fetch(PDO::FETCH_ASSOC))
+				{	
+					$output[] = $row;
+				}
+		}
+		catch(Exception $e)
+		{
+			throw new jsException($e);
+		}
+		return $output;
+	}
+
+	/**
+     * Function to update screened status entry in EXCLUSIVE_CLIENT_MEMBER_MAPPING table
+     *
+     * @param   $id
+     * @return  none
+     */ 
+	public function updateScreenedStatus($id)
+	{
+		try{
+			if(is_array($params) && $params){
+				$sql = "UPDATE  `EXCLUSIVE_CLIENT_MEMBER_MAPPING` SET  `SCREENED_STATUS` =  'Y' WHERE  `ID` =:ID ";
+				$res = $this->db->prepare($sql);
+				$res->bindValue(":ID", $ID, PDO::PARAM_INT);
+				$res->execute();
+				return true;
+			}
+		}
+		catch(Exception $e)
+		{
+			throw new jsException($e);
+			return false;
+		}
+		
+	}
+
+	/**
+     * Function to update error reason and status entry in EXCLUSIVE_CLIENT_MEMBER_MAPPING table
+     *
+     * @param   $id
+     * @return  none
+     */ 
+	public function updateSendEoiError($id,$reason)
+	{
+		try{
+			if(is_array($params) && $params){
+				$sql = "UPDATE  `EXCLUSIVE_CLIENT_MEMBER_MAPPING` SET  `SCREENED_STATUS` =  'E' AND `FAILURE_REASON`=:REASON WHERE  `ID` =:ID ";
+				$res = $this->db->prepare($sql);
+				$res->bindValue(":ID", $ID, PDO::PARAM_INT);
+				$res->bindValue(":REASON", $ID, PDO::PARAM_STR);
+				$res->execute();
+				return true;
+			}
+		}
+		catch(Exception $e)
+		{
+			throw new jsException($e);
+			return false;
+		}
+		
+	}
 }
 ?>
