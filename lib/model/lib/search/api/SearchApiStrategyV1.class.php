1aef1e2ddc1ea65e7b1c14f31b808dbcd581013b
bhavana <bhavana.kadwal@jeevansathi.com>
2017-08-03 16:11:36 +0530

    [JSM-4504] changes

--- lib/model/lib/search/api/SearchApiStrategyV1.class.php
+++ lib/model/lib/search/api/SearchApiStrategyV1.class.php
@@ -352,6 +352,11 @@ class SearchApiStrategyV1
                                                 $clusterData = array("0"=>array("key"=>"L".$clusterIndex,"value"=>$clusterLVal),"1"=>array("key"=>"H".$clusterIndex,"value"=>$clusterHVal));
                                                 $this->output["searchSummary"]["FILTER_FIELD"] = $clusterData;
                                         }else{
+                                                if($clusterIndex == "EDUCATION_GROUPING"){
+                                                        $clusterIndex = "EDU_LEVEL_NEW";
+                                                }elseif($clusterIndex == "OCCUPATION_GROUPING"){
+                                                        $clusterIndex = "OCCUPATION";
+                                                }
                                                 eval('$clusterVal = $SearchParamtersObj->get'.$clusterIndex.'();');
                                                 $clusterData = array("0"=>array("key"=>$clusterIndex,"value"=>$clusterVal));
                                                 $this->output["searchSummary"]["FILTER_FIELD"] = $clusterData;
