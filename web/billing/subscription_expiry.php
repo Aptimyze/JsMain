77920f498742ba3e5c0ad0cea644af8c05a55dd3
Ayush Chauhan <ayush.chauhan@jeevansathi.com>
2017-08-09 13:53:52 +0530

    JSC-3134

--- web/billing/subscription_expiry.php
+++ web/billing/subscription_expiry.php
@@ -32,6 +32,19 @@ while ($row = mysql_fetch_array($res)) {
         $assisted_arr[$pid][] = $row['SERVEFOR'];
     }
 
+    // Deleting entry from billing.EXCLUSIVE_SERVICING as soon as subs expire
+    $servicingSQL = "SELECT * FROM billing.EXCLUSIVE_SERVICING
+                     WHERE CLIENT_ID = $pid";
+    $sqlOutput = mysql_query_decide($servicingSQL, $db) or die($sql . mysql_error_js());
+    $result = mysql_fetch_array($sqlOutput);
+    if ($result){
+        $servicingLogSQL = "INSERT INTO billing.EXCLUSIVE_SERVICING_LOG (AGENT_USERNAME, CLIENT_ID, ASSIGNED_DT, ENTRY_DT, SERVICE_DAY, SERVICE_SET_DT, BIODATA_LOCATION, BIODATA_UPLOAD_DT, SCREENED_DT, SCREENED_STATUS, EMAIL_STAGE) VALUES (".$result["AGENT_USERNAME"].",".$result["CLIENT_ID"].",".$result["ASSIGNED_DT"].",".$result["ENTRY_DT"].",".$result["SERVICE_DAY"].",".$result["SERVICE_SET_DT"].",".$result["BIODATA_LOCATION"].",".$result["BIODATA_UPLOAD_DT"].",".$result["SCREENED_DT"].",".$result["SCREENED_STATUS"].",".$result["EMAIL_STAGE"].")";
+        $servicingLogRes = mysql_query_decide($servicingLogSQL, $db) or die($sql . mysql_error_js());
+        $servicingSQL1 = "DELETE FROM billing.EXCLUSIVE_SERVICING 
+                     WHERE CLIENT_ID = $pid";
+        $servicingRes = mysql_query_decide($servicingSQL1, $db) or die($sql . mysql_error_js());
+    }
+
     $sql1 = "UPDATE billing.SERVICE_STATUS SET ACTIVE='E' WHERE ID=$id";
     $res1 = mysql_query_decide($sql1, $db) or die($sql . mysql_error_js());
     if ($row['SERVEFOR'] == 'I') {
