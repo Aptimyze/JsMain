a98134c297f01800ffd1311279562e952ea9e89b
Sanyam Chopra <sanyam.chopra@jeevansathi.com>
2017-08-16 12:30:14 +0530

    [JSM-4491] JSM-4491 dpp relaxation diet

--- lib/model/lib/search/DppRelaxation.class.php
+++ lib/model/lib/search/DppRelaxation.class.php
@@ -213,6 +213,21 @@ class DppRelaxation {
             $registerationHeight = $this->loggedInProfileObj->getHEIGHT();
             return $registerationHeight>$hheight?$registerationHeight:$hheight;
         }
+
+        /**
+         * returns relaxed diet
+         * @param  string $diet array which contains comma seprated values.
+         * @return string relaxed string or original diet string
+         */
+        public function getRelaxedDIET($diet)
+        {
+            $dietArr = explode(',',$diet);            
+            if (in_array('E',$dietArr) && !in_array('V',$dietArr)) //if Eggetarian exists and vegetarian is not selected
+            {                          
+                return $diet.",".TopSearchBandConfig::$vegetarian;
+            }            
+            return $diet;
+        }
 }
 
 ?>
