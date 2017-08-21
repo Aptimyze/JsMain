7cd9145d621b92d28c79b3421663792a80299d50
ankita.g <ankita.g@jeevansathi.com>
2017-08-21 14:20:52 +0530

    [JSC-3208] JSC-3028

--- crontabs/chat/consumerLogMonitoring.php
+++ crontabs/chat/consumerLogMonitoring.php
@@ -94,7 +94,7 @@
 
 	function sendMailAlert($subject){
 		//mail("lavesh.rawat@gmail.com,pankaj139@gmail.com,nitishpost@gmail.com,vibhor.garg@jeevansathi.com",$subject,"Please check");
-		mail("lavesh.rawat@gmail.com,nsitankita@gmail.com,nitishpost@gmail.com,vibhor.garg@jeevansathi.com",$subject,"Please check, restarting consumers as well");
+		mail("lavesh.rawat@gmail.com,nitishpost@gmail.com,vibhor.garg@jeevansathi.com",$subject,"Please check, restarting consumers as well");
 		//mail("nitishpost@gmail.com",$subject,"Please check");
 	}
 ?>
