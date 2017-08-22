b6de2de96351d79848049cc40e44f2755ce20067
Ayush Chauhan <ayush.chauhan@jeevansathi.com>
2017-08-22 13:16:44 +0530

    [JSC-3187] JSC-3187 sql build

--- web/sql_builds/JSC-3187.sql
+++ web/sql_builds/JSC-3187.sql
@@ -7,7 +7,7 @@ CREATE TABLE `billing.ExclusiveProposalMailer` (
   `TUPLE_ID` varchar(50) DEFAULT NULL,
   `STATUS` enum('N','U','Y','I') DEFAULT 'N',
   `DATE` date NOT NULL,
-  `FOLLOWUP_STATUS` varchar(2) NOT NULL,
+  `FOLLOWUP_STATUS` varchar(2) DEFAULT 'F0',
   `AGENT_PHONE` varchar(250) NOT NULL,
   UNIQUE KEY `RECEIVER` (`RECEIVER`,`TUPLE_ID`,`DATE`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
