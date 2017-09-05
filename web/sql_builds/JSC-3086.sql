0565a6c8dab312c1b02f175d8983ecc66761238e
Bharat Wadhwa <bharat.wadhwa@jeevansathi.com>
2017-09-05 13:02:45 +0530

    jsc-3086

--- web/sql_builds/JSC-3086.sql
+++ web/sql_builds/JSC-3086.sql
@@ -5,5 +5,5 @@ ALTER TABLE  `SMS_TYPE` CHANGE  `SMS_KEY`  `SMS_KEY` VARCHAR( 25 ) CHARACTER SET
 
 INSERT INTO  `SMS_TYPE` (  `ID` ,  `SMS_KEY` ,  `SMS_TYPE` ,  `PRIORITY` ,  `SUBSCRIPTION` ,  `GENDER` ,  `COUNT` ,  `TIME_CRITERIA` ,  `CUSTOM_CRITERIA` ,  `SMS_SUBSCRIPTION` ,  `STATUS` ,  `MESSAGE` ) 
 VALUES (
-'',  'EXCLUSIVE_PROPOSAL_SMS',  'I',  '',  'P',  'A',  'SINGLE',  '0',  '0',  'SERVICE',  'Y',  'Hi, JS Exclusive client {PROFILE_ID} likes your profile.We will call you shortly to know your response. View the profile here {DESCRIPTION_LINK}.'
+'',  'EXCLUSIVE_PROPOSAL_SMS',  'I',  '',  'P',  'A',  'SINGLE',  '0',  '0',  'SERVICE',  'Y',  'Hi, JS Exclusive client {USERNAME_ID} likes your profile.We will call you shortly to know your response. View the profile here {DESCRIPTION_LINK}.'
 );
