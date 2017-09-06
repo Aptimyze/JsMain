2574160ce38cec5dfdb90ae033f5a0580181ec53
root <root@svntrac.infoedge.com>
2017-09-06 10:04:36 +0000

    Capistrano ForceAllow JS/CSS changes

--- web/profile/commonfile_functions.php
+++ web/profile/commonfile_functions.php
@@ -723,7 +723,7 @@ $js_arr[]=array("jsms/login/uploadDocument_js"       => "5");
 $js_arr[]=array("jspc/login/uploadDoc_js"       => "5");
 $js_arr[]=array("/JSNotificationExtension/JSnotification/client"    => "2");
 $js_arr[]=array("jsms/settings/privacySettings_js"  => "2");
-$js_arr[]=array("photoScreening_js"  => "1");
+$js_arr[]=array("photoScreening_js"   => "2");
 $js_arr[]=array("apps/jeevansathi/templates/_jsms3DotLayer"   => "1");
 return $js_arr;
 }
