0af9b1abeafeb5f2ef4528c31d1ad1ec25ddc06a
root <root@trunk.jeevansathi.com>
2017-09-13 14:18:15 +0530

    edit bar issue resolved

--- web/spa/src/myjs/components/MyjsEditBar.js
+++ web/spa/src/myjs/components/MyjsEditBar.js
@@ -102,11 +102,25 @@ componentWillUnmount(){
   };
 
   render(){
+    var editBarView = this.props.profileInfo.incomplete.map((options,index) => {
+                       return (
+                           <div className="cell brdr6 vtop pad13 editBarStyle4">
+            <div className="txtc ">
+            <a href={options.url}>
+                <div className="editBarStyle5">
+                  <i className={"mainsp "+options.cssClass}></i>
+              </div>
+            </a>
+            <div className="f12 color7 fontlig">{options.title}</div>
+            </div>
+          </div>
+                       );
+                   }
+          ); 
+
     return(
       <div className="pad1 preload myjsedit1" id="profileDetailSection">
         <div className="row editBarStyle">
-
-
         <div className="cell brdr6 editBarStyle1">
           <a href="/profile/viewprofile.php?ownview=1">
           <div className="fullwid pad12" id="jsmsProfilePic">
@@ -125,100 +139,17 @@ componentWillUnmount(){
           </div>
           </a>
         </div>
-
+        {editBarView}
         <div className="cell brdr6 vtop pad13 editBarStyle4">
-          <div className="txtc ">
-              <a href="/profile/viewprofile.php?ownview=1#Album">
-                <div className="editBarStyle5">
-                  <i className="mainsp camera"></i>
-              </div>
-            </a>
-            <div className="f12 color7 fontlig">Photos</div>
-          </div>
-        </div>
-
-        <div className="cell brdr6 vtop pad13 editBarStyle4">
-          <div className="txtc ">
-            <a href="/profile/viewprofile.php?ownview=1#Family">
-              <div className="editBarStyle5">
-                <i className="mainsp myjs_family"></i>
-              </div>
-            </a>
-            <div className="f12 color7 fontlig">Family</div>
-          </div>
-        </div>
-
-        <div className="cell brdr6 vtop pad13 editBarStyle4">
-          <div className="txtc ">
-            <a href="/profile/viewprofile.php?ownview=1#Details">
-              <div className="editBarStyle5">
-                  <i className="mainsp basicdetail"></i>
-              </div>
-            </a>
-            <div className="f12 color7 fontlig">Basic</div>
-          </div>
-        </div>
-
-        <div className="cell brdr6 vtop pad13 editBarStyle4">
-          <div className="txtc ">
-              <a href="/profile/viewprofile.php?ownview=1#Kundli">
-              <div className="editBarStyle5">
-                  <i className="mainsp myjs_kundli"></i>
-              </div>
-            </a>
-            <div className="f12 color7 fontlig">Kundli</div>
-          </div>
-        </div>
-
-        <div className="cell brdr6 vtop pad13 editBarStyle4">
-          <div className="txtc ">
-              <a href="/profile/viewprofile.php?ownview=1#Career">
-                <div className="editBarStyle5">
-                  <i className="mainsp myjs_career"></i>
-              </div>
-            </a>
-            <div className="f12 color7 fontlig">Career</div>
-          </div>
-        </div>
-
-        <div className="cell brdr6 vtop pad13 editBarStyle4">
-          <div className="txtc ">
-              <a href="/profile/viewprofile.php?ownview=1#Education">
-                <div className="editBarStyle5">
-                  <i className="mainsp myjs_edu"></i>
-              </div>
-            </a>
-            <div className="f12 color7 fontlig">Education</div>
-          </div>
-        </div>
-
-        <div className="cell brdr6 vtop pad13 editBarStyle4">
-          <div className="txtc ">
-              <a href="/profile/viewprofile.php?ownview=1#Lifestyle">
-              <div className="editBarStyle5">
-                  <i className="mainsp lifestyle_2"></i>
-              </div>
-            </a>
-            <div className="f12 color7 fontlig">Lifestyle</div>
-          </div>
-        </div>
-
-        <div className="cell brdr6 vtop pad13 editBarStyle4">
-          <div className="txtc ">
-              <a href="/profile/viewprofile.php?ownview=1#Dpp">
+            <div className="txtc ">
+            <a href="/profile/viewprofile.php?ownview=1#Dpp">
                 <div className="editBarStyle5">
                   <i className="dppHeart"></i>
               </div>
             </a>
             <div className="f12 color7 fontlig">Desired Partner</div>
+            </div>
           </div>
-        </div>
-
-
-
-
-
-
         </div>
       </div>
     );
