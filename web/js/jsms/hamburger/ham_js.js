c035aa537e44284384c5508e82bc4268cdcb0ccf
Palash Chordia <palash.chordia@jeevansathi.com>
2017-09-13 13:01:50 +0530

    [JSI-3382] IPHONE HAM

--- web/js/jsms/hamburger/ham_js.js
+++ web/js/jsms/hamburger/ham_js.js
@@ -186,3 +186,42 @@ $("[hamburgermenu]").each(function(){
 $("#hamTollFree").bind("click",function(ev){
 			window.location.href="tel://18004196299";
 		});	
+(function () {
+    var _overlay = document.getElementById('scrollElem');
+    var _clientY = null; // remember Y position on touch start
+
+    _overlay.addEventListener('touchstart', function (event) {
+        if (event.targetTouches.length === 1) {
+            // detect single touch
+            _clientY = event.targetTouches[0].clientY;
+        }
+    }, false);
+
+    _overlay.addEventListener('touchmove', function (event) {
+        if (event.targetTouches.length === 1) {
+            // detect single touch
+            disableRubberBand(event);
+        }
+    }, false);
+
+    function disableRubberBand(event) {
+        var clientY = event.targetTouches[0].clientY - _clientY;
+
+        if (_overlay.scrollTop === 0 && clientY > 0) {//alert('1');
+            // element is at the top of its scroll
+            event.preventDefault();
+        }
+
+        if (isOverlayTotallyScrolled() && clientY < 0) {//alert('2');
+            //element is at the top of its scroll
+            event.preventDefault();
+        }
+    }
+
+    function isOverlayTotallyScrolled() {
+        // https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollHeight#Problems_and_solutions
+        // alert(_overlay.scrollHeight - _overlay.scrollTop);
+        // alert(_overlay.clientHeight);
+        return _overlay.scrollHeight - _overlay.scrollTop <= _overlay.clientHeight;
+    }
+}())
