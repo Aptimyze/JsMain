3198643b11e7771fbec5186a44f50861d009af97
Sanyam Chopra <sanyam.chopra@jeevansathi.com>
2017-09-22 11:11:20 +0530

    [SPA] SPA removed margin bottom from PD

--- web/spa/src/viewProfile/components/FamilyTab.js
+++ web/spa/src/viewProfile/components/FamilyTab.js
@@ -184,7 +184,7 @@ class FamilyTab extends React.Component {
     	}
 
     	return(
-    		<div id="FamilyTab" className="mb56 dn pad5 bg4 fontlig color3 clearfix f14">
+    		<div id="FamilyTab" className="dn pad5 bg4 fontlig color3 clearfix f14">
 				<div className="hgt10"></div>
 				{FamilyData}
 				{space}
