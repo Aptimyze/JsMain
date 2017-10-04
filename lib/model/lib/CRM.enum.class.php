819a280e806ef3ca9d24911f228378941dfe7320
root <root@localhost.localdomain>
2017-10-04 21:23:20 +0530

    [JSC-3195] JSC-3195

--- lib/model/lib/CRM.enum.class.php
+++ lib/model/lib/CRM.enum.class.php
@@ -30,12 +30,12 @@ class crmParams
 	public static $salesRegularPuneCity		=array('MH01','MH02','MH03','MH05','MH06','MH07','MH08','MH09','MH10','MH11','MH15','MH16','MH17','MH18','MH19','MH20','MH21','MH22','MH23','MH24','MH25','MH26','MH27');
 	public static $salesRegularNriCountry 		=array(1,2,3,60,138,4,139,140,5,6,8,141,9,10,142,143,12,144,145,13,15,16,146,17,19,148,149,21,22,23,150,151,153,24,26,154,27,28,156,29,30,31,33,157,158,34,35,36,160,161,162,163,164,37,39,40,165,167,69,42,43,44,45,168,169,170,172,173,174,175,46,47,176,49,50,53,54,55,177,56,57,58,215,61,62,63,64,65,66,180,181,67,182,68,216,184,185,186,71,72,73,187,74,76,188,189,190,191,77,192,79,81,193,83,195,84,86,87,88,89,90,91,93,94,218,96,197,97,198,199,200,201,202,99,100,204,101,102,104,219,106,107,109,111,206,207,112,113,114,117,118,208,209,120,121,122,210,123,124,125,126,128,127,129,130,132,133,134,135);	
 	public static $premiumIncome			=array(13,14,17,18,19,20,21,22,23,24,25,26,27);
-	public static $salesRegularCampaign		=array("1"=>"noida","2"=>"mumbai","3"=>"pune","4"=>"nri","5"=>"delhi","6"=>"noidaNew");
-	public static $salescampaignNames		=array('noida'=>'JS_NCRNEW','mumbai'=>'JS_MAHNEW','noidaNew'=>'JS_NCRNEW_Auto');
+	public static $salesRegularCampaign		=array("1"=>"noida","2"=>"mumbai","3"=>"pune","4"=>"nri","5"=>"delhi","6"=>"noidaAuto");
+	public static $salescampaignNames		=array('noida'=>'JS_NCRNEW','mumbai'=>'JS_MAHNEW','noidaAuto'=>'JS_NCRNEW_Auto');
 	public static $inDialerCampaign			=array("noida","mumbai","pune","delhi");
-	public static $inDialerCampaignNewArr           =array("noidaNew");
+	public static $inDialerCampaignNewArr           =array("noidaAuto");
 	public static $salesRegularValueRange		=array('SCORE1'=>'70','SCORE2'=>'90','SCORE3'=>'100','DISCOUNT1'=>'70','DISCOUNT2'=>'90');
-        public static $salesRegularCampaignTables	=array("noida"=>"incentive_SALES_CSV_DATA_NOIDA","mumbai"=>"incentive_SALES_CSV_DATA_MUMBAI","pune"=>"incentive_SALES_CSV_DATA_PUNE","nri"=>"incentive_SALES_CSV_DATA_NRI","delhi"=>"incentive_SALES_CSV_DATA_DELHI","noidaNew"=>"incentive_SALES_CSV_DATA_NOIDA_NEW");
+        public static $salesRegularCampaignTables	=array("noida"=>"incentive_SALES_CSV_DATA_NOIDA","mumbai"=>"incentive_SALES_CSV_DATA_MUMBAI","pune"=>"incentive_SALES_CSV_DATA_PUNE","nri"=>"incentive_SALES_CSV_DATA_NRI","delhi"=>"incentive_SALES_CSV_DATA_DELHI","noidaAuto"=>"incentive_SALES_CSV_DATA_NOIDA_NEW");
 
 	// Other Sales Campaign Tables for Processes
 	public static $salesCampaignTables		=array("failedPaymentInDialer"=>"incentive_SALES_CSV_DATA_FAILED_PAYMENT","upsellProcessInDialer"=>"incentive_SALES_CSV_DATA_UPSELL","renewalProcessInDialer"=>"incentive_SALES_CSV_DATA_RENEWAL","paidCampaignProcess"=>"incentive_SALES_CSV_DATA_PAID_CAMPAIGN","rcbCampaignInDialer"=>"incentive_SALES_CSV_DATA_RCB");
