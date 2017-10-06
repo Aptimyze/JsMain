fd0e6b141ddc37d845f610c12de215b503a719a9
Atul Shukla <atul.shukla@jeevansathi.com>
2017-10-06 18:12:41 +0530

    url check //

--- web/spa/src/common/components/UrlDecoder.js
+++ web/spa/src/common/components/UrlDecoder.js
@@ -1,16 +1,16 @@
 export function getRoutePath(location)
 {
 	var hash = location.split('#')[1] || '';
+
   if(hash)
 	{
-		hash = hash.replace(/\//g, "");
+		hash = hash.replace(/^\/+/g, '');
 		hash = "/" + hash;
 	}
 	else
 	{
 		if ( location.indexOf('spa/dist/index.html') !== -1 )
-		{
-			hash = hash.replace(/\//g, "");
+		{		
 			hash = '/myjs';
 		}
 	}
