2618e75b85ebfe98009ebfef2da3fbdebbfa0572
Palash Chordia <palash.chordia@jeevansathi.com>
2017-10-11 12:13:35 +0530

    [SPA_fix3] LOGIN BUG AND SIMILAR PROFILES PUSH URL ISSUE

--- web/profile/commonfile_functions.php
+++ web/profile/commonfile_functions.php
@@ -510,7 +510,7 @@ $js_arr[]=array("jsms/angular/registration/scripts/services" => "6");
 $js_arr[]=array("jsms/angular/registration/scripts/directives"  => "28");
 $js_arr[]=array("jsms/angular/registration/scripts/factories"        => "59");
 $js_arr[]=array("jsms/angular/registration/hamburger_reg_js"   => "35");
-$js_arr[]=array("jsms/contact_engine/contactEngine_js"                  => "108");
+$js_arr[]=array("jsms/contact_engine/contactEngine_js"                  => "109");
 $js_arr[]=array("jsms/profile/viewProfile/jsmsViewProfile_js"         => "51");
 $js_arr[]=array("jsms/search/inview"   => "5");
 $js_arr[]=array("jsms/login/newMobLogin_js"         => "71");
