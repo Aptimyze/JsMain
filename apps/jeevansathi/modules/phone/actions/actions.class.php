3bb5c404c656e2023da7b6147b3afcf6937a207d
gautam.himanshu <gautam.himanshu@99acres.com>
2017-10-18 10:46:58 +0530

    [JSI-3447_1] initial commit

--- apps/jeevansathi/modules/phone/actions/actions.class.php
+++ apps/jeevansathi/modules/phone/actions/actions.class.php
@@ -276,6 +276,17 @@ class phoneActions extends sfActions
 				$sendingObject->deleteRequestedByOther($profileid);
 				$loggingObj = new MIS_REQUEST_DELETIONS_LOG();
                 $loggingObj->logThis(LoggedInProfile::getInstance()->getUSERNAME(),$profileid,'Other');
+				
+				reportAbuseLib::reportAbuseAction(
+				LoggedInProfile::getInstance()->getPROFILEID(),
+				LoggedInProfile::getInstance()->getUSERNAME(),
+				$otherProfileId, /* viewed user's profID */
+				'',/*$categoryNew empty */
+				'',/*$otherReason empty */
+				'',/*$category empty */
+				'',/*$crmUserName empty */
+				-1/*m_iAbuseAttachmentID - 1 */
+				);
 			}
 
 			$ReportInvalidLibObj->sendExtraNotification($selfProfileID,$profileid,$reasonNumber);
