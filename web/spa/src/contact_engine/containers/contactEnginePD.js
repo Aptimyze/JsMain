f8f186cd0afa5658eab14363099f0256acc69c8d
Palash Chordia <palash.chordia@jeevansathi.com>
2017-10-26 11:33:16 +0530

    [JSI-3507_2] JSI-3507

--- web/spa/src/contact_engine/containers/contactEnginePD.js
+++ web/spa/src/contact_engine/containers/contactEnginePD.js
@@ -345,12 +345,51 @@ getCancelDeclineLayer(actionDetails){
   }
 
 goToViewSimilar(){
+  let similarProfileCheckSumTemp = window.location.search.split('similarOf='), similarProfileCheckSum;
+  if(typeof similarProfileCheckSumTemp[1]!="undefined" && similarProfileCheckSumTemp[1])
+    similarProfileCheckSum = similarProfileCheckSumTemp[1].split("&")[0];
+  else
+    similarProfileCheckSum = "";
+
+  if(!this.canIShowNext(similarProfileCheckSum,this.props.profiledata.profilechecksum)) return;
   this.closeAllOpenLayers();
   let _this=this;
   setTimeout(
     function(){
       window.location.href = "/search/MobSimilarProfiles?profilechecksum="+_this.props.profiledata.profilechecksum+"&fromProfilePage=1&fromSPA_CE=1";
     },1000);
+}
+
+
+canIShowNext(parentUsername,username)
+{
+  var str = localStorage.getItem("viewSim4");
+  var newString='';
+        if(str && parentUsername)
+        {
+                if(str.indexOf(',')!='-1')
+                {
+                        var res = str.split(",");
+                        if(res[0]== parentUsername)
+                        {
+                                newString = res[0]+","+username;
+                        }
+      else
+      {
+        return false;
+      }
+                }
+    else if(str!=username)
+    {
+                  newString = parentUsername+","+username;
+    }
+        }
+        else{
+    newString = username;
+}
+  if(newString)
+    localStorage.setItem("viewSim4",newString);
+  return true;
 }
 
 }
