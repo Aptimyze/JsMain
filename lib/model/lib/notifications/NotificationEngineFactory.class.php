e03c42f39b12c9eac819733044322a0caf04f8db
root <root@localhost.localdomain>
2017-11-01 19:00:12 +0530

    JSC-3421

--- lib/model/lib/notifications/NotificationEngineFactory.class.php
+++ lib/model/lib/notifications/NotificationEngineFactory.class.php
@@ -3,14 +3,13 @@ class NotificationEngineFactory{
         private $sendMultipleParallelNotification;
 
         public function __construct($sendMultipleParallelNotification=false){
-                //$this->gcmObj = new GCM($sendMultipleParallelNotification);
+                $this->gcmObj = new GCM($sendMultipleParallelNotification);
 		$this->sendMultipleParallelNotification =$sendMultipleParallelNotification;
         }
 
 	public function geNotificationEngineObject($engineType,$notificationType=''){
                 switch($engineType){
                 	case GCM:
-				$this->gcmObj = new GCM($this->sendMultipleParallelNotification);
                 	        return $this->gcmObj;
                 	        break;
                 	case IOS:
