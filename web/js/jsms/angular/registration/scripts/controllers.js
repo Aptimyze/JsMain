45d7f50761caa284be8b9747c871cb2d07a1d333
Palash Chordia <palash.chordia@jeevansathi.com>
2017-11-02 14:52:08 +0530

    [pixelCodeBug] COMMONFILE AND REGISTRATION

--- web/js/jsms/angular/registration/scripts/controllers.js
+++ web/js/jsms/angular/registration/scripts/controllers.js
@@ -1543,8 +1543,16 @@
 					).then(function() {
 						if($scope.success==1)
 						{
-							if($scope.isPhoneMob)
-								setTimeout(function(){window.location="/phone/jsmsDisplay";},1000);
+							if($scope.isPhoneMob){
+								var source='', groupName='';
+								try{
+										var jsonTemp = JSON.parse(localStorage.getItem('trackServerParams'));
+										source = jsonTemp.source;
+										groupName = jsonTemp.groupname;
+								}
+								catch(e){console.log('pixel error'+e);}
+								setTimeout(function(){window.location="/phone/jsmsDisplay?fromReg=1&source="+source+"&groupname="+groupName;},1000);
+							}
 							else
 								setTimeout(function(){window.location="/profile/mainmenu.php";},1000);
 						}
