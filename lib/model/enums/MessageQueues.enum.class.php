55fa14c6742d7ce140c2d86531e8c44e4dfbd4cc
nitish.sharma <nitish.sharma@99acres.com>
2017-11-06 13:28:44 +0530

    [JSI-3557] JSI-3557 increased limit

--- lib/model/enums/MessageQueues.enum.class.php
+++ lib/model/enums/MessageQueues.enum.class.php
@@ -36,7 +36,7 @@ class MessageQueues
   CONST VIEW_LOG = "ViewLogQueue";
   CONST FALLBACK_SERVER_MSGPICK_COUNT = 10; 
   //per queue msg limit mapping
-  public static $upperMessageLimitPerQueue = array("default"=>50000,"INSTANT_NOTIFICATION_QUEUE"=>90000);
+  public static $upperMessageLimitPerQueue = array("default"=>50000,"INSTANT_NOTIFICATION_QUEUE"=>400000);
   public static $queuesWithoutMsgCountLimit = array("SCHEDULED_NOTIFICATION_QUEUE1","SCHEDULED_NOTIFICATION_QUEUE2", "SCHEDULED_NOTIFICATION_QUEUE3", "SCHEDULED_NOTIFICATION_QUEUE4","SCHEDULED_NOTIFICATION_QUEUE5","SCHEDULED_NOTIFICATION_QUEUE6","profile-created-queue","profile-deleted-queue","roster-created-acceptance","roster-created-acceptance_sent","roster-created-intrec","roster-created-intsent","roster-created-shortlist","roster-updated-queue","roster-created-dpp","chat","delayed_profile_delete_queue","DISC_HISTORY_QUEUE","DelayedMailQueue","MatchAlertNotification","WriteMsgDelayedQueue"); //queues not to be considered for msg upper limit alert
   CONST SAFE_LIMIT = 700000000;     //Limit in MB's for the difference between memory allowed and memory used by rabbitmq.
   CONST MSGBODYLIMIT = NULL;  //to prevent truncation of message. NULL specify that a message of any length can be sent over the queue.
