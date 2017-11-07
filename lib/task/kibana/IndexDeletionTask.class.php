76d7c154cc8d27b522e1cda33a35281a560661d6
mohammad.shahjahan <mohammad.shahjahan@jeevansathi.com>
2017-11-07 17:20:26 +0530

    added changes in Kibana job task.[JSM-4839]

---
+++ lib/task/kibana/IndexDeletionTask.class.php
@@ -0,0 +1,39 @@
+<?php
+
+class IndexDeletionTask extends sfBaseTask
+{
+  protected function configure()
+  {
+    $this->namespace        = 'kibana';
+    $this->name             = 'IndexDeletionTask';
+    $this->briefDescription = 'Delete indices from elk.';
+    $this->detailedDescription = <<<EOF
+The [kibana:IndexDeletionTask|INFO] task does things.
+Call it with:
+  [php symfony kibana:IndexDeletionTask|INFO]
+EOF;
+  }
+
+  protected function execute($arguments = array(), $options = array())
+  {
+    $currdate = date('Y.m.d', strtotime( '-8 days' ));
+    // Server at which ElasticSearch and kibana is running
+    $elkServer = JsConstants::$kibana['ELK_SERVER'];
+    $elkPort = JsConstants::$kibana['ELASTIC_PORT'];
+    $indexName = KibanaEnums::$COOLMETRIC_INDEX.$currdate;
+    // print_r(KibanaEnums::$COOLMETRIC_INDEX_DELETION_LIMIT );
+    $indicesToDelete = array(
+                        KibanaEnums::$COOLMETRIC_INDEX.date('Y.m.d', strtotime( KibanaEnums::$COOLMETRIC_INDEX_DELETION_LIMIT )),
+                        KibanaEnums::$FILEBEAT_INDEX.date('Y.m.d', strtotime( KibanaEnums::$FILEBEAT_INDEX_DELETION_LIMIT )),
+                        KibanaEnums::$ANDROIDCHAT_INDEX.date('Y.m.d', strtotime( KibanaEnums::$ANDROIDCHAT_INDEX_DELETION_LIMIT )),
+                        KibanaEnums::$OPENFIRE_INDEX.date('Y.m.d', strtotime( KibanaEnums::$OPENFIRE_INDEX_DELETION_LIMIT )),
+                        KibanaEnums::$APACHE_INDEX.date('Y.m.d', strtotime( KibanaEnums::$APACHE_INDEX_DELETION_LIMIT )),
+                        KibanaEnums::$SERVER_INDEX.date('Y.m.d', strtotime( KibanaEnums::$SERVER_INDEX_DELETION_LIMIT )),
+                        KibanaEnums::$RABBITTIME_INDEX.date('Y.m.d', strtotime( KibanaEnums::$RABBITTIME_INDEX_DELETION_LIMIT )),
+                        );
+
+    foreach ($indicesToDelete as $indexName) {
+      passthru("curl -XDELETE 'http://$elkServer:$elkPort/$indexName/'");
+    }
+  }
+}
