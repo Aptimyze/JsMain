8854208f21d52ae13de9720f2c80def3488dfdaf
administaror <nitesh.s@jeevansathi.com>
2017-11-13 12:22:40 +0530

    stopping logging for memchae object for viewed case

--- lib/model/lib/profileCommunication/ProfileMemcache.class.php
+++ lib/model/lib/profileCommunication/ProfileMemcache.class.php
@@ -497,7 +497,7 @@ class ProfileMemcache
         if ($this->_memcache && is_array($this->_memcache))
             return $this->_memcache;
         else
-            jsException::log("Profile Memcache Object is not set.");
+            //jsException::log("Profile Memcache Object is not set for Id : ".$this->_getProfileId());
     }
     
     /**
