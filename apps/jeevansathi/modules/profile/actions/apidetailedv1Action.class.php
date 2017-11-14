8a231d50e80a675fad13e8862c887acc1793adae
lavesh <ankit.shukla@jeevansathi.com>
2017-11-14 18:19:38 +0530

    show vsp flag

--- apps/jeevansathi/modules/profile/actions/apidetailedv1Action.class.php
+++ apps/jeevansathi/modules/profile/actions/apidetailedv1Action.class.php
@@ -325,6 +325,8 @@ class apidetailedv1Action extends sfAction
                 if (JsConstants::$hideUnimportantFeatureAtPeakLoad >= 3) {
 			$out['show_vsp'] = false;
 		}
+                if(MobileCommon::isAndroidApp() && $this->loginProfile->getPROFILEID()%9 >= 1)
+                        $out['show_vsp'] = false;
 		//adding an extra flag which was in detailedAction but was missing from the api
 		$out["astroSent"] = $this->checkIfAstroSent();
 		return $out;
