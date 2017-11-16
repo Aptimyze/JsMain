9fc28160b3cdcd512d301092a8007e5964c772e1
gautam.himanshu <gautam.himanshu@99acres.com>
2017-11-16 14:08:19 +0530

    [JSI-3539] LIGHTNING CAL INTEGRATION INTO PROFILEPAGE AND INTRODUCES CACHE TO LIGHTNING CAL

--- apps/jeevansathi/modules/profile/actions/apidetailedv1Action.class.php
+++ apps/jeevansathi/modules/profile/actions/apidetailedv1Action.class.php
@@ -160,7 +160,18 @@ class apidetailedv1Action extends sfAction
 		//Now Create OutPut Array
 		$arrOut = $this->BakeMyView();
 		$arrOut['USERNAME']=$this->profile->getUSERNAME();
+////////////lightning cal code starts here/////////////////////////////////
 
+// redis implementation
+// 
+	$request->setParameter('calFromPD',1);
+	$request->setParameter('layerId',19);
+	sfContext::getInstance()->getController()->getPresentationFor("common", "ApiCALayerV1");
+	$layerData = ob_get_contents();
+	ob_end_clean();
+	$layerData = json_decode($layerData, true);
+	$arrOut['calObject'] = $layerData['calObject'] ? $layerData['calObject'] : null;
+///////////////////
 		$respObj = ApiResponseHandler::getInstance();
 		if($x)
     	{
