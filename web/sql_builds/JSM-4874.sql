29430fb0640bda3732863981134024e3aa9020a1
bhavana <bhavana.kadwal@jeevansathi.com>
2017-11-23 14:25:58 +0530

    [JSM-4874] add city

---
+++ web/sql_builds/JSM-4874.sql
@@ -0,0 +1,21 @@
+use newjs;
+INSERT INTO `CITY_NEW` ( `ID` , `LABEL` , `VALUE` , `TYPE` , `SORTBY` , `DD_TOP` , `DD_TOP_SORTBY` , `STD_CODE` , `COUNTRY_VALUE` )
+VALUES (
+'', 'Amravati (Maharastra)', 'MH46', 'CITY', '0', NULL , NULL , NULL , '51'
+);
+
+
+update CITY_NEW target
+join
+(
+  SELECT ID, (
+@rownumber := @rownumber +1
+) AS rownum
+FROM CITY_NEWB
+CROSS JOIN (
+
+SELECT @rownumber :=0
+)r
+ORDER BY LABEL ASC
+) source on target.ID = source.ID    
+set SORTBY = rownum 
