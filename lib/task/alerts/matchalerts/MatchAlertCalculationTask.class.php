25141b7cf90a0d850ccc31a7228da914a5e04347
bhavana <bhavana.kadwal@jeevansathi.com>
2017-12-07 10:16:13 +0530

    [JSM-4951] unified to 100

--- lib/task/alerts/matchalerts/MatchAlertCalculationTask.class.php
+++ lib/task/alerts/matchalerts/MatchAlertCalculationTask.class.php
@@ -90,7 +90,8 @@ EOF;
                                         $matchLogic = $v["MATCH_LOGIC"];
 					if($loggedInProfileObj->getPROFILEID())
 					{
-                                                if($loggedInProfileObj->getPROFILEID()%99<=49){
+                                                //if($loggedInProfileObj->getPROFILEID()%99<=49){
+                                                if(1){
                                                         if($matchLogic == "O"){
                                                                 $strictDppObj = new StrictDppBasedMatchAlertsStrategy($loggedInProfileObj, MailerConfigVariables::$UNIFIED_LOGIC_LIST_COUNT,MailerConfigVariables::$UNIFIED_LOGIC_MAILER_COUNT, $trends);
                                                                 $totalResults = $strictDppObj->getMatches();
