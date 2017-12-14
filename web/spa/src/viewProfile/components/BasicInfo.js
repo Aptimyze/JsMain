6d131fe3b0c0646e6d932b0ec991093bd04834c7
lavesh <ankit.shukla@jeevansathi.com>
2017-12-14 16:24:04 +0530

    [JSM-4884] download app

--- web/spa/src/viewProfile/components/BasicInfo.js
+++ web/spa/src/viewProfile/components/BasicInfo.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import GA from "../../common/components/GA";
 
 export default class BasicInfo extends React.Component {
 	constructor(props) {
         super();
+        this.state = {};
+    }
+    showChatOnAppLayer(){
+        this.setState({showPromoLayer:true});
+    }
+    hideChatOnAppLayer(){
+        this.setState({showPromoLayer:false});
     }
     render() {
     	let have_child = "";
@@ -34,12 +42,20 @@ export default class BasicInfo extends React.Component {
     		special_case_title = <div className="f14 color1">Special Cases</div>;
     		special_case = <div className="fontlig pb15" id="vpro_special_case" >{this.props.about.special_case}</div>
     	}
+        let lastActive=(<div></div>);
+        if(this.props.onlineInfo){
+            if(this.props.onlineInfo==2)
+                lastActive = (<span className="f11 color13" id="vpro_last_active" >{this.props.about.last_active}</span>);
+            else lastActive= (<span onClick ={this.showChatOnAppLayer.bind(this)} className="f11 color13" id="vpro_last_active" >Online Now</span>);
+        }
+        let layerHtml = this.state.showPromoLayer ? <ChatLayer /> : <div></div>;
     	return (
     		<div className="pad5 bg4 fontlig color3 clearfix f14">
+                {layerHtml}
     			<div className="hgt10"></div>
   				<div className="fl">
 	  				<span className="f18" id="vpro_username" >{this.props.about.username}</span>&nbsp;&nbsp;
-	  				<span className="f11 color13" id="vpro_last_active" >{this.props.about.last_active}</span>
+                                {lastActive}
   				</div>
   				<div className="fr color2 f14 pt5 fontrobbold" id="vpro_subscription">{this.props.about.subscription_text}</div>
   				<div className="clr hgt10"></div>
@@ -78,4 +94,42 @@ export default class BasicInfo extends React.Component {
   			</div>
     	);
     }
+}
+
+
+class ChatLayer extends React.Component{
+    constructor(props){
+        super(props)
+        this.state={
+            style1:{}
+        }
+        this.GAObject = new GA();
+    }
+    
+    componentDidMount(){
+        let di = document.getElementById('chatSubLayer').getBoundingClientRect();
+        
+        this.setState({style1:{
+                marginLeft:'-'+di.width/2+'px',marginTop:'-'+di.height/2+'px',
+        }}); 
+    }
+    
+    trackDownloadAppWithGA(){
+        this.GAObject.trackJsEventGA("jsms","appD","1");
+        window.location.href = "/static/appredirect";
+    }
+    render()
+    {
+        return (<div id="chatOnAppLayer" className="backoverlayL">
+<div id="chatSubLayer" className="otpcenter cssLayerFix bg4 fontlig posabs" style={{...this.state.style1,left:'50%',top:'50%',width:'90%'}}>
+<p className="color3 txtc pt20 txtl f16 padl30" align="left">Real time CHAT with online members<br /> on Jeevansathi App</p>
+<p className="color4 txtc pt20 pb30 f14 txtl padl30 pt10" styletext-align="left">Other Benefits:<ul style={{listStylePosition:'inside'}}><li className="mt2">Get acceptances from online users</li><li className="mt2">Smoother,faster and richer experience</li><li className="mt2">100% Ad free experience guaranteed</li><li className="mt2">Real Time notifications on interests,<br />acceptances and recommendations</li></ul></p>
+<div style={{lineHeight:'60px',borderTop:'1px solid #dbdbdb'}} className="txtc">
+<div id="js-downApp" className="f19"><div onClick={this.trackDownloadAppWithGA.bind(this)} style={{color:'#d03e43'}}>Download App</div></div>
+</div>
+</div>
+</div>);
+    }
+    
+    
 }
