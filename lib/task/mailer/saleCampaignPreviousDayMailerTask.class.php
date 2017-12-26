82c9529d41bec8a5139825a0288571910268c67b
Robin <robin.singh@jeevansathi.com>
2017-12-26 11:28:19 +0530

    JSC-3418

--- lib/task/mailer/saleCampaignPreviousDayMailerTask.class.php
+++ lib/task/mailer/saleCampaignPreviousDayMailerTask.class.php
@@ -26,19 +26,19 @@ EOF;
             sfContext::createInstance($this->configuration);
         }
         
-      $date = date('Y-m-d', strtotime($date .' -1 day'));
-      $prev_date = date('Y-m-d', strtotime($date .' -1 day'));
-      $prev_date = $prev_date." 00:00:00";
+      $date = date('Y-m-d'); 
+      $prev_date = date('Y-m-d', strtotime($date.' -1 day'));
+      $start_date = $prev_date." 00:00:00";
       $end_date = $prev_date." 23:59:59";
       
-      $incentiveSaleCampaignDetaiObj = new incentive_SALES_CAMPAIGN_PROFILE_DETAILS();
+      $incentiveSaleCampaignDetaiObj = new incentive_SALES_CAMPAIGN_PROFILE_DETAILS("crm_slave");
       $serviceCount = $incentiveSaleCampaignDetaiObj -> getCountSentMailPreviousDate($prev_date,$end_date);
        
       $freeServiceCount = $serviceCount['IB_Service'];
       $paidServiceCount = $serviceCount['IB_PaidService'];
       $to ="anant.gupta@naukri.com,anurag.tripathi@jeevansathi.com";
       $from = "JeevansathiCrm@jeevansathi.com";
-      $subject = "CSAT survey mails sent for paid and free service < $date >";
+      $subject = "CSAT survey mails sent for paid and free service < $prev_date >";
       $message = "Free survey mails sent : $freeServiceCount <br> Paid survey mails sent : $paidServiceCount";
 
       sendMail::send_email($to,$message,$subject,$from);
