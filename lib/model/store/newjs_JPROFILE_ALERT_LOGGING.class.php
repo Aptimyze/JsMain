bf184dc0bbe6f3b04662e2c7fbc9864dcea2123e
Robin <robin.singh@jeevansathi.com>
2017-12-27 14:46:45 +0530

    JSC-3619

--- lib/model/store/newjs_JPROFILE_ALERT_LOGGING.class.php
+++ lib/model/store/newjs_JPROFILE_ALERT_LOGGING.class.php
@@ -8,6 +8,8 @@ class newjs_JPROFILE_ALERT_LOGGING extends TABLE
     }
     
     public function insert($profileid,$changetype,$perviousValue,$currentValue,$date){
+        $perviousValue = $this->mappingValue($perviousValue);
+        $currentValue  = $this->mappingValue($currentValue);
         try {
             $sql="INSERT INTO newjs.JPROFILE_ALERT_LOGGING(PROFILEID,CHANGE_TYPE,PREVIOUS_VALUE,CURRENT_VALUE,MOD_DT) VALUES (:PROFILEID,:CHANGETYPE,:PREVIOUSVALUE,:CURRENTVALUE,:MODDT)";
             $prep = $this->db->prepare($sql);
@@ -19,6 +21,21 @@ class newjs_JPROFILE_ALERT_LOGGING extends TABLE
             $prep->execute();
         } catch (PDOException $e) {
             throw new jsException($e);
+        }
+    }
+    
+    public function mappingValue($value){
+        if($value == 'U'){
+            return "Unsub";
+        }
+        if($value == 'S'){
+            return "Sub";
+        }
+        if($value == 'D' || $value == 'A'){
+            return "Daily";
+        }
+        if($value == 'O'){
+            return "3 Times";
         }
     }
 }
