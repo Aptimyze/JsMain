d7d2b161c720a1b8bc6c02a7cec36893da221297
bhavana <bhavana.kadwal@jeevansathi.com>
2018-01-03 12:54:14 +0530

    [JSM-5072] test server remove sms

--- crontabs/swap_jpartner.php
+++ crontabs/swap_jpartner.php
@@ -260,9 +260,14 @@ function mysql_error1($db)
 {
 	global $sql_update,$sql,$sql_total_points;
 	$msg=$sql_update .":".$sql.":".$sql_total_points;
-echo mysql_error($db);
-	mail("lavesh.rawat@jeevansathi.com,kumar.anand@jeevansathi.com,lavesh.rawat@gmail.com","Jeevansathi Error in swapping",$msg);
-	mail("lavesh.rawat@jeevansathi.com,kumar.anand@jeevansathi.com,lavesh.rawat@gmail.com","Jeevansathi Error in swapping",mysql_error($db));
+        echo mysql_error($db);
+        if(JsConstants::$whichMachine == 'prod'){
+                mail("lavesh.rawat@jeevansathi.com,kumar.anand@jeevansathi.com,lavesh.rawat@gmail.com,reshu.rajput@jeevansathi.com,bhavanakadwal@gmail.com","Jeevansathi Error in swapping",$msg);
+                mail("lavesh.rawat@jeevansathi.com,kumar.anand@jeevansathi.com,lavesh.rawat@gmail.com,reshu.rajput@jeevansathi.com,bhavanakadwal@gmail.com","Jeevansathi Error in swapping",mysql_error($db));
+        }elseif(JsConstants::$whichMachine == 'test'){
+                mail("anish.singh@jeevansathi.com,vidushi@naukri.com,sunendra.gupta@jeevansathi.com","Jeevansathi Error in swapping",$msg);
+                mail("anish.singh@jeevansathi.com,vidushi@naukri.com,sunendra.gupta@jeevansathi.com","Jeevansathi Error in swapping",mysql_error($db));
+        }
 }
 
 function updateBucket($myrow_sql_rel_caste_result,$db)
